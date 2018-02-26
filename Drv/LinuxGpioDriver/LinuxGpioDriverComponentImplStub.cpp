9be586274492652a65877da7de16d0dd4f1856ec
Gene Merewether <mereweth@jpl.nasa.gov>
2018-02-26 08:56:31 -0800

    Add Linux device drivers

---
+++ Drv/LinuxGpioDriver/LinuxGpioDriverComponentImplStub.cpp
@@ -0,0 +1,67 @@
+// ======================================================================
+// \title  LinuxGpioDriverImpl.cpp
+// \author tcanham
+// \brief  cpp file for LinuxGpioDriver component implementation class
+//
+// \copyright
+// Copyright 2009-2015, by the California Institute of Technology.
+// ALL RIGHTS RESERVED.  United States Government Sponsorship
+// acknowledged. Any commercial use must be negotiated with the Office
+// of Technology Transfer at the California Institute of Technology.
+//
+// This software may be subject to U.S. export control laws and
+// regulations.  By accepting this document, the user agrees to comply
+// with all U.S. export laws and regulations.  User has the
+// responsibility to obtain export licenses, or other export authority
+// as may be required before exporting such information to foreign
+// countries or providing access to foreign persons.
+// ======================================================================
+
+
+#include <Drv/LinuxGpioDriver/LinuxGpioDriverComponentImpl.hpp>
+#include "Fw/Types/BasicTypes.hpp"
+
+namespace Drv {
+
+  // ----------------------------------------------------------------------
+  // Handler implementations for user-defined typed input ports
+  // ----------------------------------------------------------------------
+
+  void LinuxGpioDriverComponentImpl ::
+    gpioRead_handler(
+        const NATIVE_INT_TYPE portNum,
+        bool &state
+    )
+  {
+    // TODO
+  }
+
+  void LinuxGpioDriverComponentImpl ::
+    gpioWrite_handler(
+        const NATIVE_INT_TYPE portNum,
+        bool state
+    )
+  {
+    // TODO
+  }
+
+  bool LinuxGpioDriverComponentImpl ::
+    open(NATIVE_INT_TYPE gpio, GpioDirection direction) {
+      return false;
+  }
+
+  Os::Task::TaskStatus LinuxGpioDriverComponentImpl ::
+    startIntTask(NATIVE_INT_TYPE priority, NATIVE_INT_TYPE cpuAffinity) {
+     return Os::Task::TASK_OK;
+   }
+
+  LinuxGpioDriverComponentImpl ::
+    ~LinuxGpioDriverComponentImpl(void)
+  {
+
+  }
+
+  void LinuxGpioDriverComponentImpl ::
+    exitThread(void) {
+  }
+} // end namespace Drv
