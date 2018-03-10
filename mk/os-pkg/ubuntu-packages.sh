6dc5e58350936ec600f44431c79adcd2090f196e
Timothy Canham <timothy.canham@jpl.nasa.gov>
2018-03-10 12:44:06 -0800

    Missing connections

--- mk/os-pkg/ubuntu-packages.sh
+++ mk/os-pkg/ubuntu-packages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo apt-get install python-pip python-dev python-lxml python-tk python-cheetah python-enum34 python-markdown python-pexpect python-pmw python-numpy python-scipy python-matplotlib python-pycurl python-pil.imagetk
+sudo apt-get install time python-pip python-dev python-lxml python-tk python-cheetah python-enum34 python-markdown python-pexpect python-pmw python-numpy python-scipy python-matplotlib python-pycurl python-pil.imagetk
