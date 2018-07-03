1ec62d9c44a09aefe4f1f2f6c75ae31b20eb8235
jbiberst <josef.x.biberstein@jpl.nasa.gov>
2018-07-03 16:27:40 -0700

    Added callback for events to Event panel

--- Gds/src/templates/cmd_template.py
+++ Gds/src/templates/cmd_template.py
@@ -8,9 +8,61 @@ Instances of this class describe a specific command type.
 
 @bug No known bugs
 '''
-
+from enum import Enum
 import data_template
+
+
+
 class CommandTemplate(data_template.DataTemplate):
 
-    def __init__(self, parameter_list):
-        pass
+    def __init__(self, component, mnemonic, opcode, description, arguments):
+        ## Make sure correct types are passed
+
+        if not type(component) == type(str()):
+            raise TypeMismatchException(type(str()),type(component))
+
+        if not type(mnemonic) == type(str()):
+            raise TypeMismatchException(type(str()),type(mnemonic))
+
+        if not type(opcode) == type(int()):
+            raise TypeMismatchException(type(int()),type(opcode))
+
+        if not type(description) == type(str()):
+            raise TypeMismatchException(type(str()),type(description))
+
+        if not type(arguments) == type(list()):
+            raise TypeMismatchException(type(list()),type(arguments))
+
+        for (argname,argdesc,argtype) in arguments:
+            #
+            if not type(argname) == type(str()):
+                raise TypeMismatchException(type(int()),type(argname))
+            #
+            if not type(argdesc) == type(str()):
+                raise TypeMismatchException(type(int()),type(argdesc))
+            #
+            if not issubclass(type(argtype), type(BaseType())):
+                raise TypeMismatchException(type(BaseType()),type(argtype))
+
+        # Initialize command internal variables
+        self.component = component
+        self.mnemonic = mnemonic
+        self.opcode = opcode
+        self.description = description
+        self.arguments = arguments
+
+
+    def get_component(self):
+        return self.component
+
+    def get_mnemonic(self):
+        return self.mnemonic
+
+    def get_op_code(self):
+        return self.opcode
+
+    def get_description(self):
+        return self.description
+
+    def get_args(self):
+        return self.arguments
