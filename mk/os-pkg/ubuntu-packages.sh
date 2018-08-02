23019579b62022fecf695caeeeac6f1755014f4b
Timothy Canham <timothy.canham@jpl.nasa.gov>
2018-08-02 07:23:27 -0700

    Added wxPython dependencies

--- mk/os-pkg/ubuntu-packages.sh
+++ mk/os-pkg/ubuntu-packages.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo apt-get install pkg-config g++ python-pip python-lxml python-tk python-dev
+sudo apt-get install pkg-config g++ python-pip python-lxml python-tk python-dev gtk+-3.0 libgstreamer-plugins-base1.0-dev
