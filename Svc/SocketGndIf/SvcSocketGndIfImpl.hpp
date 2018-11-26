e3d1211e6aa538dd94a944b6f10042e933b686d6
Gene Merewether <mereweth@jpl.nasa.gov>
2018-11-26 11:00:41 -0800

    Pull in GndIf and SocketGndIf changes

--- Svc/SocketGndIf/SvcSocketGndIfImpl.hpp
+++ Svc/SocketGndIf/SvcSocketGndIfImpl.hpp
@@ -24,6 +24,8 @@ namespace Svc {
                 SEND_TCP
             };
 
+            void setSocketTaskProperties(I32 priority, NATIVE_INT_TYPE stackSize, U32 portNumber, char* hostname, DownlinkProt prot = SEND_UDP, NATIVE_INT_TYPE cpuAffinity = -1);
+
             void startSocketTask(NATIVE_INT_TYPE priority, NATIVE_INT_TYPE stackSize, U32 port_number, char* hostname,  DownlinkProt prot = SEND_UDP, NATIVE_INT_TYPE cpuAffinity = -1);
             void setUseDefaultHeader(bool useDefault);
         private:
@@ -39,6 +41,13 @@ namespace Svc {
                 Fw::Buffer &fwBuffer
             );
 
+            //! Implementation for GNDIF_ENABLE_INTERFACE command handler
+            //! Enable the interface for ground communications
+            void GNDIF_ENABLE_INTERFACE_cmdHandler(
+                const FwOpcodeType opCode, /*!< The opcode*/
+                const U32 cmdSeq /*!< The command sequence number*/
+            );
+
             static void socketReadTask(void* ptr);
             void downlinkPort_handler(NATIVE_INT_TYPE portNum, Fw::ComBuffer &data, U32 context);
             Svc::ConnectionStatus isConnected_handler(NATIVE_INT_TYPE portNum);
@@ -49,11 +58,16 @@ namespace Svc {
             NATIVE_INT_TYPE m_udpConnectionFd; // !< connection file descriptor
             struct sockaddr_in m_servAddr; // !< UDP server
 
+            I32 m_priority;
+            NATIVE_INT_TYPE m_stackSize;
             U32 port_number;
             char* hostname;
+            NATIVE_INT_TYPE m_cpuAffinity;
             Os::Task socketTask;
             bool useDefaultHeader; // Use the default header for downlink, ie "A5A5 GUI "
             DownlinkProt m_prot; // is downlink TCP or UDP
+
+            bool m_portConfigured;
     };
 
 }
