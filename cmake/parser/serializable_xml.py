4b4a617c4ff5d1d6939ab758106a5d39b060f18b
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2019-03-14 16:21:27 -0700

    mstarch: cmake scripts to python 3

--- cmake/parser/serializable_xml.py
+++ cmake/parser/serializable_xml.py
@@ -56,7 +56,7 @@ def get_serializable_targets(input_file, library_name):
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print "Usage: serializable_xml.py <input_Ai.xml> <module_name>"
+        print("Usage: serializable_xml.py <input_Ai.xml> <module_name>")
         exit(1)
 
     get_serializable_targets(sys.argv[1], sys.argv[2])
