0867a770d30670aaf947252a42f42c966265aea6
Michael Starch <Michael.D.Starch@jpl.nasa.gov>
2019-03-19 12:07:20 -0700

    mstarch: adding in cross-compile, and toolchain support with examples

--- cmake/platform/arm-linux-gnueabihf.cmake
+++ cmake/platform/arm-linux-gnueabihf.cmake
@@ -1,19 +1,9 @@
-# Set system name
-set(CMAKE_SYSTEM_NAME "arm-linux-gnueabihf")
+##
+# arm-linux-gnueabihf.cmake:
+#
+# A platform file supporting the raspberry pi. This is to parallel the toolchain file
+# of the same name.
+##
 
-# Location of pi toolchain
-set(PI_TOOLCHAIN "/home/campuzan/tools/pi-tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf/")
-
-# specify the cross compiler
-set(CMAKE_C_COMPILER "${PI_TOOLCHAIN}/bin/arm-linux-gnueabihf-gcc")
-set(CMAKE_CXX_COMPILER "${PI_TOOLCHAIN}/bin/arm-linux-gnueabihf-g++")
-
-# where is the target environment
-set(CMAKE_FIND_ROOT_PATH  "${PI_TOOLCHAIN}")
-
-# search for programs in the build host directories
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-# for libraries and headers in the target directories
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+# RPI is Linux based, so include the common Linux items
+include("${CMAKE_CURRENT_LIST_DIR}/Linux-common.cmake")
