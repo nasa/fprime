1563ed7ba38b50d453503ebe86927e9be8bd2b06
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2019-04-17 14:04:08 -0700

    mstarch: purging remaining ITAR notices for: #189

--- Svc/ActiveRateGroup/ActiveRateGroupImplCfg.hpp
+++ Svc/ActiveRateGroup/ActiveRateGroupImplCfg.hpp
@@ -11,11 +11,6 @@
 *   acknowledged. Any commercial use must be negotiated with the Office
 *   of Technology Transfer at the California Institute of Technology.
 *
-*   Information included herein is controlled under the International
-*   Traffic in Arms Regulations ("ITAR") by the U.S. Department of State.
-*   Export or transfer of this information to a Foreign Person or foreign
-*   entity requires an export license issued by the U.S. State Department
-*   or an ITAR exemption prior to the export or transfer.
 */
 
 #ifndef ACTIVERATEGROUP_ACTIVERATEGROUPIMPLCFG_HPP_
