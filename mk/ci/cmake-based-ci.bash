5d36c9fea4ced39acc52bad5d0dddf6464bf5f23
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2019-04-30 17:47:11 -0700

    mstarch: better AC UTs

--- mk/ci/cmake-based-ci.bash
+++ mk/ci/cmake-based-ci.bash
@@ -15,12 +15,22 @@ fi
 # Run standard UTs via CMake
 cd ..
 mkdir build-dir-ut && cd build-dir-ut
-cmake ../Ref -DPLATFORM=ut/`uname` && make -j32 && make -j32 check
+cmake .. -DPLATFORM=ut/`uname` && make -j32 && make -j32 check
 if (( $? != 0 ))
 then
     echo "[ERROR] Failed to run CMake and process UTs for 'Ref'"
     exit 2
 fi
+
+for target in `make help | grep "exe"`
+do
+    (
+        mkdir ut_${target}
+        cd ut_${target}
+        cmake ../.. -DPLATFORM=ut/`uname` && make ${target} -j32 && make test ${target}
+    )
+done
+
 # Change to the CMake test directory and run CMake UTs
 cd ..
 cd cmake/test/src
