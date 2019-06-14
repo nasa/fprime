957d441cb1ffa3119248ad3038f413134e84fb31
koran <kevin.c.oran@jpl.nasa.gov>
2019-06-13 18:24:37 -0700

    Updated API to better mimic old assert naming

--- Gds/src/fprime_gds/common/utils/gds_test_api.py
+++ Gds/src/fprime_gds/common/utils/gds_test_api.py
@@ -14,44 +14,83 @@ class IntegrationTestAPI:
     """
     def __init__(self, pipeline):
         self.pipeline = pipeline
+
+    def clear_histories(self, fsw_time_stamp=None):
+        """
+        Clears histories. Because the command history is not correlated to a FSW timestamp, it will be cleared 
+        entirely. This function can be used to setup tests so that the histories only represent objects received 
+        during a test.  
+        :param fsw_time_stamp: If specified, event and telemetry histories will be cleared up until the timestamp. 
+        """
+        pass
         
-    def assert_send_cmd(self, cmd, cmd_args):
+    def assert_send_command(self, command, args):
         """
         Sends a command and asserts that the command was translated successfully. If the command is in conflict
         with the flight dictionary, this will raise a test failure. 
-        :param cmd: either the command id or a command mnemonic to specify the type of command
-        :param cmd_args: a list of command arguments to send
+        Note: this assert does not check that the command was received by flight software, only that the command
+        and arguments were valid with respect to the flight dictionary
+        :param command: Either the command id or a command mnemonic to specify the type of command
+        :param args: a list of command arguments to send
         :return: If the assert is successful, will an instance of CmdData from data_types.cmd_data
         """
         pass
     
-    def assert_received_tlm(self, tlm, timeout=0, val_pred=None, fsw_time_pred=None, history=None):
+    def assert_receive_telemetry(self, channel, val_pred=None, fsw_time_pred=None, history=None, timeout=0):
         """
-        Asserts that a telemetry update was received on the specified channel. This assert will search the 
-        history for a telemetry that satisfies the specified constraints. If unsuccessful, the assert will 
-        then await a valid telemetry update on the history until the timeout. The assert will succeed and 
-        return immediately when a valid telemetry object is found. If no valid update was found in the history 
-        nor while waiting, the assert will fail. 
-        :param tlm: either the channel id or a channel mnemonic to specify the telemetry channel
-        :param timeout: If specified, the assert will behave as an await statement after searching available history. Negative values are ignored.
+        Asserts that a specified telemetry update was received. This funciton will search the history for an 
+        update that satisfies the specified constraints. If unsuccessful, this call will await a valid 
+        telemetry update then return successfully. If no valid update was received before the timeout, this 
+        call will assert failure.
+        :param channel: Either the channel id or a channel mnemonic to specify the telemetry channel
         :param val_pred: If specified, a valid telemetry update must have a value that satisfies this predicate.
         :param fsw_time_pred: If specified, a valid telemetry update must have a fsw timestamp that satisfies this predicate.
-        :param history: If specified, the assert will substitute the given history for the aggregate history.
+        :param history: If specified, the assert will substitute the given history for the default history.
+        :param timeout: If specified, the assert will behave as an await statement after searching available history. Negative values are ignored.
         :return: If the assert is successful, will return the instance of ChData from data_types.ch_data that satisfied the assert.
         """
         pass
 
-    def assert_received_evr(self, evr, timeout=0, args=None, fsw_time_pred=None, history=None):
+    def assert_telemetry_count(self, count_pred, channel_list=None, val_pred=None, fsw_time_pred=None, history=None, timeout=0):
+        """
+        An assert on the number of telemetry updates received. If the history doesn't have the correct update 
+        count, the call will await until a correct count is achieved or the timeout, at which point it will 
+        assert failure.
+        :param count_pred: A predicate to determine whether the correct amount has been received.
+        :param channel_list: If specified, a counted telemetry update must have a channel on this list of channels.
+        :param val_pred: If specified, a counted telemetry update must have a value that satisfies this predicate.
+        :param fsw_time_pred: If specified, a counted telemetry update must have a FSW timestamp that satisfies this predicate.
+        :param history: If specified, the assert will substitute the given history for the default history.
+        :param timeout: If specified, the assert will behave as an await statement after searching available history. Negative values are ignored.
+        :return: If the assert is successful, will return the instance of ChData from data_types.ch_data that satisfied the assert.
+        """
+        pass
+
+    def assert_receive_event(self, event, args=None, fsw_time_pred=None, history=None, timeout=0):
         """
         Asserts that a specified event message was received. This funciton will search the history for an 
         event that satisfies the specified constraints. If unsuccessful, this call will await a valid 
-        event until the timeout. The assert will succeed and return immediately when a valid event object 
-        is found. If no valid message was found in the history nor while waiting, the assert will fail. 
-        :param tlm: either the event id or a event mnemonic to specify the event type.
-        :param timeout: If specified, the assert will behave as an await statement after searching available history. Negative values are ignored.
+        event then return successfully. If no valid message was received before the timeout, this 
+        call will assert failure.
+        :param event: Either the event id or an event mnemonic to specify the event type.
         :param args: If specified, a valid event message must have arguments matching the given list of args. Include None to ignore an element.
         :param fsw_time_pred: If specified, a valid event must have a fsw timestamp that satisfies this predicate.
-        :param history: If specified, the assert will substitute the given history for the aggregate history.
-        :return: If the assert is successful, will return the instance of ChData from data_types.ch_data that satisfied the assert.
+        :param history: If specified, the assert will substitute the given history for the default history.
+        :param timeout: If specified, the assert will behave as an await statement after searching available history. Negative values are ignored.
+        :return: If the assert is successful, this call will return the instance of EventData from data_types.event_data that satisfied the assert.
         """
+        pass
+
+    def assert_event_count(self, count_pred, event_list=None, args=None, fsw_time_pred=None, history=None, timeout=0):
+        """
+        An assert on the number of events received. If the history doesn't have the correct event count, the
+        call will await until a correct count is achieved or the timeout at which point it will assert failure.
+        :param count_pred: A predicate to determine whether the correct amount has been received.
+        :param event_list: If specified, a counted event must have an ID on this list of channels.
+        :param args: If specified, a valid event message must have arguments matching the given list of args. Include None to ignore an element.
+        :param fsw_time_pred: If specified, a valid event must have a fsw timestamp that satisfies this predicate.
+        :param history: If specified, the assert will substitute the given history for the default history.
+        :param timeout: If specified, the assert will behave as an await statement after searching available history. Negative values are ignored.
+        :return: If the assert is successful, this call will return the number that satisfied the predicate.
+        """    
         pass
