6014a563b64e1309b9cdd57c4e0f24da144e1108
koran <kevin.c.oran@jpl.nasa.gov>
2019-06-19 18:00:38 -0700

    Added field getters to the cmd, evr, ch datat types.

--- Gds/src/fprime_gds/common/data_types/event_data.py
+++ Gds/src/fprime_gds/common/data_types/event_data.py
@@ -26,7 +26,7 @@ class EventData(sys_data.SysData):
                         Each element's class should match the class of the
                         corresponding argument type object in the event_temp
                         object. This can be None.
-            event_time: The time the event occured (TimeType)
+            event_time: The time the event occurred (TimeType)
             event_temp: Event template instance for this event
 
         Returns:
@@ -38,10 +38,14 @@ class EventData(sys_data.SysData):
         self.time = event_time
         self.template = event_temp
 
+    def get_id(self):
+        return self.id
 
     def get_args(self):
         return self.args
 
+    def get_time(self):
+        return self.time
 
     @staticmethod
     def get_empty_obj(event_temp):
