dd6b6ad0e6d9c274e28abd62d3d558442ccf921e
koran <kevin.c.oran@jpl.nasa.gov>
2019-07-09 17:48:55 -0700

    added Ref app start script and demo integration tests

---
+++ Ref/api-demo/demo_integration_test.py
@@ -0,0 +1,50 @@
+import os
+import sys
+filename = os.path.dirname(__file__)
+gdsName = os.path.join(filename, "../../Gds/src")
+fprimeName = os.path.join(filename, "../../Fw/Python/src")
+sys.path.insert(0, gdsName)
+sys.path.insert(0, fprimeName)
+
+from fprime_gds.common.pipeline.standard import StandardPipeline
+from fprime_gds.common.testing_fw import predicates
+from fprime_gds.common.testing_fw.api import IntegrationTestAPI
+from fprime_gds.common.utils.config_manager import ConfigManager
+
+
+class TestRefAppClass(object):
+
+    @classmethod
+    def setup_class(cls):
+        cls.pipeline = StandardPipeline()
+        config = ConfigManager()
+        filename = os.path.dirname(__file__)
+        path = os.path.join(filename, '../Top/RefTopologyAppDictionary.xml')
+        cls.pipeline.setup(config, path)
+        cls.pipeline.connect('127.0.0.1', 50000)
+        cls.api = IntegrationTestAPI(cls.pipeline)
+
+    @classmethod
+    def teardown_class(cls):
+        cls.pipeline.disconnect()
+
+    def setup_method(self, method):
+        self.api.start_test_case(method.__name__)
+
+    def test_is_streaming(self):
+        pred = predicates.greater_than(0)
+        results = self.api.await_telemetry_count(pred, timeout=5)
+        for result in results:
+            print("received channel update: {}".format(result.get_str()))
+        assert pred(len(results))
+
+    def test_send_command(self):
+        self.api.send_command("CMD_NO_OP", [])
+        assert self.api.get_command_test_history().size() == 1
+
+    def test_send_and_await_no_op(self):
+        evrs = ["OpCodeDispatched", "NoOpReceived", "OpCodeCompleted"]
+        results = self.api.send_and_await_event("CMD_NO_OP", events=evrs)
+        assert len(results) == 3
+        for event in results:
+            print(event)
---
+++ Ref/api-demo/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# *******************************************************************************
+# * Copyright 2013, by the California Institute of Technology.
+# * ALL RIGHTS RESERVED. United States Government Sponsorship
+# * acknowledged.
+# *
+# * regulations. By accepting this document, the user agrees to comply
+# * with all applicable U.S. export laws and regulations.  User has the 
+# * responsibility to obtain export licenses,
+# * or other export authority as may be required before exporting such
+# * information to foreign countries or providing access to foreign
+# * persons.
+# *
+ /home/kevin/software/fprime-sw/Gds/bin/run_deployment.sh --dictionary /home/kevin/software/fprime-sw/Ref/Top -d /home/kevin/software/fprime-sw/Ref -g none
