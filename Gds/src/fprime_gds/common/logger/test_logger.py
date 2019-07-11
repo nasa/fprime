3c3bb1878d19ca8a51485a39af579b3dded2d8b7
koran <kevin.c.oran@jpl.nasa.gov>
2019-07-10 18:03:04 -0700

    Updated test logger documentation

--- Gds/src/fprime_gds/common/logger/test_logger.py
+++ Gds/src/fprime_gds/common/logger/test_logger.py
@@ -1,8 +1,14 @@
 """
 test_logger.py:
 
-A wrapper on the openpyxl library that provides the ability to log messages in an excel sheet. Used by
-the test API to log events, asserts, test cases and user messages.
+A wrapper on the openpyxl library that provides the ability to log messages in an excel sheet. Used
+by the test API to log events, asserts, test cases and user messages. The documentation for openpyxl
+can be found here:
+https://openpyxl.readthedocs.io/en/stable/index.html
+
+This class uses a write-only optimization that should allow for creating large log files without
+hogging too much memory. Write-only optimization can be found here:
+https://openpyxl.readthedocs.io/en/stable/optimized.html#write-only-mode
 
 :author: koran
 """
@@ -15,9 +21,8 @@ from openpyxl.cell import WriteOnlyCell
 
 class TestLogger:
     """
-    User-accessible colors.
+    User-accessible colors. Can be used for the color arguments
     """
-
     BROWN = "996633"
     RED = "FF6666"
     ORANGE = "FF9900"
@@ -27,8 +32,9 @@ class TestLogger:
     PURPLE = "9933FF"
     GRAY = "C0C0C0"
     WHITE = "FFFFFF"
+
     """
-    User-accessible styles
+    User-accessible styles. Must be used for the style arguments
     """
     BOLD = "BOLD"
     ITALICS = "ITALICS"
@@ -53,8 +59,17 @@ class TestLogger:
         header.append(self.__get_cell__("Sender", style=self.BOLD))
         header.append(self.__get_cell__("Message", style=self.BOLD))
         self.worksheet.append(header)
-    
+
     def __get_cell__(self, string, color=None, style=None):
+        """
+        Helper method for log message. This method takes a string as well as color and style
+        arguments to create a write-only cell.
+
+        Args:
+            string: a string object to be written to the cell's value field.
+            color: a string object containing a color hex code "######"
+            style: a string choosing 1 of 3 formatting options (ITALICS, BOLD, UNDERLINED)
+        """
         cell = WriteOnlyCell(self.worksheet, value=string)
         if color is not None:
             cell.fill = PatternFill("solid", fgColor=color)
@@ -67,6 +82,15 @@ class TestLogger:
         return cell
 
     def log_message(self, message, sender="NA", color=None, style=None):
+        """
+        Logs a message to the TestLog. Each message will include a timestamp beforehand.
+
+        Args:
+            message: a message to log (str).
+            sender: a short string describing who created the log message
+            color: a string object containing a color hex code "######"
+            style: a string choosing 1 of 3 formatting options (ITALICS, BOLD, UNDERLINED)
+        """
         ts = time.time()
         timestring = datetime.datetime.fromtimestamp(ts).strftime("%m/%d/%Y %H:%M:%S")
 
@@ -77,4 +101,7 @@ class TestLogger:
         self.worksheet.append(row)
 
     def close_log(self):
+        """
+        Saves the write-only workbook. Should be called only once when the log is completed.
+        """
         self.workbook.save(filename=self.filename)
