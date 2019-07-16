83901274766dacf9e8ffa01ab5a1222d4085bfa5
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2019-07-16 10:18:59 -0700

    mstarch: small fixes for the templating system

--- Gds/src/fprime_gds/flask/static/js/simple-templates.js
+++ Gds/src/fprime_gds/flask/static/js/simple-templates.js
@@ -15,7 +15,40 @@
  *
  * @author mstarch
  */
+let __SIMPLE_TEMPLATES_RERENDER_LIST = {};
+
+/**
+ * Rerenders attached templates that are affected by the given reference.
+ * @param reference: reference to force rerender
+ */
+function __ST_rerenderChanged(reference) {
+    let updates = __SIMPLE_TEMPLATES_RERENDER_LIST[reference];
+    for (let key in ((typeof (updates) == "undefined") ? [] : updates)) {
+        updates[key].rerender();
+    }
+}
+
+/**
+ * Register global update paths for simple templates.
+ * @param template: template to rerender
+ * @param reference: reference to trigger it
+ * @private
+ */
+function __ST_registerReference(template, reference) {
+    // Create a list on first call
+    if (typeof (__SIMPLE_TEMPLATES_RERENDER_LIST[reference]) === "undefined") {
+        __SIMPLE_TEMPLATES_RERENDER_LIST[reference] = [];
+    }
+    // Don't reregister
+    if (__SIMPLE_TEMPLATES_RERENDER_LIST[reference].indexOf(template) == -1) {
+        __SIMPLE_TEMPLATES_RERENDER_LIST[reference].push(template);
+    }
+}
+
 export class SimpleTemplate {
+    FOR_REGEX = /for\s+([a-zA-Z0-9_]+)\s+in(.*)/;
+    TPL_REGEX = /{{(.*)}}/;
+
     /**
      * Creation of a template requires an element that is the container for the output html from the template. In
      * addition, an HTML element or string containing HTML is read in to provide the templated code, and (optionally)
@@ -25,8 +58,6 @@ export class SimpleTemplate {
      * @param data: data to bind to, or undefined if to be registered later.
      */
     constructor(element, template, data) {
-        this.forreg = /for\s+([a-zA-Z0-9_]+)\s+in\s+([a-zA-Z0-9_]+)/;
-        this.mustreg = /{{(.*)}}/;
         this.data = data;
         this.element = element;
         // Check if the element is undefined
@@ -38,7 +69,6 @@ export class SimpleTemplate {
                 throw TypeError("Invalid element id supplied to template: " + element);
             }
         }
-        this._registerEvents();
         // Read the interHtml, if defined, otherwise treat as string
         if (!(template instanceof HTMLElement)) {
             template = document.getElementById(template);
@@ -47,7 +77,7 @@ export class SimpleTemplate {
     }
 
     /**
-     * Register the event handlers to handle several different events.
+     * Registers events "change", "mousedown", and "click" to the given element. Wraps the given data in
      * @param element: element to register for events
      * @param data: data attribute to read for passing in data
      * @private
@@ -72,6 +102,8 @@ export class SimpleTemplate {
         this.element.addEventListener("click", this, false);
     }
 
+
+
     /**
      * Element ID to rerender w.r.t. the driven data.
      * @param id: (optional) rerender this sub ID, or the current element if not supplied.
@@ -86,14 +118,15 @@ export class SimpleTemplate {
             elem = this.element.getElementById(id);
             elem.value = data;
         } else {
-            let newNode = this._template(this.template, this.data);
+            // Create a new node that is our template
+            let newNode = this._template(this.template, this.data, true);
             // With active children replace last always
             if (this.element.childNodes.length >= 1) {
                 this.element.replaceChild(newNode, this.element.lastChild);
             }
             // Zero children, append
             else {
-                    this.element.appendChild(newNode);
+                this.element.appendChild(newNode);
             }
         }
     }
@@ -116,6 +149,25 @@ export class SimpleTemplate {
     }
 
     /**
+     * Register a click. When this element is clicked, the
+     * @param element:
+     * @param data:
+     * @private
+     */
+    _registerClick(element, data) {
+        let _self = this;
+        let clickFun = function(event) {
+            let key = element.getAttribute("click-action");
+            let reference = _self.data[key];
+            _self.data[key] = data;
+            __ST_rerenderChanged(reference);
+            return true;
+        };
+        element.addEventListener("click", clickFun, false);
+    }
+
+
+    /**
      * Update data from element change
      * @private
      */
@@ -135,69 +187,157 @@ export class SimpleTemplate {
             throw Error("Unable to find data item for id: " + element.id);
         }
     }
+
     /**
      * Run the templating function.
      * @param tempNode: template node used as an input for templates.
      * @param data: data map to reference for elements. Will read directly from data bindings if not available.
+     * @param top: top-level node, strip "hidden"
      * @private
      */
-    _template(tempNode, data) {
-        let dataBind = null;
-        let match = null;
-
-        // Text nodes template the text and return the result.
+    _template(tempNode, data, top) {
+        let dolly = null;
+        top = (typeof(top) !== "undefined") ? top : false;
+        // Comments essentially get stripped from templates
         if (tempNode.nodeType == Node.COMMENT_NODE) {
             return document.createDocumentFragment();
         }
+        // Handle text nodes, as they are barren and without attributes
         else if (tempNode.nodeType == Node.TEXT_NODE) {
-            return this._templateText(tempNode, data);
+            let currentNode = tempNode.cloneNode(false);
+            currentNode.textContent = this._templateText(currentNode.textContent, data);
+            return currentNode;
         }
-        // Check if has data binding property. Only supports "for x in y"
-        else if ((dataBind = tempNode.getAttribute("data-bind")) != null && (match = dataBind.match(this.forreg)) != null) {
-            let itemList = data[match[2]];
-            let fragment = document.createDocumentFragment();
-            // Loop over all elements
-            for (let i = 0; i < itemList.length; i++) {
-                // Assign subdata and clone node
-                let newData = {};
-                newData[match[1]] = data[match[2]][i];
-                let newNode = tempNode.cloneNode(false);
-                // Loop over all children
-                for (let j = 0; j < tempNode.childNodes.length; j++) {
-                    newNode.appendChild(this._template(tempNode.childNodes[j], newData));
-                }
-                fragment.appendChild(newNode);
-            }
-            return fragment;
-        }
+        // Multiply the node
         else {
-            let newNode = tempNode.cloneNode(false);
-            newNode.removeAttribute("hidden");
-            // Loop over all children
-            for (let j = 0; j < tempNode.childNodes.length; j++) {
-                newNode.appendChild(this._template(tempNode.childNodes[j], data));
-            }
-            return newNode;
+            dolly = this._templateCloneMultiply(tempNode, data);
         }
+        // Loop over all nodes, templating the attributes, and children nodes
+        for (let i = 0; i < dolly.childNodes.length; i++) {
+            let currentNode = dolly.childNodes[i];
+            // Strip the hidden attribute from our template
+            if (top) {
+                currentNode.removeAttribute("hidden");
+            }
+            // Evaluate data based on item in list
+            let newData = Object.assign({}, data);
+            if (currentNode.hasAttribute("data-eval") && currentNode.hasAttribute("data-name")) {
+                newData[currentNode.getAttribute("data-name")] = this._templateEvaluation(currentNode.getAttribute("data-eval"), newData);
+            }
+            // Data Bind elements update when changed
+            if (currentNode.hasAttribute("data-bind") && top) {
+                let reference = this._templateEvaluation(currentNode.getAttribute("data-bind"), newData);
+                __ST_registerReference(this, reference);
+            }
+            if (currentNode.hasAttribute("click-action")) {
+                let evalu = newData;
+                // Get the attached data
+                if (currentNode.hasAttribute("data-eval") && currentNode.hasAttribute("data-name")) {
+                    evalu = this._templateEvaluation(currentNode.getAttribute("data-eval"), newData);
+                }
+                this._registerClick(currentNode, evalu);
+            }
+            // Template out attributes and child nodes
+            this._templateAttributes(currentNode, newData);
+            for (let j = 0; j < tempNode.childNodes.length; j++) {
+                currentNode.appendChild(this._template(tempNode.childNodes[j], newData));
+            }
+        }
+        return dolly;
     }
+
     /**
-     * Templates text based on a mustashe style template. Everything inside the template is evalued and sub-in.
-     * @param child: child node to clone
+     * Clone/multiply nodes with conditionals and repeats thrown in.
+     * @param tempNode: template node used for templating
+     * @param data: data to use when multiplying and running conditionals
+     * @return {DocumentFragment}: the newly cloned node(s)
+     * @private
+     */
+    _templateCloneMultiply(tempNode, data) {
+        let match = null;
+        let repeatAttr = null;
+
+        let fragment = document.createDocumentFragment();
+        // First process repeats in order to get a set of
+        if ((repeatAttr = tempNode.getAttribute("repeat")) != null && (match = repeatAttr.match(this.FOR_REGEX))) {
+            let itemList = this._templateEvaluation(match[2], data);
+            // Loop over all elements in the repeat list, clone the template node, and add clones to list
+            for (let key in itemList) {
+                // Subset data and assign it to the node
+                let newData = Object.assign({}, data);
+                newData[match[1]] = itemList[key];
+                // Short circuit if any "if" clause would fail
+                if (tempNode.hasAttribute("if") &&
+                    !this._templateEvaluation(tempNode.getAttribute("if"), newData)) {
+                    continue;
+                }
+                // Clone the node and add it to the result set
+                let newNode = tempNode.cloneNode(false);
+                fragment.appendChild(newNode);
+                newNode.setAttribute("data-name", match[1]);
+                newNode.setAttribute("data-eval", match[2]+"["+ key + "]");
+            }
+        }
+        // Otherwise add a single clone of the template given the "if" evaluates properly
+        else if (!tempNode.hasAttribute("if") ||
+                 this._templateEvaluation(tempNode.getAttribute("if"), data)) {
+            let newNode = tempNode.cloneNode(false);
+            fragment.appendChild(newNode);
+        }
+        return fragment;
+    }
+
+    /**
+     * Evaluate the expression inside a mustache teamplate producing a JS result.
+     * @param matchedText: text matched inside template
+     * @param data: data to put into the evaluation
+     * @return {result of evaluation|null}
+     * @private
+     */
+    _templateEvaluation(matchedText, data) {
+        try {
+            let argList = Object.keys(data); // Get all keys from data and prepare them as names
+            argList.push("return " + matchedText + ";"); // Add an argument that is the evaluation itself
+            let jsEval = Function.apply(null, argList); // Create the function object
+            return jsEval.apply(null, Object.values(data)); // Run the evaluation, and produce result
+        } catch (err) {
+            console.error("Invalid template evaluation: '" + matchedText + "' caused error: " + err);
+        }
+        return null;
+    }
+
+    /**
+     * Takes a string and replaces the mustache templates within it to the equivalent evaluation of the expression
+     * inside the template. For example: "owl-{{ 1 }}" becomes "owl-1" and "owl-{{ 15.toString(16) }}" becomes owl-f.
+     * @param text: text searched for the replacement.
      * @param data: used as template arguments
      * @private
      */
-    _templateText(child, data) {
-        let content = child.textContent;
-        let match = child.textContent.match(this.mustreg);
+    _templateText(text, data) {
+        let match = text.match(this.TPL_REGEX);
         if (match != null) {
-            let argList = Object.keys(data);
-            argList.push("return " + match[1] + ";");
-            let jsEval = Function.apply(null, argList);
-            let result = jsEval.apply(null, Object.values(data));
-            content = content.replace(this.mustreg, result);
+            let result = this._templateEvaluation(match[1], data);
+            if (result != null) {
+                text = text.replace(this.TPL_REGEX, result);
+            }
         }
-        let clone = child.cloneNode(false);
-        clone.textContent = content;
-        return clone;
+        return text;
+    }
+
+    /**
+     * Loops over a node's attributes and replaces all the mustache templates with the evaluation of the inside of that
+     * template. This only updates attributes, it does not clone nor creat new nodes.
+     * @param newNode: node to update attributes
+     * @param data: data to use when templating
+     * @private
+     */
+    _templateAttributes(newNode, data) {
+        // Loop over all attributes, only if an attribute exists
+        if (newNode.hasAttributes()) {
+            for (let i = 0; i < newNode.attributes.length; i++) {
+                newNode.setAttribute(newNode.attributes[i].name, this._templateText(newNode.attributes[i].value, data));
+            }
+        }
+        return newNode;
     }
 }
