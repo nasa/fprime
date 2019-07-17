9e69f7dde872a6568e203ba2a728a41a5275d799
Jordan Ishii <jordan.ishii@jpl.nasa.gov>
2019-07-17 15:43:24 -0700

    Removed Unnecessary Import from codegen

--- Autocoders/Python/bin/testgen.py
+++ Autocoders/Python/bin/testgen.py
@@ -39,7 +39,6 @@ from fprime_ac.parsers import XmlParser
 from fprime_ac.parsers import XmlComponentParser
 from fprime_ac.parsers import XmlPortsParser
 from fprime_ac.parsers import XmlSerializeParser
-from fprime_ac.parsers import XmlTopologyParser
 
 from lxml import etree
 
