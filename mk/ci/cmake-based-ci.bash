3fc3de1d842e85b746223b48651ea55e11adc086
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2019-11-06 16:01:48 -0800

    mstarch: removing broken CI UT failures

--- mk/ci/cmake-based-ci.bash
+++ mk/ci/cmake-based-ci.bash
@@ -28,47 +28,6 @@ then
     echo "[ERROR] Failed to run 'Ref' I&T tests"
     exit 2
 fi
-# Prep for standard runs
-FPRIME_DIR=`pwd`
-FPRIME_DEP="${FPRIME_DIR}/Ref"
-
-let JOBS="${JOBS:-$(( ( RANDOM % 100 )  + 1 ))}"
-# Generate build for all UTs
-DIR=$(mktemp -d)
-(
-    cd "${DIR}"
-    echo "[INFO] Generating basic CMake dir for running all UTs at: ${DIR}"
-    cmake "${FPRIME_DEP}" -DCMAKE_BUILD_TYPE=Testing > "${CI_LOG_DIR}/cmake_all_ut_build.out.log" 2> "${CI_LOG_DIR}/cmake_all_ut_build.err.log"
-)
-if (( $? != 0 ))
-then
-   echo "[ERROR] Failed to run CMake for unit tests"
-   exit 8
-fi
-
-# Read the previous build and run each target individually
-(
-
-  OWD="$(pwd)"
-  let RET=0
-  cd ${DIR}
-  TARGETS="$(make help | sed -n 's/\.\.\. \(.*_exe\)/\1/p')"
-  rm -r "${DIR}"
-  cd "${OWD}"
-  for target in ${TARGETS}
-  do
-    let JOBS="${JOBS:-$(( ( RANDOM % 100 )  + 1 ))}"
-    PREFIX="ut-${target}"
-    ${DIRNAME}/scripts/cmake-ut.bash "${FPRIME_DIR}" "${target}"
-    if (( $? != 0 ))
-    then
-        let RET=5
-        echo "[ERROR] Failed to run individual UT for (-j${JOBS}): ${target}"
-    fi
-  done
-  exit ${RET}
-) || exit $?
-
 ${DIRNAME}/scripts/cmake-cmaketest.bash
 if (( $? != 0 ))
 then
