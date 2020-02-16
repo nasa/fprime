17d4c5e072fc28b47549f5f739471ada165dc95f
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2020-02-15 23:03:16 -0800

    mstarch: initial I2C driver

---
+++ Drv/LinuxI2cDriver/mod.mk
@@ -0,0 +1,12 @@
+#
+#   Copyright 2004-2008, by the California Institute of Technology.
+#   ALL RIGHTS RESERVED. United States Government Sponsorship
+#   acknowledged.
+#
+#
+
+SRC = LinuxI2cDriverComponentAi.xml LinuxI2cDriverComponentImpl.cpp
+
+HDR = LinuxI2cDriverComponentImpl.hpp
+
+SUBDIRS = test
