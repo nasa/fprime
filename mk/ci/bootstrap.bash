32825108d76fffb11cc234815d2c9764c506f80a
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2020-02-26 16:17:31 -0800

    mstarch: locking out git clean -xdf in ci tests

--- mk/ci/bootstrap.bash
+++ mk/ci/bootstrap.bash
@@ -9,6 +9,9 @@
 ####
 # Clean the repository as part of setup
 echo -e "${BLUE}Cleaning Repository and Installing PIP Package${NOCOLOR}"
-git clean -xdf .
+if [[ "${CI_CLEAN_REPO}" == "NO_REALLY_I_WANT_TO_NUKE_ALL_YOUR_BASE" ]]
+then
+    git clean -xdf . 1>/dev/null 2>/dev/null || fail_and_stop "Failed to clean git repository before testing"
+fi
 pip install ./Fw/Python 1>/dev/null 2>/dev/null || fail_and_stop "Failed to install fprime PIP module from ./Fw/Python"
 pip install ./Gds[test-api-xls] 1>/dev/null 2>/dev/null || fail_and_stop "Failed to install fprime PIP module from ./Gds[test-api-xls]"
