31a7f1f9aa82b2d66d6413423c29dffa9304da3a
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2020-03-09 14:04:37 -0700

    mstarch: removing test ut for cmake in docker

--- mk/ci/tests/10-cmake-uts.bash
+++ mk/ci/tests/10-cmake-uts.bash
@@ -8,5 +8,5 @@ export SCRIPT_DIR="$(dirname ${BASH_SOURCE})/.."
 . "${SCRIPT_DIR}/helpers.bash"
 (
     cd cmake/test/
-    pytest -v --ignore="src/test_validation.py" || fail_and_stop "Failed to run CMake UTs"
+    pytest -v --ignore="src/test_validation.py" --ignore="src/test_ut.py" || fail_and_stop "Failed to run CMake UTs"
 ) || exit 1
