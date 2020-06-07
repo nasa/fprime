d5f608ab9bc20e620d2a625c7d9aa385bbfef3e3
Timothy Canham <timothy.canham@jpl.nasa.gov>
2020-06-06 22:12:32 -0700

    Copied over from Sabertooth

--- Drv/SocketIpDriver/SocketIpDriverComponentImpl.cpp
+++ Drv/SocketIpDriver/SocketIpDriverComponentImpl.cpp
@@ -9,24 +9,6 @@
 // acknowledged.
 //
 // ======================================================================
-// Includes for the IP layer
-#ifdef TGT_OS_TYPE_VXWORKS
-    #include <hostLib.h>
-    #include <ioLib.h>
-    #include <vxWorks.h>
-    #include <sockLib.h>
-    #include <fioLib.h>
-    #include <taskLib.h>
-    #include <sysLib.h>
-    #include <errnoLib.h>
-#elif defined TGT_OS_TYPE_LINUX || TGT_OS_TYPE_DARWIN || TGT_OS_TYPE_RTEMS
-    #include <sys/socket.h>
-    #include <netdb.h>
-    #include <unistd.h>
-    #include <errno.h>
-#else
-    #error OS not supported for IP Socket Communications
-#endif
 
 #include <Drv/SocketIpDriver/SocketIpDriverComponentImpl.hpp>
 #include <Drv/SocketIpDriver/SocketIpDriverCfg.hpp>
@@ -35,10 +17,6 @@
 #include <Fw/Types/Assert.hpp>
 #include <Fw/Types/EightyCharString.hpp>
 #include <Os/Log.hpp>
-
-extern "C" {
-    #include <stdio.h>
-};
 
 namespace Drv {
 
@@ -51,11 +29,8 @@ namespace Drv {
   SocketIpDriverComponentImpl ::
     SocketIpDriverComponentImpl(
 #if FW_OBJECT_NAMES == 1
-        const char *const compName,
+        const char *const compName
 #endif
-        bool send_udp,
-        U32 timeout_seconds,
-        U32 timeout_microseconds
     ) :
 #if FW_OBJECT_NAMES == 1
       SocketIpDriverComponentBase(compName)
@@ -64,15 +39,7 @@ namespace Drv {
 #endif
     ,
     m_buffer(0xbeef, 0xbeef, reinterpret_cast<U64>(m_backing_data), sizeof(m_buffer)),
-    m_socketInFd(-1),
-    m_socketOutFd(-1),
-    m_hostname(""),
-    m_port(0),
-    m_stop(false),
-    // Configuration values
-    m_send_udp(send_udp),
-    m_timeout_seconds(timeout_seconds),
-    m_timeout_microseconds(timeout_microseconds)
+    m_stop(false)
   { }
 
   void SocketIpDriverComponentImpl ::
@@ -87,159 +54,53 @@ namespace Drv {
     ~SocketIpDriverComponentImpl(void)
   {}
 
-  SocketIpDriverComponentImpl::SocketIpStatus SocketIpDriverComponentImpl :: open() {
-      SocketIpDriverComponentImpl::SocketIpStatus status = SocketIpDriverComponentImpl::SUCCESS;
-      // Only the input (TCP) socket needs closing
-      (void) close(m_socketInFd); // Close open sockets, to force a re-open
-      m_socketInFd = -1;
-      // Open a TCP socket for incoming commands, and outgoing data if not using UDP
-      if ((status = openProtocol(SOCK_STREAM)) != SocketIpDriverComponentImpl::SUCCESS) {
-          return status;
-      }
-      // If we need UDP sending, attempt to open UDP
-      if (m_send_udp && m_socketOutFd == -1 && (status = openProtocol(SOCK_DGRAM, false)) != SocketIpDriverComponentImpl::SUCCESS) {
-          (void) close(m_socketInFd);
-          return status;
-      }
-      // Not sending UDP, reuse our input TCP socket
-      else if (!m_send_udp) {
-          m_socketOutFd = m_socketInFd;
-      }
-      // Coding error, if not successful here
-      FW_ASSERT(status == SocketIpDriverComponentImpl::SUCCESS);
-      return status;
+  SocketIpStatus SocketIpDriverComponentImpl :: configure(
+          const char* hostname,
+          U16 port,
+          const bool send_udp,
+          const U32 timeout_seconds,
+          const U32 timeout_microseconds
+          ) {
+
+      return this->m_helper.configure(hostname,port,send_udp,timeout_seconds,timeout_microseconds);
   }
 
-  SocketIpDriverComponentImpl::SocketIpStatus SocketIpDriverComponentImpl :: openProtocol(
-      NATIVE_INT_TYPE protocol,
-      bool isInput
-  ) {
-      NATIVE_INT_TYPE socketFd = -1;
-      struct sockaddr_in address;
-
-      // Clear existing file descriptors
-      if (isInput) {
-          (void) close(m_socketInFd);
-          m_socketInFd = -1;
-      } else {
-          (void) close(m_socketOutFd);
-          m_socketOutFd = -1;
-      }
-      // Acquire a socket, or return error
-      if ((socketFd = socket(AF_INET, protocol, 0)) == -1) {
-          return SocketIpDriverComponentImpl::FAILED_TO_GET_SOCKET;
-      }
-      // Set timeout socket option
-      struct timeval timeout;
-      timeout.tv_sec = m_timeout_seconds;
-      timeout.tv_usec = m_timeout_microseconds;
-      // set socket write to timeout after 1 sec
-      if (setsockopt(socketFd, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout, sizeof(timeout)) < 0) {
-          (void) close(socketFd);
-          return FAILED_TO_SET_SOCKET_OPTIONS;
-      }
-      // Set up the address port and name
-      address.sin_family = AF_INET;
-      address.sin_port = htons(m_port);
-#if defined TGT_OS_TYPE_VXWORKS || TGT_OS_TYPE_DARWIN
-      address.sin_len = static_cast<U8>(sizeof(struct sockaddr_in));
-#endif
-// Get the IP address from host
-#ifdef TGT_OS_TYPE_VXWORKS
-      if ((address.sin_addr.s_addr = hostGetByName(m_hostname)) == -1) {
-          close(socketFd);
-          return FAILED_TO_GET_HOST_IP;
-      }
-#else
-      // Get possible IP addresses
-      struct hostent *host_entry;
-      if ((host_entry = gethostbyname(m_hostname)) == NULL || host_entry->h_addr_list[0] == NULL) {
-          close(socketFd);
-          return FAILED_TO_GET_HOST_IP;
-      }
-      // First IP address to socket sin_addr
-      if (inet_pton(address.sin_family, m_hostname, &(address.sin_addr)) < 0) {
-          close(socketFd);
-          return INVALID_IP_ADDRESS;
-      };
-#endif
-      // If TCP, connect to the socket to allow for communication
-      if (protocol != SOCK_DGRAM && connect(socketFd, reinterpret_cast<struct sockaddr *>(&address),
-              sizeof(address)) < 0) {
-          close(socketFd);
-          return FAILED_TO_CONNECT;
-      }
-      // Store the UDP address for later
-      else if (protocol == SOCK_DGRAM) {
-          FW_ASSERT(sizeof(m_udpAddr) == sizeof(address), sizeof(m_udpAddr), sizeof(address));
-          memcpy(&m_udpAddr, &address, sizeof(m_udpAddr));
-      }
-
-      // Set the member socket variable
-      if (isInput) {
-          m_socketInFd = socketFd;
-      } else {
-          m_socketOutFd = socketFd;
-      }
-      Fw::Logger::logMsg("Connected to %s:%hu for %s using %s\n", reinterpret_cast<POINTER_CAST>(m_hostname), m_port,
-                         reinterpret_cast<POINTER_CAST>(isInput ? "uplink" : "downlink"),
-                         reinterpret_cast<POINTER_CAST>((protocol == SOCK_DGRAM) ? "udp" : "tcp"));
-      return SocketIpDriverComponentImpl::SUCCESS;
-  }
-
-  void SocketIpDriverComponentImpl :: readTask(void* pointer) {
-      FW_ASSERT(pointer != NULL);
-      SocketIpDriverComponentImpl::SocketIpStatus status = SocketIpDriverComponentImpl::SUCCESS;
+  void SocketIpDriverComponentImpl::readTask(void* pointer) {
+      FW_ASSERT(pointer);
+      SocketIpStatus status = SOCK_SUCCESS;
       SocketIpDriverComponentImpl* self = reinterpret_cast<SocketIpDriverComponentImpl*>(pointer);
       do {
-          // Open a network connection, if it has not already been open
-          if (self->m_socketInFd == -1 && self->open() != SocketIpDriverComponentImpl::SUCCESS && not self->m_stop) {
+          // Open a network connection if it has not already been open
+          if (not self->m_helper.isOpened()
+                  and (self->m_helper.open() != SOCK_SUCCESS)
+                  and not self->m_stop) {
               Os::Task::delay(PRE_CONNECTION_RETRY_INTERVAL_MS);
           }
+
           // If the network connection is open, read from it
-          if (self->m_socketInFd != -1) {
-              status = self->receive();
-              if (status != SocketIpDriverComponentImpl::SUCCESS &&
-                  status != SocketIpDriverComponentImpl::INTERRUPTED_TRY_AGAIN) {
-                  (void) close(self->m_socketInFd);
-                  self->m_socketInFd = -1;
+          if (self->m_helper.isOpened()) {
+              BYTE* data = reinterpret_cast<U8*>(self->m_buffer.getdata());
+              I32 size = 0;
+              status = self->m_helper.recv(data,size);
+              if (status != SOCK_SUCCESS &&
+                  status != SOCK_INTERRUPTED_TRY_AGAIN) {
+                  self->m_helper.close();
+              } else {
+                  // Ignore KEEPALIVE data and send out any other data.
+                  if (memcmp(data, KEEPALIVE_CONST,
+                         (size > static_cast<I32>(sizeof(KEEPALIVE_CONST)) - 1) ? sizeof(KEEPALIVE_CONST) -1 : size) != 0) {
+                      self->m_buffer.setsize(size);
+                      self->recv_out(0, self->m_buffer);
+                  }
               }
           }
       }
       // As long as not told to stop, and we are successful interrupted or ordered to retry, keep receiving
-      while(not self->m_stop && (status == SocketIpDriverComponentImpl::SUCCESS ||
-            status == SocketIpDriverComponentImpl::INTERRUPTED_TRY_AGAIN || RECONNECT_AUTOMATICALLY != 0));
+      while(not self->m_stop && (status == SOCK_SUCCESS ||
+            status == SOCK_INTERRUPTED_TRY_AGAIN || RECONNECT_AUTOMATICALLY != 0));
   }
 
-    SocketIpDriverComponentImpl::SocketIpStatus SocketIpDriverComponentImpl :: receive() {
-      U8* data = reinterpret_cast<U8*>(m_buffer.getdata());
-      SocketIpDriverComponentImpl::SocketIpStatus status = SocketIpDriverComponentImpl::SUCCESS;
-      // Attempt to recv out data
-      I32 recd = recv(m_socketInFd, data, MAX_RECV_BUFFER_SIZE, SOCKET_RECV_FLAGS);
-      // Error returned, and it wasn't an interrupt, nor a reset
-      if (recd == -1 && errno != EINTR && errno != ECONNRESET) {
-          Fw::Logger::logMsg("[ERROR] IP recv failed ERRNO: %d\n", errno);
-          status = READ_ERROR; // Stop recv task on error
-      }
-      // Error is EINTR, just try again
-      else if (recd == -1 && errno == EINTR) {
-          status = INTERRUPTED_TRY_AGAIN;
-      }
-      // Zero bytes read means a closed socket
-      else if (recd == 0 || errno == ECONNRESET) {
-          status = READ_DISCONNECTED;
-      }
-      // Ignore KEEPALIVE data and send out any other data.
-      else if (memcmp(data, KEEPALIVE_CONST,
-              (recd > static_cast<I32>(sizeof(KEEPALIVE_CONST)) - 1) ? sizeof(KEEPALIVE_CONST) -1 : recd) != 0) {
-          m_buffer.setsize(recd);
-          // Expected to be a guarded or sync output to ensure processing immediately
-          recv_out(0, m_buffer);
-      }
-      return status;
-  }
-
-  void SocketIpDriverComponentImpl :: startSocketTask(
+  void SocketIpDriverComponentImpl::startSocketTask(
         NATIVE_INT_TYPE priority,
         NATIVE_INT_TYPE stack,
         const char* host,
@@ -250,11 +111,13 @@ namespace Drv {
       Fw::EightyCharString name("IpSocketRead");
       // Do not restart task
       if (not m_recvTask.isStarted()) {
-          m_hostname = host;
-          m_port = port;
+          this->configure(host,port);
           // Start by opening the socket
-          if (m_socketInFd == -1) {
-              open();
+          if (not this->m_helper.isOpened()) {
+              SocketIpStatus stat = this->m_helper.open();
+              if (stat != SOCK_SUCCESS) {
+                  Fw::Logger::logMsg("Unable to open socket: %d\n",stat);
+              }
           }
           Os::Task::TaskStatus stat = m_recvTask.start(name, 0, priority, stack,
                                                        SocketIpDriverComponentImpl::readTask, this, cpuAffinity);
@@ -276,45 +139,8 @@ namespace Drv {
         Fw::Buffer &fwBuffer
     )
   {
-      U32 total = 0;
       U32 size = fwBuffer.getsize();
-      U8* data = reinterpret_cast<U8*>(fwBuffer.getdata());
-      // Prevent transmission before connection, or after a disconnect
-      if (m_socketOutFd == -1) {
-          return;
-      }
-      // Attempt to send out data
-      for (U32 i = 0; i < MAX_SEND_ITERATIONS && total < size; i++) {
-          I32 sent = 0;
-          // Output to send UDP
-          if (m_send_udp) {
-              sent = sendto(m_socketOutFd, data + total, size - total, SOCKET_SEND_FLAGS,
-                            reinterpret_cast<struct sockaddr *>(&m_udpAddr), sizeof(m_udpAddr));
-          }
-          // Output to send TCP
-          else {
-              sent = send(m_socketOutFd, data + total, size - total, SOCKET_SEND_FLAGS);
-          }
-          // Error is EINTR, just try again
-          if (sent == -1 && errno == EINTR) {
-              continue;
-          }
-          // Error bad file descriptor is a close
-          else if (sent == -1 && errno == EBADF) {
-              Fw::Logger::logMsg("[ERROR] Server disconnected\n");
-              (void) close(m_socketOutFd);
-              m_socketOutFd = -1;
-              break;
-          }
-          // Error returned, and it wasn't an interrupt
-          else if (sent == -1 && errno == EINTR) {
-              Fw::Logger::logMsg("[ERROR] IP send failed ERRNO: %d UDP: %d\n", errno, m_send_udp);
-              break;
-          }
-          // Successful write
-          else {
-              total += sent;
-          }
-      }
+      BYTE* data = reinterpret_cast<BYTE*>(fwBuffer.getdata());
+      this->m_helper.send(data,size);
   }
 } // end namespace Svc
--- Drv/SocketIpDriver/mod.mk
+++ Drv/SocketIpDriver/mod.mk
@@ -1,14 +1,13 @@
 #
-#   Copyright 2004-2008, by the California Institute of Technology.
+#   Copyright 2004-2016, by the California Institute of Technology.
 #   ALL RIGHTS RESERVED. United States Government Sponsorship
 #   acknowledged.
 #
 #
 
-SRC = SocketIpDriverComponentAi.xml SocketIpDriverComponentImpl.cpp
-
-HDR = SocketIpDriverComponentImpl.hpp
-
-SUBDIRS = 
-
-
+SRC = 	SocketIpDriverComponentAi.xml \
+		SocketIpDriverComponentImpl.cpp \
+		SocketHelper.cpp
+		
+HDR =	SocketIpDriverComponentImpl.hpp \
+		SocketHelper.hpp
