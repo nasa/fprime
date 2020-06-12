d6107baf25224cdde8a90589ae642782b2117688
Jake Deerin <2016deerinjo@gmail.com>
2020-06-12 16:25:21 -0400

    Refactored CLI commands to have a base class

---
+++ Gds/src/fprime_gds/common/gds_cli/base_commands.py
@@ -0,0 +1,124 @@
+"""
+A collection of base classes used for the backend implementations of the GDS
+CLI commands
+"""
+
+import types
+from typing import List
+
+import fprime_gds.common.gds_cli.filtering_utils as filtering_utils
+import fprime_gds.common.gds_cli.misc_utils as misc_utils
+import fprime_gds.common.gds_cli.test_api_utils as test_api_utils
+
+from fprime_gds.common.pipeline.dictionaries import Dictionaries
+from fprime_gds.common.testing_fw import predicates
+
+
+class BaseCommand:
+    """
+    The base class for implementing a GDS CLI command's functionality, once
+    arguments are parsed and passed into this command.
+    """
+
+    @classmethod
+    def handle_arguments(cls, *args, **kwargs):
+        """
+        Do something to handle the input arguments given
+        """
+        pass
+
+
+class QueryHistoryCommand(BaseCommand):
+    """
+    The base class for a set of related GDS CLI commands that need to query and
+    display received data from telemetry channels, F' events, command histories,
+    or similar functionalities with closely-related interfaces.
+    """
+
+    @classmethod
+    def print_items_list(
+        cls,
+        project_dictionary: Dictionaries,
+        filter_predicate: predicates.predicate,
+        json: bool = False,
+    ):
+        """
+        Gets a sorted list of items from the dictionary, filtered by the given
+        predicate, and prints put their details.
+
+        :param project_dictionary: The dictionary object for the project
+            containing the item type definitions
+        :param filter_predicate: Test API predicate used to filter shown items
+        :param json: Whether to print out each item in JSON format or not
+        """
+        pass
+
+    @classmethod
+    def print_upcoming_item(
+        cls,
+        api,
+        filter_predicate: predicates.predicate,
+        min_start_time="NOW",
+        json: bool = False,
+    ):
+        """
+        Retrieves an F' item that's occurred and prints out its data. Must
+        support "misc_utils.repeat_until_interrupt" usage.
+        """
+        pass
+
+    # TODO: Somehow link this directly to the parser arguments in fprime_cli?
+    # TODO: Just use args/kwargs instead of this massive argument list? But I
+    # kind of do want some coupling with the frontend code to keep these in sync
+    # TODO: Should separate frontend printing code from backend data-getting
+    # (this is basically frontend code right now, which isn't necessarily bad)
+    @classmethod
+    def handle_arguments(
+        cls,
+        dictionary_path: str,
+        ip_address: str,
+        port: int,
+        list: bool,
+        follow: bool,
+        ids: List[int],
+        components: List[str],
+        search: str,
+        json: bool,
+        *args,
+        **kwargs
+    ):
+        """
+        Takes in the given arguments and uses them to print out a formatted
+        string of items the user wants to see from the Test API; if the option
+        is present, repeat doing this until the user exits the program.
+
+        For descriptions of these arguments, and more function details, see:
+            Gds/src/fprime_gds/executables/fprime_cli.py
+        """
+        # ======================================================================
+        pipeline, api = test_api_utils.initialize_test_api(
+            dictionary_path, server_ip=ip_address, server_port=port
+        )
+        # ======================================================================
+
+        filter_predicate = filtering_utils.get_full_filter_predicate(
+            ids, components, search
+        )
+
+        # TODO: Try and refactor this to avoid nested if statements?
+        if list:
+            cls.print_items_list(pipeline.dictionaries, filter_predicate, json)
+        else:
+            if follow:
+                misc_utils.repeat_until_interrupt(
+                    cls.print_upcoming_item, api, filter_predicate, "NOW", json
+                )
+            else:
+                cls.print_upcoming_item(api, filter_predicate, json=json)
+
+        # TODO: Disable Test API from also logging to console somehow?
+
+        # ======================================================================
+        pipeline.disconnect()
+        api.teardown()
+        # ======================================================================
--- Gds/src/fprime_gds/common/gds_cli/events.py
+++ Gds/src/fprime_gds/common/gds_cli/events.py
@@ -6,6 +6,7 @@ import json
 import types
 from typing import List
 
+from fprime_gds.common.gds_cli.base_commands import QueryHistoryCommand
 import fprime_gds.common.gds_cli.filtering_utils as filtering_utils
 import fprime_gds.common.gds_cli.misc_utils as misc_utils
 import fprime_gds.common.gds_cli.test_api_utils as test_api_utils
@@ -16,6 +17,7 @@ from fprime_gds.common.testing_fw import predicates
 
 
 # TODO: Need to do user tests to find a better print format
+# TODO: Just put inside the below class?
 def get_event_string(event, as_json: bool = False) -> str:
     """
     Takes in the given event object and prints out a human-readable string
@@ -30,71 +32,56 @@ def get_event_string(event, as_json: bool = False) -> str:
     return misc_utils.get_item_string(event)
 
 
-def print_events_list(
-    project_dictionary: Dictionaries,
-    filter_predicate: predicates.predicate,
-    json: bool = False,
-):
-    """
-    Gets a list of possible events that can occur in the system and prints their
-    details out in an ID-sorted list
-
-    :param project_dictionary: The dictionary object for the project containing
-        the event type definitions
-    :param filter_predicate: Test API predicate used to filter shown events
-    :param json: Whether to print out each item in JSON format or not
-    """
-    event_list = test_api_utils.get_item_list(
-        project_dictionary.event_id, filter_predicate, EventData.get_empty_obj
-    )
-    for event in event_list:
-        print(get_event_string(event, json))
-
-
-def print_upcoming_event(
-    api,
-    filter_predicate: predicates.predicate,
-    min_start_time="NOW",
-    json: bool = False,
-):
-    """
-    Prints out the next upcoming event information after the given time that
-    matches the given filter (in a way usable by the "repeat_until_interrupt"
-    function)
-    """
-    event_object = test_api_utils.get_upcoming_event(
-        api, filter_predicate, min_start_time
-    )
-    print(get_event_string(event_object, json))
-    # Update time so we catch all events since the last one
-    if event_object:
-        min_start_time = predicates.greater_than(event_object.get_time())
-    return (api, filter_predicate, min_start_time, json)
-
-
-# TODO: Should separate frontend printing code from backend data-getting
-# ...at the same time, might be a good idea to keep this strongly coupled to the
-# CLI interface code so they stay in sync? (This is essentially frontend code
-# right now, but might not have sufficient separation of concerns)
-def get_events_output(
-    dictionary_path: str,
-    ip_address: str,
-    port: int,
-    list: bool,
-    follow: bool,
-    ids: List[int],
-    components: List[str],
-    search: str,
-    json: bool,
-):
+class EventsCommand(QueryHistoryCommand):
     """
     Takes in the given arguments and prints an appropriate formatted string of
     recent event data that matches the user's criteria.
 
-    For descriptions of these arguments, and more function details, see:
-    Gds/src/fprime_gds/executables/fprime_cli.py
+    For descriptions of these arguments, and more function details, see the
+    events command definition at:
+        Gds/src/fprime_gds/executables/fprime_cli.py
     """
-    args = misc_utils.QueryCommandArgs(
-        dictionary_path, ip_address, port, list, follow, ids, components, search, json
-    )
-    misc_utils.query_command_template(print_events_list, print_upcoming_event, args)
+
+    @classmethod
+    def print_items_list(
+        cls,
+        project_dictionary: Dictionaries,
+        filter_predicate: predicates.predicate,
+        json: bool = False,
+    ):
+        """
+        Gets a list of possible events that can occur in the system and prints
+        their details out in an ID-sorted list.
+
+        :param project_dictionary: The dictionary object for the project
+            containing the event type definitions
+        :param filter_predicate: Test API predicate used to filter shown events
+        :param json: Whether to print out each item in JSON format or not
+        """
+        event_list = test_api_utils.get_item_list(
+            project_dictionary.event_id, filter_predicate, EventData.get_empty_obj
+        )
+        for event in event_list:
+            print(get_event_string(event, json))
+
+    @classmethod
+    def print_upcoming_item(
+        cls,
+        api,
+        filter_predicate: predicates.predicate,
+        min_start_time="NOW",
+        json: bool = False,
+    ):
+        """
+        Prints out the next upcoming event information after the given time
+        that matches the given filter (in a way usable by the
+        "repeat_until_interrupt" function)
+        """
+        event_object = test_api_utils.get_upcoming_event(
+            api, filter_predicate, min_start_time
+        )
+        print(get_event_string(event_object, json))
+        # Update time so we catch all events since the last one
+        if event_object:
+            min_start_time = predicates.greater_than(event_object.get_time())
+        return (api, filter_predicate, min_start_time, json)
