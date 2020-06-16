a64b8b2fc67674e3b84d04f499606a35df73902e
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2020-06-16 13:27:05 -0700

    mstarch: formatting cmake.py

--- Fw/Python/src/fprime/fbuild/cmake.py
+++ Fw/Python/src/fprime/fbuild/cmake.py
@@ -34,6 +34,7 @@ class CMakeBuildCache(object):
     Builds CMake deployment for the purposes of inspecting that build. This exists because generating a build on every
     call take a long time. This cache will hold the results to prevent recalculation.
     """
+
     def __init__(self):
         """ Sets up the known project to None """
         self.project = None
@@ -41,17 +42,25 @@ class CMakeBuildCache(object):
 
     def get_cmake_temp_build(self, proj_dir, verbose=False):
         """ Gets a CMake build directory for the specified proj_dir """
-        if self.project is not None and proj_dir is not None and self.project != proj_dir:
+        if (
+            self.project is not None
+            and proj_dir is not None
+            and self.project != proj_dir
+        ):
             raise CMakeException("Already tracking project {}".format(self.project))
         # No tempdir, prepare a build
         if self.tempdir is None:
             if proj_dir is None:
-                raise ValueError("No build cache available, and no project_dir specified")
+                raise ValueError(
+                    "No build cache available, and no project_dir specified"
+                )
             # Create a temp directory and register its deletion at the end of the program run
             self.tempdir = tempfile.mkdtemp()
             atexit.register(lambda: shutil.rmtree(self.tempdir, ignore_errors=True))
             # Turn that directory into a CMake build
-            fprime.fbuild.builder().generate_build(proj_dir, self.tempdir, ignore_output=not verbose)
+            fprime.fbuild.builder().generate_build(
+                proj_dir, self.tempdir, ignore_output=not verbose
+            )
         self.project = proj_dir
         return self.tempdir
 
@@ -60,8 +69,13 @@ class CMakeHandler(object):
     """
     CMake handler interacts with an F prime CMake-based system. This will help us interact with CMake in refined ways.
     """
+
     CMAKE_DEFAULT_BUILD_NAME = "build-fprime-automatic-{}"
-    CMAKE_LOCATION_FIELDS = ["FPRIME_PROJECT_ROOT", "FPRIME_LIBRARIES", "FPRIME_FRAMEWORK_PATH"]
+    CMAKE_LOCATION_FIELDS = [
+        "FPRIME_PROJECT_ROOT",
+        "FPRIME_LIBRARIES",
+        "FPRIME_FRAMEWORK_PATH",
+    ]
 
     def __init__(self):
         """
@@ -73,13 +87,17 @@ class CMakeHandler(object):
         try:
             self._run_cmake(["--help"], print_output=False)
         except Exception as exc:
-            raise CMakeExecutionException("CMake executable 'cmake' not found", str(exc), printed=False)
+            raise CMakeExecutionException(
+                "CMake executable 'cmake' not found", str(exc), printed=False
+            )
 
     def set_verbose(self, verbose):
         """ Sets verbosity """
         self.verbose = verbose
 
-    def execute_known_target(self, target, build_dir, path, cmake_args=None, make_args=None, top_target=False):
+    def execute_known_target(
+        self, target, build_dir, path, cmake_args=None, make_args=None, top_target=False
+    ):
         """
         Executes a known target for a given build_dir. Path will default to a known path.
         :param build_dir: build_dir to use to run this.
@@ -91,29 +109,53 @@ class CMakeHandler(object):
         """
         cmake_args = {} if cmake_args is None else cmake_args
         make_args = {} if make_args is None else make_args
-        fleshed_args = list(map(lambda key: ("{}={}" if key.startswith("--") else "-D{}={}")
-                           .format(key, cmake_args[key]), cmake_args.keys()))
-        fleshed_args += ["--"] + list(map(lambda key: "{}={}".format(key, make_args[key]), make_args.keys()))
+        fleshed_args = list(
+            map(
+                lambda key: ("{}={}" if key.startswith("--") else "-D{}={}").format(
+                    key, cmake_args[key]
+                ),
+                cmake_args.keys(),
+            )
+        )
+        fleshed_args += ["--"] + list(
+            map(lambda key: "{}={}".format(key, make_args[key]), make_args.keys())
+        )
         # Get module name from the relative path to include root
         include_root = self.get_include_info(path, build_dir)[1]
-        module = os.path.relpath(path,  include_root).replace(".", "").replace(os.sep, "_")
-        cmake_target = module if target == "" else \
-            ("{}_{}".format(module, target).lstrip("_") if not top_target else target)
+        module = (
+            os.path.relpath(path, include_root).replace(".", "").replace(os.sep, "_")
+        )
+        cmake_target = (
+            module
+            if target == ""
+            else (
+                "{}_{}".format(module, target).lstrip("_") if not top_target else target
+            )
+        )
         run_args = ["--build", build_dir]
         environment = {}
         if self.verbose:
             environment["VERBOSE"] = "1"
         run_args.extend(["--target", cmake_target])
         try:
-            return self._run_cmake(run_args + fleshed_args, write_override=True, environment=environment)
+            return self._run_cmake(
+                run_args + fleshed_args, write_override=True, environment=environment
+            )
         except CMakeExecutionException as exc:
-            no_target = functools.reduce(lambda cur, line: cur or "No rule to make target" in line,
-                                         exc.get_errors(), False)
+            no_target = functools.reduce(
+                lambda cur, line: cur or "No rule to make target" in line,
+                exc.get_errors(),
+                False,
+            )
             if not no_target:
                 raise
-            print("[CMAKE] CMake failed to detect target, attempting CMake cache refresh and retry")
+            print(
+                "[CMAKE] CMake failed to detect target, attempting CMake cache refresh and retry"
+            )
             self._cmake_referesh_cache(build_dir)
-            return self._run_cmake(run_args + fleshed_args, write_override=True, environment=environment)
+            return self._run_cmake(
+                run_args + fleshed_args, write_override=True, environment=environment
+            )
 
     def find_hashed_file(self, build_dir, hash):
         """
@@ -124,9 +166,13 @@ class CMakeHandler(object):
         """
         hashes_file = os.path.join(build_dir, "hashes.txt")
         if not os.path.exists(hashes_file):
-            raise CMakeException("Failed to find {}, was the build generated.".format(hashes_file))
+            raise CMakeException(
+                "Failed to find {}, was the build generated.".format(hashes_file)
+            )
         with open(hashes_file, "r") as file_handle:
-            lines = filter(lambda line: "{:x}".format(hash) in line, file_handle.readlines())
+            lines = filter(
+                lambda line: "{:x}".format(hash) in line, file_handle.readlines()
+            )
         return list(lines)
 
     def find_nearest_standard_build(self, platform, path):
@@ -140,15 +186,20 @@ class CMakeHandler(object):
         # Get current directory to be checked, by removing file if path is not already a directory
         current = path if os.path.isdir(path) else os.path.dirname(path)
         # Look for a potential build that is valid
-        potential_build = os.path.join(current, CMakeHandler.CMAKE_DEFAULT_BUILD_NAME.format(platform))
+        potential_build = os.path.join(
+            current, CMakeHandler.CMAKE_DEFAULT_BUILD_NAME.format(platform)
+        )
         if os.path.exists(potential_build):
             self._cmake_validate_build_dir(potential_build)
             return potential_build
         # Check for root, and throw error if it is already a root
         new_dir = os.path.dirname(current)
         if new_dir == path:
-            raise CMakeException("{} not in ancestor tree"
-                                 .format(CMakeHandler.CMAKE_DEFAULT_BUILD_NAME.format(platform)))
+            raise CMakeException(
+                "{} not in ancestor tree".format(
+                    CMakeHandler.CMAKE_DEFAULT_BUILD_NAME.format(platform)
+                )
+            )
         return self.find_nearest_standard_build(platform, new_dir)
 
     def get_include_locations(self, cmake_dir):
@@ -161,7 +212,9 @@ class CMakeHandler(object):
         """
         # Reading config fields. If the cmake_dir is a project dir, a build cache may be setup.
         # !! Note: using a project dir will cause file-system side effects, and incur a one-time cost to setup cache !!
-        config_fields = self.get_fprime_configuration(CMakeHandler.CMAKE_LOCATION_FIELDS, cmake_dir)
+        config_fields = self.get_fprime_configuration(
+            CMakeHandler.CMAKE_LOCATION_FIELDS, cmake_dir
+        )
         non_null = filter(lambda item: item is not None, config_fields)
         # Read cache fields for each possible directory the build_dir, and the new tempdir
         locations = itertools.chain(*map(lambda value: value.split(";"), non_null))
@@ -182,17 +235,25 @@ class CMakeHandler(object):
         :return: (relative include path, include root for the given path)
         !!! Note: supplying a project dir as cmake_dir will setup a build-cache, and thus take much time. !!!
         """
-        path = os.path.abspath(path) if path is not None else os.path.abspath(os.getcwd())
+        path = (
+            os.path.abspath(path) if path is not None else os.path.abspath(os.getcwd())
+        )
         possible_parents = self.get_include_locations(cmake_dir)
         # Check there is some possible parent
         if not possible_parents:
-            raise CMakeProjectException(cmake_dir, "Does not define any of the cache fields: {}"
-                                        .format(",".join(CMakeHandler.CMAKE_LOCATION_FIELDS)))
+            raise CMakeProjectException(
+                cmake_dir,
+                "Does not define any of the cache fields: {}".format(
+                    ",".join(CMakeHandler.CMAKE_LOCATION_FIELDS)
+                ),
+            )
         # Common-prefix will fail in the case that two directories share a common prefix in their name
         # So force directory separator to force common prefix to only work on full directory names
         full_parents = map(lambda dir: os.path.abspath(dir) + os.sep, possible_parents)
         # Parents *are* the common prefix for their children
-        parents = filter(lambda parent: os.path.commonprefix([parent, path]) == parent, full_parents)
+        parents = filter(
+            lambda parent: os.path.commonprefix([parent, path]) == parent, full_parents
+        )
 
         def parent_reducer(accum, item):
             """
@@ -208,7 +269,12 @@ class CMakeHandler(object):
             # Calculate common path
             prefix = os.path.commonprefix([item, path])
             # Return the previous parent, if new prefix is not the directory itself, or the old one was closer
-            return accum if prefix != item or (accum is not None and len(accum) > len(item)) else item
+            return (
+                accum
+                if prefix != item or (accum is not None and len(accum) > len(item))
+                else item
+            )
+
         nearest_parent = functools.reduce(parent_reducer, parents, None)
         # Check that a parent is the true parent
         if nearest_parent is None:
@@ -255,11 +321,19 @@ class CMakeHandler(object):
         # We will CD for build, so this path must become absolute
         source_dir = os.path.abspath(source_dir)
         args = {} if args is None else args
-        fleshed_args = map(lambda key: ("{}={}" if key.startswith("--") else "-D{}={}")
-                           .format(key, args[key]), args.keys())
+        fleshed_args = map(
+            lambda key: ("{}={}" if key.startswith("--") else "-D{}={}").format(
+                key, args[key]
+            ),
+            args.keys(),
+        )
         self._cmake_validate_source_dir(source_dir)
-        self._run_cmake(["-S", source_dir] + list(fleshed_args), workdir=build_dir, print_output=not ignore_output,
-                        write_override=True)
+        self._run_cmake(
+            ["-S", source_dir] + list(fleshed_args),
+            workdir=build_dir,
+            print_output=not ignore_output,
+            write_override=True,
+        )
 
     def _read_values_from_cache(self, keys, build_dir):
         """
@@ -291,7 +365,10 @@ class CMakeHandler(object):
 
     def get_toolchain_config(self):
         """Returns the default toolchain"""
-        return self.settings.get("default_toolchain", "native"), self.settings.get("toolchain_locations", [])
+        return (
+            self.settings.get("default_toolchain", "native"),
+            self.settings.get("toolchain_locations", []),
+        )
 
     def load_settings(self, settings_file, cmake_dir):
         """
@@ -299,7 +376,9 @@ class CMakeHandler(object):
         """
         # Non-generate targets can read the settings file location directly from the build-cache
         if settings_file is None:
-            settings_file = self.get_fprime_configuration(fprime.fbuild.settings.IniSettings.SET_ENV, cmake_dir)
+            settings_file = self.get_fprime_configuration(
+                fprime.fbuild.settings.IniSettings.SET_ENV, cmake_dir
+            )
         self.settings = fprime.fbuild.settings.IniSettings.load(settings_file)
 
     @staticmethod
@@ -314,9 +393,13 @@ class CMakeHandler(object):
             raise CMakeProjectException(source_dir, "No CMakeLists.txt is defined")
         # Test the cmake_file for project(
         with open(cmake_file, "r") as file_handle:
-            project_lines = list(filter(lambda line: "project(" in line, file_handle.readlines()))
+            project_lines = list(
+                filter(lambda line: "project(" in line, file_handle.readlines())
+            )
             if not project_lines:
-                raise CMakeProjectException(source_dir, "No 'project()' calls in {}".format(cmake_file))
+                raise CMakeProjectException(
+                    source_dir, "No 'project()' calls in {}".format(cmake_file)
+                )
 
     @staticmethod
     def _cmake_validate_build_dir(build_dir):
@@ -342,7 +425,14 @@ class CMakeHandler(object):
         run_args.extend(["--target", "rebuild_cache"])
         self._run_cmake(run_args, write_override=True, environment=environment)
 
-    def _run_cmake(self, arguments, workdir=None, print_output=True, write_override=False, environment={}):
+    def _run_cmake(
+        self,
+        arguments,
+        workdir=None,
+        print_output=True,
+        write_override=False,
+        environment={},
+    ):
         """
         Will run the cmake system supplying the given arguments. Assumes that the CMake executable is somewhere on the
         path in order for this to run.
@@ -371,17 +461,31 @@ class CMakeHandler(object):
         # side. Most importantly, pseudo-terminal usage will trick CMake into highlighting for us.
         pty_err_r, pty_err_w = pty.openpty()
         pty_out_r, pty_out_w = pty.openpty()
-        proc = subprocess.Popen(cargs, stdout=pty_out_w, stderr=pty_err_w, cwd=workdir, env=cm_environ, close_fds=True)
+        proc = subprocess.Popen(
+            cargs,
+            stdout=pty_out_w,
+            stderr=pty_err_w,
+            cwd=workdir,
+            env=cm_environ,
+            close_fds=True,
+        )
         # Close our side of the writing pipes. Subproc will also close when it it finished.
         os.close(pty_out_w)
         os.close(pty_err_w)
-        ret, stdout, stderr = self._communicate(proc, io.open(pty_out_r, mode="rb"), io.open(pty_err_r, mode="rb"),
-                                                print_output)
+        ret, stdout, stderr = self._communicate(
+            proc,
+            io.open(pty_out_r, mode="rb"),
+            io.open(pty_err_r, mode="rb"),
+            print_output,
+        )
         # Raising an exception when the return code is non-zero allows us to handle the exception internally if it is
         # needed. Thus we do not just exit.
         if ret != 0:
-            raise CMakeExecutionException("CMake erred with return code {}".format(proc.returncode),
-                                          stderr, print_output)
+            raise CMakeExecutionException(
+                "CMake erred with return code {}".format(proc.returncode),
+                stderr,
+                print_output,
+            )
         return stdout, stderr
 
     @staticmethod
@@ -439,43 +543,57 @@ class CMakeHandler(object):
 
 class CMakeException(Exception):
     """ Error occurred within this CMake package """
+
     pass
 
 
 class CMakeInconsistencyException(CMakeException):
     """ Project CMakeLists.txt is inconsistent with build dir """
+
     def __init__(self, project_cmake, build_dir):
         """ Force an appropriate message """
-        super(CMakeInconsistencyException, self).__init__("{} is inconsistent with build {}. Regenerate the build"
-                                                          .format(project_cmake, build_dir))
+        super(CMakeInconsistencyException, self).__init__(
+            "{} is inconsistent with build {}. Regenerate the build".format(
+                project_cmake, build_dir
+            )
+        )
 
 
 class CMakeOrphanException(CMakeException):
     """ File is not managed by CMake project """
+
     def __init__(self, file_dir):
         """ Force an appropriate message """
-        super(CMakeOrphanException, self).__init__("{} is outside the F prime project".format(file_dir))
+        super(CMakeOrphanException, self).__init__(
+            "{} is outside the F prime project".format(file_dir)
+        )
 
 
 class CMakeProjectException(CMakeException):
     """ Invalid project directory """
+
     def __init__(self, project_dir, error):
         """ Force an appropriate message """
-        super(CMakeProjectException, self)\
-            .__init__("{} is an invalid F prime deployment. {}".format(project_dir, error))
+        super(CMakeProjectException, self).__init__(
+            "{} is an invalid F prime deployment. {}".format(project_dir, error)
+        )
 
 
 class CMakeInvalidBuildException(CMakeException):
     """ The supplied build directory was not setup as a CMake value """
+
     def __init__(self, build_dir):
         """ Force an appropriate message """
-        super(CMakeInvalidBuildException, self)\
-            .__init__("{} is not a CMake build directory. Please setup using 'fprime-util generate'"
-                      .format(build_dir))
+        super(CMakeInvalidBuildException, self).__init__(
+            "{} is not a CMake build directory. Please setup using 'fprime-util generate'".format(
+                build_dir
+            )
+        )
 
 
 class CMakeExecutionException(CMakeException):
     """ Pass up a CMake Error as an Exception """
+
     def __init__(self, message, stderr, printed):
         """ The error data should be stored """
         super(CMakeExecutionException, self).__init__(message)
@@ -496,8 +614,12 @@ class CMakeExecutionException(CMakeException):
         """
         return self.need
 
+
 class CMakeNoSuchTargetException(CMakeException):
     """ The target does not exist. """
+
     def __init__(self, build_dir, target):
         """  Better messaging for this exception """
-        super(CMakeNoSuchTargetException, self).__init__("{} does not support target {}".format(build_dir, target))
+        super(CMakeNoSuchTargetException, self).__init__(
+            "{} does not support target {}".format(build_dir, target)
+        )
