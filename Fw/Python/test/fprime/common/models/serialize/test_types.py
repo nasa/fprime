22d9ea06d573eec61678fec5ef84f911abdd64fe
hunterpaulson <hwp1113@gmail.com>
2020-06-25 15:21:33 -0600

    hpaulson: adds serialization pytest for all types

--- Fw/Python/test/fprime/common/models/serialize/test_types.py
+++ Fw/Python/test/fprime/common/models/serialize/test_types.py
@@ -1,9 +1,15 @@
 """
-Created on Jun 23, 2020
-@author: tcanham, reder, hpaulson
+(test) bool_type.py
+
+Tests the serialization and deserialization of all the types in Fw/Python/src/fprime/common/models/serialize/
+
+Created on Jun 25, 2020
+@author: hpaulson
 """
-from __future__ import print_function
+
 from __future__ import absolute_import
+
+import pytest
 
 from fprime.common.models.serialize import type_base
 from fprime.common.models.serialize.bool_type import BoolType
@@ -24,179 +30,162 @@ from fprime.common.models.serialize.u16_type import U16Type
 from fprime.common.models.serialize.u32_type import U32Type
 from fprime.common.models.serialize.u64_type import U64Type
 
-from fprime.common.models.serialize.type_exceptions import TypeException
+
+def test_bool_type():
+    """
+    Tests the BoolType serialization and deserialization
+    """
+    val1 = BoolType(True)
+    buff = val1.serialize()
+    val2 = BoolType()
+    val2.deserialize(buff, 0)
+    assert val1.val == val2.val
+
+    val1 = BoolType(False)
+    buff = val1.serialize()
+    val2 = BoolType()
+    val2.deserialize(buff, 0)
+    assert val1.val == val2.val
 
 
-if __name__ == "__main__":
+def test_enum_type():
+    """
+    Tests the EnumType serialization and deserialization
+    """
+    members = {"MEMB1": 0, "MEMB2": 6, "MEMB3": 9}
+    val1 = EnumType("SomeEnum", members, "MEMB3")
+    buff = val1.serialize()
+    val2 = EnumType("SomeEnum", members)
+    val2.deserialize(buff, 0)
+    assert val1.val == val2.val
 
-    # test_bool_type
-    print("\nBOOL")
-    try:
-        val = BoolType(True)
-        print("Value: %s" % str(val.val))
-        buff = val.serialize()
-        type_base.showBytes(buff)
-        val2 = BoolType()
-        val2.deserialize(buff, 0)
-        print("Deserialized: %s" % str(val2.val))
-    except TypeException as e:
-        print("Exception: %s" % e.getMsg())
 
-    # test_enum_type
-    print("\nENUM")
-    try:
-        members = {"MEMB1": 0, "MEMB2": 6, "MEMB3": 9}
-        print("Members: ", members)
-        val = EnumType("SomeEnum", members, "MEMB3")
-        print("Value: %s" % val.val)
-        buff = val.serialize()
-        type_base.showBytes(buff)
-        val2 = EnumType("SomeEnum", members)
-        val2.deserialize(buff, 0)
-        print("Deserialize: %s" % val2.val)
-    except TypeException as e:
-        print("Exception: %s" % e.getMsg())
+def test_f32_type():
+    """
+    Tests the F32Type serialization and deserialization
+    """
+    val1 = F32Type(6.9)
+    buff = val1.serialize()
+    val2 = F32Type()
+    val2.deserialize(buff, 0)
+    assert val1.val == pytest.approx(val2.val, 0.0000001)
 
-    # test_f32_type
-    print("\nF32")
-    try:
-        val = F32Type(3.6)
-        print("Value: %s" % str(val.val))
-        buff = val.serialize()
-        type_base.showBytes(buff)
-        print("Serialized: ", repr(buff))
-        val2 = F32Type()
-        val2.deserialize(buff, 0)
-        print("Deserialize: %f" % val2.val)
-    except TypeException as e:
-        print("Exception: %s" % e.getMsg())
 
-    # test_f64_type
-    print("\nF64")
-    try:
-        val = F64Type(100000.23)
-        print("Value: %s" % str(val.val))
-        buff = val.serialize()
-        type_base.showBytes(buff)
-        print("Serialized: ", repr(buff))
-        val2 = F64Type()
-        val2.deserialize(buff, 0)
-        print("Deserialize: %f" % val2.val)
-    except TypeException as e:
-        print("Exception: %s" % e.getMsg())
+def test_f64_type():
+    """
+    Tests the F64Type serialization and deserialization
+    """
+    val1 = F64Type(100000.23)
+    buff = val1.serialize()
+    val2 = F64Type()
+    val2.deserialize(buff, 0)
+    assert val1.val == pytest.approx(val2.val, 0.0000001)
 
-    # test_i8_type
-    print("\nI8")
-    try:
-        val = I8Type(-2)
-        print("Value: %s" % str(val.val))
-        buff = val.serialize()
-        type_base.showBytes(buff)
-        print("Serialized: ", repr(buff))
-        val2 = I8Type()
-        val2.deserialize(buff, 0)
-        print("Deserialize: %s" % str(val2.val))
-    except TypeException as e:
-        print("Exception: %s" % e.getMsg())
 
-    # test_i16_type
-    print("\nI16")
-    try:
-        val = I16Type(-2000)
-        print("Value: %s" % str(val.val))
-        buff = val.serialize()
-        type_base.showBytes(buff)
-        print("Serialized: ", repr(buff))
-        val2 = I16Type()
-        val2.deserialize(buff, 0)
-        print("Deserialize: %s" % str(val2.val))
-    except TypeException as e:
-        print("Exception: %s" % e.getMsg())
+def test_i8_type():
+    """
+    Tests the I8Type serialization and deserialization
+    """
+    val1 = I8Type(-2)
+    buff = val1.serialize()
+    val2 = I8Type()
+    val2.deserialize(buff, 0)
+    assert val1.val == val2.val
 
-    # test_i32_type
-    print("\nI32")
-    try:
-        val = I32Type(-2000000)
-        print("Value: %s" % str(val.val))
-        buff = val.serialize()
-        type_base.showBytes(buff)
-        print("Serialized: ", repr(buff))
-        val2 = I32Type()
-        val2.deserialize(buff, 0)
-        print("Deserialize: %s" % str(val2.val))
-        #
-        val3 = I32Type(1000000)
-        print("Value: %s" % str(val3.val))
-        buff = val3.serialize()
-        type_base.showBytes(buff)
-        print("Serialized: ", repr(buff))
-        val4 = I32Type()
-        val4.deserialize(buff, 0)
-        print("Deserialize: %s" % str(val4.val))
 
-    except TypeException as e:
-        print("Exception: %s" % e.getMsg())
+def test_i16_type():
+    """
+    Tests the I16Type serialization and deserialization
+    """
+    val1 = I16Type(-2000)
+    buff = val1.serialize()
+    val2 = I16Type()
+    val2.deserialize(buff, 0)
+    assert val1.val == val2.val
 
-    # test_i64_type
-    print("\nI64")
-    try:
-        val = I64Type(-2000000)
-        print("Value: %s" % str(val.val))
-        buff = val.serialize()
-        type_base.showBytes(buff)
-        print("Serialized: ", repr(buff))
-        val2 = I64Type()
-        val2.deserialize(buff, 0)
-        print("Deserialize: %s" % str(val2.val))
-    except TypeException as e:
-        print("Exception: %s" % e.getMsg())
 
-    # test_serializable_type
-    print("\nSerializable")
-    try:
-        i32Mem = U32Type(1000000)
-        stringMem = StringType("something to say")
-        members = {"MEMB1": 0, "MEMB2": 6, "MEMB3": 9}
-        enumMem = EnumType("SomeEnum", members, "MEMB3")
-        memList = [
-            ("mem1", i32Mem, ">i"),
-            ("mem2", stringMem, ">H"),
-            ("mem3", enumMem, ">i"),
-        ]
-        print("Value: %s" % repr(memList))
-        serType = SerializableType("ASerType", memList)
-        buff = serType.serialize()
-        type_base.showBytes(buff)
-        print("Serialized: ", repr(buff))
-        serType2 = SerializableType("ASerType", memList)
-        serType2.deserialize(buff, 0)
-        print("Deserialized: %s" % repr(serType2.mem_list))
-    except TypeException as e:
-        print("Exception: %s" % e.getMsg())
+def test_i32_type():
+    """
+    Tests the I32Type serialization and deserialization
+    """
+    val1 = I32Type(-2000000)
+    buff = val1.serialize()
+    val2 = I32Type()
+    val2.deserialize(buff, 0)
+    assert val1.val == val2.val
 
-    # test_string_type
-    print("\nString")
-    try:
-        val = StringType("This is a string")
-        print("Value: %s" % val.val)
-        buff = val.serialize()
-        type_base.showBytes(buff)
-        val2 = StringType()
-        val2.deserialize(buff, 0)
-        print("Deserialize: %s" % val2.val)
-    except TypeException as e:
-        print("Exception: %s" % e.getMsg())
+    val1 = I32Type(1000000)
+    buff = val1.serialize()
+    val2 = I32Type()
+    val2.deserialize(buff, 0)
+    assert val1.val == val2.val
 
-    # test_time_type
+
+def test_i64_type():
+    """
+    Tests the I64Type serialization and deserialization
+    """
+    val1 = I64Type(-10000000)
+    buff = val1.serialize()
+    val2 = I64Type()
+    val2.deserialize(buff, 0)
+    assert val1.val == val2.val
+
+    val1 = I64Type(10000000)
+    buff = val1.serialize()
+    val2 = I64Type()
+    val2.deserialize(buff, 0)
+    assert val1.val == val2.val
+
+
+def test_serializable_type():
+    """
+    Tests the SerializableType serialization and deserialization
+    """
+    u32Mem = U32Type(1000000)
+    stringMem = StringType("something to say")
+    members = {"MEMB1": 0, "MEMB2": 6, "MEMB3": 9}
+    enumMem = EnumType("SomeEnum", members, "MEMB3")
+    memList = [
+        ("mem1", u32Mem, ">i"),
+        ("mem2", stringMem, ">H"),
+        ("mem3", enumMem, ">i"),
+    ]
+    serType1 = SerializableType("ASerType", memList)
+    buff = serType1.serialize()
+    serType2 = SerializableType("ASerType", memList)
+    serType2.deserialize(buff, 0)
+    assert serType1.mem_list == serType2.mem_list
+
+    i32Mem = I32Type(-1000000)
+    stringMem = StringType("something else to say")
+    members = {"MEMB1": 4, "MEMB2": 2, "MEMB3": 0}
+    enumMem = EnumType("SomeEnum", members, "MEMB3")
+    memList = [
+        ("mem1", i32Mem, ">i"),
+        ("mem2", stringMem, ">H"),
+        ("mem3", enumMem, ">i"),
+    ]
+    serType1 = SerializableType("ASerType", memList)
+    buff = serType1.serialize()
+    serType2 = SerializableType("ASerType", memList)
+    serType2.deserialize(buff, 0)
+    assert serType1.mem_list == serType2.mem_list
+
+
+def test_time_type():
+    """
+    Tests the TimeType serialization and deserialization
+    """
     TIME_SIZE = 11
-    test_buff = bytearray(
-        "\x01\x02\x03\x00\x02\x03\x00\x00\x00\x04\x00\x00\x00\x05", "utf-8"
-    )
-    test_offset = 3
-    test_base = 2
-    test_context = 3
-    test_secs = 4
-    test_usecs = 5
+    # test_buff = bytearray(
+    #     "\x01\x02\x03\x00\x02\x03\x00\x00\x00\x04\x00\x00\x00\x05", "utf-8"
+    # )
+    # test_offset = 3
+    # test_base = 2
+    # test_context = 3
+    # test_secs = 4
+    # test_usecs = 5
 
     in_no_err_list = [
         (TimeBase["TB_NONE"].value, 1, 100, 999999),
@@ -213,102 +202,58 @@ if __name__ == "__main__":
         (TimeBase["TB_WORKSTATION_TIME"].value, 1, 700000, 1234567),
     ]
 
-    passed = 0
-    failed = 0
-
-    print("\nTimeType Tests")
-    print("--------------------------------------------------------------------")
-
     val = TimeType()
     size = val.getSize()
-    if size != TIME_SIZE:
-        print(("Test Failed, getSize() returned %d, expected %d" % (size, TIME_SIZE)))
-        failed += 1
-    else:
-        print(("Test Passed, getSize() returned %d" % size))
-        passed += 1
-
-    print("\nNext %d tests expected to be exception free" % len(in_no_err_list))
+    assert size == TIME_SIZE
 
     for (t_base, t_context, secs, usecs) in in_no_err_list:
         result = ser_deser_test(t_base, t_context, secs, usecs)
-
-        if result:
-            print("Test Passed\n\n")
-            passed += 1
-        else:
-            print("Test FAILED\n\n")
-            failed += 1
-
-    print("\nNext %d tests expected to have exceptions" % len(in_err_list))
+        assert result
 
     for (t_base, t_context, secs, usecs) in in_err_list:
         result = ser_deser_test(t_base, t_context, secs, usecs, should_err=True)
+        assert result
 
-        if result:
-            print("Test Passed\n\n")
-            passed += 1
-        else:
-            print("Test FAILED\n\n")
-            failed += 1
 
-    print("-------------------------------------------------------------------")
-    print("Results:")
-    print(("\tPassed: %d" % passed))
-    print(("\tFAILED: %d" % failed))
+def test_u8_type():
+    """
+    Tests the U8Type serialization and deserialization
+    """
+    val1 = U8Type(8)
+    buff = val1.serialize()
+    val2 = U8Type()
+    val2.deserialize(buff, 0)
+    assert val1.val == val2.val
 
-    # test_u8_type
-    print("\nU8")
-    try:
-        val = U8Type(8)
-        print("Value: %s" % str(val.val))
-        buff = val.serialize()
-        type_base.showBytes(buff)
-        print("Serialized: ", repr(buff))
-        val2 = U8Type()
-        val2.deserialize(buff, 0)
-        print("Deserialized: %s" % str(val2.val))
-    except TypeException as e:
-        print("Exception: %s" % e.getMsg())
 
-    # test_u16_type
-    print("\nU16")
-    try:
-        val = U16Type(1000)
-        print("Val: %s" % str(val.val))
-        buff = val.serialize()
-        type_base.showBytes(buff)
-        print("Serialized: ", repr(buff))
-        val2 = U16Type()
-        val2.deserialize(buff, 0)
-        print("Deser: %s" % str(val2.val))
-    except TypeException as e:
-        print("Exception: %s" % e.getMsg())
+def test_u16_type():
+    """
+    Tests the U16Type serialization and deserialization
+    """
+    val1 = U16Type(1000)
+    buff = val1.serialize()
+    val2 = U16Type()
+    val2.deserialize(buff, 0)
+    assert val1.val == val2.val
 
-    # test_u32_type
-    print("\nU32")
-    try:
-        val = U32Type(1000000)
-        print("Value: %s" % str(val.val))
-        buff = val.serialize()
-        type_base.showBytes(buff)
-        print("Serialized: ", repr(buff))
-        val2 = U32Type()
-        val2.deserialize(buff, 0)
-        print("Deserialize: %s" % str(val2.val))
-    except TypeException as e:
-        print("Exception: %s" % e.getMsg())
 
-    # test_u64_type
-    print("\nU64")
-    try:
-        val = U64Type(1000000)
-        print("Value: %s" % str(val.val))
-        buff = val.serialize()
-        type_base.showBytes(buff)
-        print("Serialized: ", repr(buff))
-        val2 = U64Type()
-        val2.deserialize(buff, 0)
-        print("Deserialized: %s" % str(val2.val))
-    except TypeException as e:
-        print("Exception: %s" % e.getMsg())
+def test_u32_type():
+    """
+    Tests the U32Type serialization and deserialization
+    """
+    val1 = U32Type(1000000)
+    buff = val1.serialize()
+    val2 = U32Type()
+    val2.deserialize(buff, 0)
+    assert val1.val == val2.val
+
+
+def test_u64_type():
+    """
+    Tests the U64Type serialization and deserialization
+    """
+    val1 = U64Type(20000000)
+    buff = val1.serialize()
+    val2 = U64Type()
+    val2.deserialize(buff, 0)
+    assert val1.val == val2.val
