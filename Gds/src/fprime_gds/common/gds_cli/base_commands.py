c15829e1508c5404ed92a656b421d66607800900
Jake Deerin <2016deerinjo@gmail.com>
2020-07-01 11:51:23 -0400

    Fixed error where gds-cli sometimes missed events due to incorrect timing comparisons

--- Gds/src/fprime_gds/common/gds_cli/base_commands.py
+++ Gds/src/fprime_gds/common/gds_cli/base_commands.py
@@ -195,12 +195,16 @@ class QueryHistoryCommand(BaseCommand):
             item = cls._get_upcoming_item(api, search_filter, "NOW", timeout)
             cls._log(cls._get_item_string(item, json))
         else:
+
             def print_upcoming_item(min_start_time="NOW"):
                 item = cls._get_upcoming_item(api, search_filter, min_start_time)
                 cls._log(cls._get_item_string(item, json))
                 # Update time so we catch the next item since the last one
                 if item:
                     min_start_time = predicates.greater_than(item.get_time())
+                    min_start_time = filtering_utils.time_to_data_predicate(
+                        min_start_time
+                    )
                 return (min_start_time,)
 
             misc_utils.repeat_until_interrupt(print_upcoming_item, "NOW")
