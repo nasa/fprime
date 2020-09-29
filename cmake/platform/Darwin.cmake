e0168eed387e90f2d1ec0613be845b6d3845346a
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2020-09-28 18:52:59 -0700

    mstarch: updating Darwin Platform file

--- cmake/platform/Darwin.cmake
+++ cmake/platform/Darwin.cmake
@@ -1,8 +1,8 @@
 ####
 # Darwin.cmake:
 #
-# Darwin based platform file used for Darwin (Mac OS X) targets. Note: this uses Linux type targets but specifies Mac OS
-# specific flags.
+# Darwin based platform file used for Darwin (Mac OS X) targets. Note: this sets some OS X flags before calling into the common
+# Linux implementations to use the posix types defined there.
 ####
 add_definitions(-DTGT_OS_TYPE_DARWIN)
 
