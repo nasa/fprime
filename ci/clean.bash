3bcddb60bd5d73850a22fcdc3b61b283a6e0e9f7
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2020-10-20 20:50:49 -0700

    mstarch: forcing clean with -ff

--- ci/clean.bash
+++ ci/clean.bash
@@ -8,5 +8,5 @@
 if [[ "${CI_CLEAN_REPO}" == "NO_REALLY_I_WANT_TO_NUKE_ALL_YOUR_BASE" ]]
 then
     echo -e "${BLUE}Cleaning Repository at ${FPRIME_DIR:-.}${NOCOLOR}"
-    git clean -xdfe "ci-*" "${FPRIME_DIR:-.}" 1>/dev/null 2>/dev/null || fail_and_stop "Failed to clean git repository before testing"
+    git clean -xdffe "ci-*" "${FPRIME_DIR:-.}" 1>/dev/null 2>/dev/null || fail_and_stop "Failed to clean git repository before testing"
 fi
