e48c45aa0425ad0e22fc661e2191fd1e6091fed4
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2020-11-16 14:46:25 -0800

    mstarch: fixing master themeing

--- Gds/src/fprime_gds/flask/static/js/config.js
+++ Gds/src/fprime_gds/flask/static/js/config.js
@@ -9,7 +9,7 @@ export let config = {
     // Allows projects to brand the UI
     projectName: "Infrastructure",
     // Logo of the project. Will be grey when timed-out, otherwise will be full-color
-    logo: "https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg",
+    logo: "/img/logo.svg",
     // Time in seconds to wait before reporting data flow error
     dataTimeout: 5,
     // Set the icon for the condition when there is data flowing
