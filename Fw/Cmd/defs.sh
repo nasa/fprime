79528ce6576f63f2de8d3d9b05d236694f086da4
bocchino <bocchino@jpl.nasa.gov>
2021-01-21 20:06:06 -0800

    Revise build for Fw/Cmd

---
+++ Fw/Cmd/defs.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+
+export FPRIME_ROOT=`cd ../..; echo $PWD`
+
+export FPP_FILES="
+Cmd.fpp
+"
+
+deps=`fpp-depend $FPRIME_ROOT/defs.fpp $FPP_FILES`
+export DEPS=`echo $deps | sed 's/ /,/g'`
