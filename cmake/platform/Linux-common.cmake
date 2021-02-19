4ddc1c5168a9cac77d521213a5c916edeb339226
David9006 <78573428+David9006@users.noreply.github.com>
2021-02-19 18:48:40 +0800

    Update Linux-common.cmake

--- cmake/platform/Linux-common.cmake
+++ cmake/platform/Linux-common.cmake
@@ -7,10 +7,10 @@
 
 # Set linux target
 add_definitions(-DTGT_OS_TYPE_LINUX)
-set(FPRIME_USE_POSIX ON)
+set(FPRIME_USE_POSIX OFF)
 # Requires threading library, use cmake to find appropriate library
 message(STATUS "Requiring thread library")
 FIND_PACKAGE ( Threads REQUIRED )
 
 # Add Linux specific headers into the system
-include_directories(SYSTEM "${FPRIME_FRAMEWORK_PATH}/Fw/Types/Linux")
+include_directories(SYSTEM "${FPRIME_FRAMEWORK_PATH}/Fw/Types")
