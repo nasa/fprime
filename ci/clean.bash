90ee811240a8f83963a96527882d19d960079adf
David9006 <78573428+David9006@users.noreply.github.com>
2021-02-20 17:27:01 +0800

    Update clean.bash

--- ci/clean.bash
+++ ci/clean.bash
@@ -5,8 +5,8 @@
 # "CI_CLEAN_REPO" is set to "NO_REALLY_I_WANT_TO_NUKE_ALL_YOUR_BASE". This is to allow the CI system
 # to clean the repo over and over while protecting  innocent developers from nuking updates....again.
 ####
-if [[ "${CI_CLEAN_REPO}" == "NO_REALLY_I_WANT_TO_NUKE_ALL_YOUR_BASE" ]]
+if [[ "${CI_CLEAN_REPO}" != "NO_REALLY_I_WANT_TO_NUKE_ALL_YOUR_BASE" ]]
 then
-    echo -e "${BLUE}Cleaning Repository at ${FPRIME_DIR:-.}${NOCOLOR}"
-    git clean -xdffe "ci-*" "${FPRIME_DIR:-.}" 1>/dev/null 2>/dev/null || fail_and_stop "Failed to clean git repository before testing"
+    echo "${BLUE}Cleaning Repository at ${FPRIME_DIR:-.}${NOCOLOR}"
+    git clone -xdffe "ci-*" "${FPRIME_DIR:-.}" 1>/dev/null 2>/dev/null || fail_and_stop "Failed to clean git repository before testing"
 fi
