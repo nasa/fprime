f2b8ed7cfd6b6f01b16de8148bca689780400751
Josh Soref <jsoref@users.noreply.github.com>
2021-03-03 20:23:57 -0500

    spelling: merely
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- cmake/platform/Linux.cmake
+++ cmake/platform/Linux.cmake
@@ -1,7 +1,7 @@
 ####
 # Linux.cmake:
 #
-# Linux platform file for standard linux targets. Meerly defers to [./Linux-common.cmake](Linux-common.cmake).
+# Linux platform file for standard linux targets. Merely defers to [./Linux-common.cmake](Linux-common.cmake).
 ####
 # Use common linux setup
 include("${CMAKE_CURRENT_LIST_DIR}/Linux-common.cmake")
