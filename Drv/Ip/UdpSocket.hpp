d3726de5faab13bd9923fa7e3a25c6ba83a243a0
M Starch <LeStarch@googlemail.com>
2021-03-08 07:48:22 -0800

    Update Drv/Ip/UdpSocket.hpp
    Co-authored-by: Josh Soref <jsoref@users.noreply.github.com>

--- Drv/Ip/UdpSocket.hpp
+++ Drv/Ip/UdpSocket.hpp
@@ -72,7 +72,7 @@ class UdpSocket : public IpSocket {
      * \param hostname: socket uses for incoming transmissions. Must be of form x.x.x.x
      * \param port: port socket uses for incoming transmissions. Must NOT be 0.
      * \param send_timeout_seconds: send timeout seconds portion
-     * \param semd_timeout_microseconds: send timeout microseconds portion. Must be less than 1000000
+     * \param send_timeout_microseconds: send timeout microseconds portion. Must be less than 1000000
      * \return status of configure
      */
     SocketIpStatus configureRecv(const char* hostname, const U16 port);
