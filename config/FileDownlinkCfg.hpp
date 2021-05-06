da8f11eee6a2a02483e08fa71d0125c8a199ec5e
Joshua Anderson <joshua.l.anderson@jpl.nasa.gov>
2021-05-05 19:01:18 -0700

    FileDownlink: Don't return an error status if attempting to transfer past EOF

--- config/FileDownlinkCfg.hpp
+++ config/FileDownlinkCfg.hpp
@@ -13,11 +13,11 @@ namespace Svc {
     // see if a packet is ready. If it is false, the next packet
     // will be sent as soon as the previous is complete.
     static const bool FILEDOWNLINK_PACKETS_BY_RUN = false;
-    // If this is set to true, if a file is not found the command will fail.
-    // If it is false, the command will still pass.
-    // The false setting is to prevent a sequence from ending with
-    // an error simply because one file name was wrong
-    static const bool FILEDOWNLINK_COMMAND_FAIL_ON_MISSING_FILE = false;
+    // If this is set, errors that would cause FileDownlink to return an error response, such as a
+    // missing file or attempting to send a partial chunk past the end of the file will instead
+    // return success. This is recommended to avoid a non-serious FileDownlink error aborting a
+    // sequence early. These errors will still be logged as events.
+    static const bool FILEDOWNLINK_COMMAND_FAILURES_DISABLED = true;
     // Size of the internal file downlink buffer. This must now be static as
     // file down maintains its own internal buffer.
     static const U32 FILEDOWNLINK_INTERNAL_BUFFER_SIZE = FW_COM_BUFFER_MAX_SIZE-sizeof(FwPacketDescriptorType);
