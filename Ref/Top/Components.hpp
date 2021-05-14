73b785c092c0d02d36fe98ef153f40732fc55380
M Starch <LeStarch@googlemail.com>
2021-05-14 00:16:38 -0700

    lestarch: removing groundIf entirely from Ref

--- Ref/Top/Components.hpp
+++ Ref/Top/Components.hpp
@@ -24,13 +24,11 @@
 #include <Svc/AssertFatalAdapter/AssertFatalAdapterComponentImpl.hpp>
 #include <Svc/FatalHandler/FatalHandlerComponentImpl.hpp>
 #include <Drv/BlockDriver/BlockDriverImpl.hpp>
-#include <Svc/GroundInterface/GroundInterface.hpp>
 #include <Svc/StaticMemory/StaticMemoryComponentImpl.hpp>
 #include <Svc/Framer/FramerComponentImpl.hpp>
 #include <Svc/Deframer/DeframerComponentImpl.hpp>
 
 #include <Drv/TcpClient/TcpClientComponentImpl.hpp>
-#include <Drv/Udp/UdpComponentImpl.hpp>
 
 void constructRefArchitecture(void);
 bool constructApp(bool dump, U32 port_number, char* hostname);
@@ -40,7 +38,6 @@ void exitTasks(void);
 extern Svc::RateGroupDriverImpl rateGroupDriverComp;
 extern Svc::ActiveRateGroupImpl rateGroup1Comp, rateGroup2Comp, rateGroup3Comp;
 extern Svc::CmdSequencerComponentImpl cmdSeq;
-extern Svc::GroundInterfaceComponentImpl groundIf;
 extern Svc::ConsoleTextLoggerImpl textLogger;
 extern Svc::ActiveLoggerImpl eventLogger;
 extern Svc::LinuxTimeImpl linuxTime;
