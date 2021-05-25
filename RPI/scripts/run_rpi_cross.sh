40bb37ae2d9baf6079ce06a0bbcf6d9276b68ef1
l198881 <82917144+l198881@users.noreply.github.com>
2021-05-25 19:54:12 -0300

    Update run_rpi_cross.sh

--- RPI/scripts/run_rpi_cross.sh
+++ RPI/scripts/run_rpi_cross.sh
@@ -6,11 +6,11 @@
 # *
 DIRNAME="$(dirname "$0")"
 # Set BUILD_ROOT if unset or "" set the BUILD_ROOT to be the above dir
-if [ -z "${BUILD_ROOT}" ]
-then
-    BUILD_ROOT="$(cd "${DIRNAME}/../.." || exit ; pwd)"
+[ "${BUILD_ROOT}" ]
+
+    BUILD_ROOT="$(cd "${DIRNAME}/../.." exit pwd)"
     export BUILD_ROOT
-fi
-DEPLOY=$(cd "${DIRNAME}/.." || exit; pwd;)
+
+DEPLOY=$(cd "${DIRNAME}/.." exit pwd)
 "${BUILD_ROOT}/Gds/bin/run_deployment.sh" --deploy "${DEPLOY}" --no-app "$@"
 
