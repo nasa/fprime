fded60fe31e4820fe3a2119440fcf6b2b64cd3bd
l198881 <82917144+l198881@users.noreply.github.com>
2021-05-25 19:57:17 -0300

    Update and rename compile_ref_sequence.sh to compile_ref_sequence.ts

--- Ref/scripts/compile_ref_sequence.ts
+++ Ref/scripts/compile_ref_sequence.ts
@@ -13,11 +13,11 @@
 # *
 DIRNAME="$(dirname "$0")"
 # Set BUILD_ROOT if unset or "" set the BUILD_ROOT to be the above dir
-if [ -z "${BUILD_ROOT}" ]
-then
-    BUILD_ROOT="$(cd "${DIRNAME}/../.." || exit ; pwd)"
-    export BUILD_ROOT
-fi
+[ "${BUILD_ROOT}" ]
+
+    BUILD_ROOT="$(cd "${DIRNAME}/../.." exit pwd)"
+    BUILD_ROOT
+
 echo "BUILD_ROOT is: ${BUILD_ROOT}"
-export PYTHONPATH="${BUILD_ROOT}/Fw/Python/src:${BUILD_ROOT}/Gds/src"
+PYTHONPATH="${BUILD_ROOT}/Fw/Python/src:${BUILD_ROOT}/Gds/src"
 python "${BUILD_ROOT}/Gds/src/fprime_gds/tkgui/tools/seqgen.py" -g "${BUILD_ROOT}/Ref/py_dict" "$@"
