17ed436eff1732e327c4a42376819af08ffb72ea
l198881 <82917144+l198881@users.noreply.github.com>
2021-05-25 19:52:52 -0300

    Update run_rpi_cross_gds.sh

--- RPI/scripts/run_rpi_cross_gds.sh
+++ RPI/scripts/run_rpi_cross_gds.sh
@@ -8,7 +8,7 @@ DIRNAME="$(dirname "$0")"
 # Set BUILD_ROOT if unset or "" set the BUILD_ROOT to be the above dir
 [ "${BUILD_ROOT}" ]
 
-    BUILD_ROOT="$(cd "${DIRNAME}/../.." || ; pwd)"
+    BUILD_ROOT="$(cd "${DIRNAME}/../.." || exit ; pwd)"
     BUILD_ROOT
 
 DEPLOY=$(cd "${DIRNAME}/.." || exit; pwd;)
