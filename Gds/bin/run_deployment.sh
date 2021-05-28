76af56a90ca503ae45ea0312c8408cefea2a71b2
l198881 <82917144+l198881@users.noreply.github.com>
2021-05-28 15:22:28 -0300

    Eclipse cleanup (#55)
    * Fix shellcheck warnings
    
    Warnings reported by shellcheck:
      * https://www.shellcheck.net/wiki/SC2071 -- > is for string comparisons
      * https://www.shellcheck.net/wiki/SC2155 -- Declare and assign separately.
      * https://www.shellcheck.net/wiki/SC2164 -- Use 'cd ... || exit' or 'cd ... |...
      * https://www.shellcheck.net/wiki/SC2006 -- Use $(...) notation instead of legacy backticked `...`
      * https://www.shellcheck.net/wiki/SC2086 -- Double quote to prevent globbing and word splitting
    
    Usage:
    docker run --rm -v "$PWD:/mnt" koalaman/shellcheck:stable $(find . -name "*.sh")
    
    * Dropped trailing ;
    
    * Remove outdated eclipse project files
    
    Co-authored-by: Christian Muck <cmuckmuck@gmail.com>
    Co-authored-by: M Starch <LeStarch@googlemail.com>
    Co-authored-by: Joshua Anderson <joshua.l.anderson@jpl.nasa.gov>

--- Gds/bin/run_deployment.sh
+++ Gds/bin/run_deployment.sh
@@ -4,11 +4,12 @@
 # * ALL RIGHTS RESERVED. United States Government Sponsorship
 # * acknowledged.
 # *
-DIRNAME="`dirname $0`"
+DIRNAME="$(dirname "$0")"
 # Set BUILD_ROOT if unset or "" set the BUILD_ROOT to be the above dir
-if [ -z ${BUILD_ROOT} ]
+if [ -z "${BUILD_ROOT}" ]
 then
-    export BUILD_ROOT="`cd ${DIRNAME}/../..; pwd`"
+    BUILD_ROOT="$(cd "${DIRNAME}/../.." || exit; pwd)"
+    export BUILD_ROOT
 fi
 export PYTHONPATH="${BUILD_ROOT}/Fw/Python/src:${BUILD_ROOT}/Gds/src"
 python -m fprime_gds.executables.run_deployment "$@"
