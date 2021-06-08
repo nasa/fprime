beec355a1ff8ad122259b09f8916cb5e453951c3
Rohan Dhesikan <rohandhesikan@gmail.com>
2021-06-08 11:00:52 -0700

    Added test for default initializaion of serializable structs

--- Autocoders/Python/test/serialize3/test/ut/main.cpp
+++ Autocoders/Python/test/serialize3/test/ut/main.cpp
@@ -1,7 +1,30 @@
 #include <Autocoders/Python/test/serialize3/AlltypesSerializableAc.hpp>
 
-int main(int argc, char* argv[]) {
+#include <Fw/Types/SerialBuffer.hpp>
+#include <Fw/Types/Assert.hpp>
+#include "Fw/Test/UnitTestAssert.hpp"
 
-    Ns::Something::Alltypes atypes;
+#include "gtest/gtest.h"
 
+#include <iostream>
+#include <string.h>
+
+TEST(Serialize3, DefaultInit) {
+	// Create Alltypes serializable struct
+	Ns::Something::Alltypes atypes;
+
+	// Check that default values were initialized
+	ASSERT_EQ(atypes.geti8(), 43);
+	ASSERT_EQ(atypes.getf64(), 1.2345);
+	ASSERT_EQ(atypes.getstr(), "hello");
+	ASSERT_EQ(atypes.getb(), true);
 }
+
+
+int main(int argc, char* argv[]) {
+     ::testing::InitGoogleTest(&argc, argv);
+     int status = RUN_ALL_TESTS();
+     return status;
+}
+
+
