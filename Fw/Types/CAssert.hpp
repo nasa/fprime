995558a201be8d3674f96593564559a7e9e1cf78
LEONARDO HAIM NIGRI <82917144+l198881@users.noreply.github.com>
2021-06-14 22:53:21 -0300

    Update CAssert.hpp

--- Fw/Types/CAssert.hpp
+++ Fw/Types/CAssert.hpp
@@ -5,40 +5,40 @@
  *      Author: tcanham
  */
 
-#ifndef FWCASSERT_HPP_
-#define FWCASSERT_HPP_
+# FWCASSERT_HPP_
+# FWCASSERT_HPP_
 
-#include <FpConfig.hpp>
-#include <Fw/Types/BasicTypes.hpp>
+# <FpConfig.hpp>
+# <Fw/Types/BasicTypes.hpp>
 
-#if FW_ASSERT_LEVEL == FW_NO_ASSERT
+# FW_ASSERT_LEVEL == FW_NO_ASSERT
 
-#define FW_CASSERT(...)
+# FW_CASSERT(...)
 
-#else // ASSERT is defined
+# // ASSERT is defined
 
-#if FW_ASSERT_LEVEL == FW_FILEID_ASSERT
-#define FILE_NAME_ARG NATIVE_UINT_TYPE
-#define FW_CASSERT(cond) \
-    ((void) ((cond) ? (0) : \
+# FW_ASSERT_LEVEL == FW_FILEID_ASSERT
+# FILE_NAME_ARG NATIVE_UINT_TYPE
+# FW_CASSERT() \
+    (() (() ? (0) : \
     (CAssert0(ASSERT_FILE_ID, __LINE__))))
-#else
-#define FILE_NAME_ARG const U8*
-#define FW_CASSERT(cond) \
-    ((void) ((cond) ? (0) : \
+#
+# FILE_NAME_ARG  U8*
+# FW_CASSERT() \
+    (() (() ? (0) : \
     (CAssert0((U8*)__FILE__, __LINE__))))
-#endif
+#
 
-#ifdef __cplusplus
+# __cplusplus
 extern "C" {
-#endif // __cplusplus
+# // __cplusplus
 
 I32 CAssert0(FILE_NAME_ARG file, U32 lineNo); //!< C assert function
 I32 CAssert1(FILE_NAME_ARG file, U32 lineNo, NATIVE_INT_TYPE arg1); //!< C assert function 1
 
-#ifdef __cplusplus
+# __cplusplus
 } // extern "C"
-#endif // __cplusplus
+# // __cplusplus
 
 
 
