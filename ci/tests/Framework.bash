28a82fb791c38b441754e17edc928968266fec3e
r9pena <r9_pena@hotmail.com>
2021-07-26 11:10:01 -0600

    Modified path for framework job test

--- ci/tests/Framework.bash
+++ ci/tests/Framework.bash
@@ -10,13 +10,13 @@ export SCRIPT_DIR="$(dirname ${BASH_SOURCE})/.."
 . ${SCRIPT_DIR}/tests/fputil.bash
 
 export CMAKE_EXTRA_SETTINGS=""
-echo -e "${BLUE}Testing ${FPUTIL_DEPLOYS} against fprime-util targets: ${FPUTIL_TARGETS[@]}${NOCOLOR}"
+echo -e "${BLUE}Testing ${FPRIME_DIR} against fprime-util targets: ${FPUTIL_TARGETS[@]}${NOCOLOR}"
 export CHECK_TARGET_PLATFORM="native"
 for target in "${FPUTIL_TARGETS[@]}"
 do
     if [[ "${target}" == "generate" ]]
     then
-        rm -rf "${FPUTIL_DEPLOYS}/build-fprime-automatic-"*
+        rm -rf "${FRIME_DIR}/build-fprime-automatic-"*
     fi
     fputil_action "${FPRIME_DIR}" "${target}"
 done
