70b6c0b4f7d5886fd751097ede30169bd890a633
L. H. N <82917144+l198881@users.noreply.github.com>
2021-08-15 23:10:02 -0300

    Update master.bash

--- ci/master.bash
+++ ci/master.bash
@@ -5,22 +5,22 @@
 # Master script to run the CI process. This will setup and run the tests for the CI system.
 ####
 
-export CTEST_OUTPUT_ON_FAILURE=1
-export SCRIPT_DIR="$(dirname ${BASH_SOURCE})"
+ CTEST_OUTPUT_ON_FAILURE=1
+ SCRIPT_DIR="$(dirname ${BASH_SOURCE})"
 . "${SCRIPT_DIR}/helpers.bash"
 
-export TEST="${@}"
+ TEST="${@}"
 TEST_RUN="${SCRIPT_DIR}/tests/${TEST}.bash"
-export TEST_TYPE="${TEST}"
+ TEST_TYPE="${TEST}"
 
 #### NEEDED ENVIRONMENT ####
-export FPRIME_DIR="$(cd ${SCRIPT_DIR}/..; pwd)"
-export LOG_DIR="${FPRIME_DIR}/ci-${TEST}-logs-$(date +"%Y-%m-%dT%H%M%S")"
-mkdir -p "${LOG_DIR}"
+ FPRIME_DIR="$(cd ${SCRIPT_DIR}/..; pwd)"
+ LOG_DIR="${FPRIME_DIR}/ci-${TEST}-logs-$(date +"%Y-%m-%dT%H%M%S")"
+    -p "${LOG_DIR}"
 
 # Run the test script
-echo -e "${BLUE}Starting CI test ${TEST_RUN} ${TEST}${NOCOLOR}"
-/usr/bin/time "${TEST_RUN}" || fail_and_stop "${TEST} failed"
-echo -e "${GREEN}CI test ${TEST_RUN} ${TEST} SUCCESSFUL${NOCOLOR}"
+    -e "${BLUE}Starting CI test ${TEST_RUN} ${TEST}${NOCOLOR}"
+/usr/bin/time "${TEST_RUN}"  fail_and_stop "${TEST} failed"
+    -e "${GREEN}CI    ${TEST_RUN} ${TEST} SUCCESSFUL${NOCOLOR}"
 
 archive_logs
