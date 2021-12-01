d6cc84d909d847db8f47594b4afb9ce863a1035b
M Starch <LeStarch@googlemail.com>
2021-11-30 18:19:53 -0800

    lestarch: fixing python format for new profile processor

--- cmake/profile/profile_process.py
+++ cmake/profile/profile_process.py
@@ -1,6 +1,6 @@
 import random
 
-MEMBER_SIZES=3
+MEMBER_SIZES = 3
 
 with open("fp_profiling", "r") as fh:
     lines = [line.strip().split() for line in fh.readlines()]
@@ -16,16 +16,26 @@ for stamp, stage, token, module in lines:
     elif stage == "STOP":
         accum, count, min_delta, max_delta, hits = totals[token]
         delta = stamp - lasts[token]
-        totals[token] = (accum + delta, count + 1, min(delta, min_delta), max(delta, max_delta), hits + [(delta, module)])
+        totals[token] = (
+            accum + delta,
+            count + 1,
+            min(delta, min_delta),
+            max(delta, max_delta),
+            hits + [(delta, module)],
+        )
 
 for token, pair in totals.items():
     accum, count, min_delta, max_delta, hits = totals[token]
     if count == 0:
         continue
-    print("{} {:.6f} ({:.6f}, {:.6f}, {:.6f}) {}".format(token, accum, min_delta, accum/count, max_delta, hits[int(count/2)][0]))
+    print(
+        "{} {:.6f} ({:.6f}, {:.6f}, {:.6f}) {}".format(
+            token, accum, min_delta, accum / count, max_delta, hits[int(count / 2)][0]
+        )
+    )
     hist = {index: (0, []) for index in range(0, 1000)}
     for item, module in hits:
-        index = int(int(item * 1000)/10) * 10
+        index = int(int(item * 1000) / 10) * 10
         if index not in hist:
             hist[index] = (0, [])
         hist[index] = (hist[index][0] + 1, hist[index][1] + [module])
@@ -33,5 +43,11 @@ for token, pair in totals.items():
         count, members = pair
         random.shuffle(members)
         if count != 0:
-            print("    {:3} ms: {:4}        -- {}".format(index, count, " ".join(members[:MEMBER_SIZES]) + ("" if len(members) <= MEMBER_SIZES else " ...")))
-
+            print(
+                "    {:3} ms: {:4}        -- {}".format(
+                    index,
+                    count,
+                    " ".join(members[:MEMBER_SIZES])
+                    + ("" if len(members) <= MEMBER_SIZES else " ..."),
+                )
+            )
