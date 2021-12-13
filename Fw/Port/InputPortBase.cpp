f16c778a18102cc8b1f922735d54274a3ea31239
Fabrizio Sandri <dev.fabriziosandri@gmail.com>
2021-12-13 21:46:11 +0100

    InputPortBase snprintf fix

--- Fw/Port/InputPortBase.cpp
+++ Fw/Port/InputPortBase.cpp
@@ -29,10 +29,12 @@ namespace Fw {
     
 #if FW_OBJECT_TO_STRING == 1
     void InputPortBase::toString(char* buffer, NATIVE_INT_TYPE size) {
-#if FW_OBJECT_NAMES == 1        
-        (void)snprintf(buffer, size, "InputPort: %s->%s", this->m_objName,
-                        this->isConnected() ? this->m_connObj->getObjName() : "None");
-        buffer[size-1] = 0;
+#if FW_OBJECT_NAMES == 1 
+        FW_ASSERT(size > 0);
+        if (snprintf(buffer, size, "InputPort: %s->%s", this->m_objName,
+                     this->isConnected() ? this->m_connObj->getObjName() : "None") < 0) {
+            buffer[0] = 0;
+        }
 #else
         (void)snprintf(buffer,size,"%s","Unnamed Input port");
 #endif
