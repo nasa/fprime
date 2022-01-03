90b05b690094052a741f4bfacc4955edf4d6bc52
Saba Janamian <35859004+saba-ja@users.noreply.github.com>
2022-01-03 22:20:41 +0530

    saba-ja: added missing GenericHub.hpp file (#1192)

---
+++ Svc/GenericHub/GenericHub.hpp
@@ -0,0 +1,17 @@
+// ======================================================================
+// GenericHub.hpp
+// Standardization header for GenericHub
+// ======================================================================
+
+#ifndef Svc_GenericHub_HPP
+#define Svc_GenericHub_HPP
+
+#include "Svc/GenericHub/GenericHubComponentImpl.hpp"
+
+namespace Svc {
+
+  using GenericHub = GenericHubComponentImpl;
+
+}
+
+#endif
