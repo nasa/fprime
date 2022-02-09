0ca751c6084a06b2756295d154f7c639d00eda62
M Starch <LeStarch@googlemail.com>
2022-02-08 22:32:16 -0500

    lestarch: fixing version generation

--- cmake/target/version.cmake
+++ cmake/target/version.cmake
@@ -7,10 +7,11 @@ set(FPRIME_VERSION_SCRIPT "${CMAKE_CURRENT_LIST_DIR}/version/generate_version_he
 
 function(version_add_global_target TARGET)
     set(OUTPUT_FILE "${CMAKE_BINARY_DIR}/version.hpp")
-    add_custom_command(
-       OUTPUT "${OUTPUT_FILE}" __PHONY__
-       COMMAND ${CMAKE_COMMAND} -E chdir "${FPRIME_PROJECT_ROOT}" "${FPRIME_VERSION_SCRIPT}" "${OUTPUT_FILE}")
-    add_custom_target(${TARGET} DEPENDS "${OUTPUT_FILE}" __PHONY__)
+    add_custom_target("${TARGET}" ALL BYPRODUCTS "${OUTPUT_FILE}"
+        COMMAND "${FPRIME_VERSION_SCRIPT}" "${OUTPUT_FILE}.tmp"
+	COMMAND "${CMAKE_COMMAND}" -E copy_if_different "${OUTPUT_FILE}.tmp" "${OUTPUT_FILE}"
+	WORKING_DIRECTORY "${FPRIME_PROJECT_ROOT}"
+    )
 endfunction()
 
 function(version_add_deployment_target MODULE TARGET SOURCES DEPENDENCIES FULL_DEPENDENCIES)
