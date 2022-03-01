1b7a255f1d7fc5d6b5ecc3ae2122c874347fece4
bocchino <bocchino@jpl.nasa.gov>
2022-03-01 10:54:32 -0800

    Update FPP version

--- cmake/fpp-download/fpp.cmake
+++ cmake/fpp-download/fpp.cmake
@@ -5,7 +5,7 @@
 # should install FPP on the system path and that will be used.
 ####
 
-set(FPP_VERSION v1.0.0-24-g52b996cb)
+set(FPP_VERSION v1.0.0-28-ga8705b3e)
 set(FPP_TOOLS_PATH "${CMAKE_BINARY_DIR}/fpp-tools-install" CACHE PATH "Installation path for fpp tools")
 
 ####
