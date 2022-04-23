58f08c3c7418e185f312a973b93ea3ad012e635a
bocchino <bocchino@jpl.nasa.gov>
2022-04-23 15:52:23 -0700

    Refactor Deframer UTs

--- Svc/Deframer/test/ut-fprime-protocol/Tester.hpp
+++ Svc/Deframer/test/ut-fprime-protocol/Tester.hpp
@@ -122,99 +122,20 @@ namespace Svc {
             // ----------------------------------------------------------------------
 
             //! Get the min packet size
-            static U32 getMinPacketSize() {
-                // Packet must hold the packet type
-                return sizeof(FwPacketDescriptorType);
-            }
+            static U32 getMinPacketSize();
 
             //! Get the max packet size that will fit in the test buffer
             //! This is an invalid size for the deframer
-            static U32 getInvalidPacketSize() {
-                FW_ASSERT(
-                    MAX_FRAME_SIZE >= NON_PACKET_SIZE,
-                    MAX_FRAME_SIZE,
-                    NON_PACKET_SIZE
-                );
-                const U32 result = MAX_FRAME_SIZE - NON_PACKET_SIZE;
-                // Make sure the size is invalid
-                FW_ASSERT(
-                    result > getMaxValidCommandPacketSize(),
-                    result,
-                    getMaxValidCommandPacketSize()
-                );
-                FW_ASSERT(
-                    result > getMaxValidFilePacketSize(),
-                    result,
-                    getMaxValidFilePacketSize()
-                );
-                return result;
-            }
+            static U32 getInvalidPacketSize();
 
             //! Get the max valid file packet size
-            static U32 getMaxValidFilePacketSize() {
-                FW_ASSERT(
-                    MAX_VALID_FRAME_SIZE >= NON_PACKET_SIZE,
-                    MAX_VALID_FRAME_SIZE,
-                    NON_PACKET_SIZE
-                );
-                return MAX_VALID_FRAME_SIZE - NON_PACKET_SIZE;
-            }
+            static U32 getMaxValidFilePacketSize();
 
             //! Get the max valid command packet size
-            static U32 getMaxValidCommandPacketSize() {
-                return std::min(
-                    // Packet must fit into a com buffer
-                    static_cast<U32>(FW_COM_BUFFER_MAX_SIZE),
-                    // Frame must fit into the ring buffer
-                    getMaxValidFilePacketSize()
-                );
-            }
+            static U32 getMaxValidCommandPacketSize();
 
             //! Construct a random frame
-            static UplinkFrame random() {
-                // Randomly set the packet type
-                Fw::ComPacket::ComPacketType packetType =
-                    Fw::ComPacket::FW_PACKET_UNKNOWN;
-                const U32 packetSelector = STest::Pick::lowerUpper(0,1);
-                U32 maxValidPacketSize = 0;
-                switch (packetSelector) {
-                    case 0:
-                        packetType = Fw::ComPacket::FW_PACKET_COMMAND;
-                        maxValidPacketSize = getMaxValidCommandPacketSize();
-                        break;
-                    case 1:
-                        packetType = Fw::ComPacket::FW_PACKET_FILE;
-                        maxValidPacketSize = getMaxValidFilePacketSize();
-                        break;
-                    default:
-                        FW_ASSERT(0);
-                        break;
-                }
-                // Randomly set the packet size
-                U32 packetSize = 0;
-                // Invalidate 1 in 100 packet sizes
-                const U32 invalidSizeIndex = STest::Pick::startLength(0, 100);
-                if (invalidSizeIndex == 0) {
-                    // This packet size fits in the test buffer,
-                    // but is invalid for the deframer
-                    packetSize = getInvalidPacketSize();
-                }
-                else {
-                    // Choose a valid packet size
-                    // This packet size fits in the test buffer and is
-                    // valid for the deframer
-                    packetSize = STest::Pick::lowerUpper(
-                        getMinPacketSize(),
-                        maxValidPacketSize
-                    );
-                }
-                // Construct the frame
-                UplinkFrame frame = UplinkFrame(packetType, packetSize);
-                // Randomly invalidate the frame, or leave it alone
-                frame.randomlyInvalidate();
-                // Return the frame
-                return frame;
-            }
+            static UplinkFrame random();
 
           private:
 
--- Svc/Deframer/test/ut-fprime-protocol/UplinkFrame.cpp
+++ Svc/Deframer/test/ut-fprime-protocol/UplinkFrame.cpp
@@ -86,4 +86,97 @@ namespace Svc {
         return data;
     }
 
+    // ----------------------------------------------------------------------
+    // Public static methods 
+    // ----------------------------------------------------------------------
+
+    U32 Tester::UplinkFrame::getMinPacketSize() {
+        // Packet must hold the packet type
+        return sizeof(FwPacketDescriptorType);
+    }
+
+    U32 Tester::UplinkFrame::getInvalidPacketSize() {
+        FW_ASSERT(
+            MAX_FRAME_SIZE >= NON_PACKET_SIZE,
+            MAX_FRAME_SIZE,
+            NON_PACKET_SIZE
+        );
+        const U32 result = MAX_FRAME_SIZE - NON_PACKET_SIZE;
+        // Make sure the size is invalid
+        FW_ASSERT(
+            result > getMaxValidCommandPacketSize(),
+            result,
+            getMaxValidCommandPacketSize()
+        );
+        FW_ASSERT(
+            result > getMaxValidFilePacketSize(),
+            result,
+            getMaxValidFilePacketSize()
+        );
+        return result;
+    }
+
+    U32 Tester::UplinkFrame::getMaxValidFilePacketSize() {
+        FW_ASSERT(
+            MAX_VALID_FRAME_SIZE >= NON_PACKET_SIZE,
+            MAX_VALID_FRAME_SIZE,
+            NON_PACKET_SIZE
+        );
+        return MAX_VALID_FRAME_SIZE - NON_PACKET_SIZE;
+    }
+
+    U32 Tester::UplinkFrame::getMaxValidCommandPacketSize() {
+        return std::min(
+            // Packet must fit into a com buffer
+            static_cast<U32>(FW_COM_BUFFER_MAX_SIZE),
+            // Frame must fit into the ring buffer
+            getMaxValidFilePacketSize()
+        );
+    }
+
+    Tester::UplinkFrame Tester::UplinkFrame::random() {
+        // Randomly set the packet type
+        Fw::ComPacket::ComPacketType packetType =
+            Fw::ComPacket::FW_PACKET_UNKNOWN;
+        const U32 packetSelector = STest::Pick::lowerUpper(0,1);
+        U32 maxValidPacketSize = 0;
+        switch (packetSelector) {
+            case 0:
+                packetType = Fw::ComPacket::FW_PACKET_COMMAND;
+                maxValidPacketSize = getMaxValidCommandPacketSize();
+                break;
+            case 1:
+                packetType = Fw::ComPacket::FW_PACKET_FILE;
+                maxValidPacketSize = getMaxValidFilePacketSize();
+                break;
+            default:
+                FW_ASSERT(0);
+                break;
+        }
+        // Randomly set the packet size
+        U32 packetSize = 0;
+        // Invalidate 1 in 100 packet sizes
+        const U32 invalidSizeIndex = STest::Pick::startLength(0, 100);
+        if (invalidSizeIndex == 0) {
+            // This packet size fits in the test buffer,
+            // but is invalid for the deframer
+            packetSize = getInvalidPacketSize();
+        }
+        else {
+            // Choose a valid packet size
+            // This packet size fits in the test buffer and is
+            // valid for the deframer
+            packetSize = STest::Pick::lowerUpper(
+                getMinPacketSize(),
+                maxValidPacketSize
+            );
+        }
+        // Construct the frame
+        UplinkFrame frame = UplinkFrame(packetType, packetSize);
+        // Randomly invalidate the frame, or leave it alone
+        frame.randomlyInvalidate();
+        // Return the frame
+        return frame;
+    }
+
 }
