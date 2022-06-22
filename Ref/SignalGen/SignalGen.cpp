aef79093d78db5afab35123809a4ff860991db38
M Starch <LeStarch@googlemail.com>
2022-06-22 11:36:18 -0700

    lestarch: fixing file naming

--- Ref/SignalGen/SignalGen.cpp
+++ Ref/SignalGen/SignalGen.cpp
@@ -1,24 +1,12 @@
 // ======================================================================
-// \title  SequenceFileLoader.cpp
-// \author bocchino
-// \brief  cpp file for SequenceFileLoader component implementation class
-//
-// \copyright
-// Copyright (C) 2009-2016 California Institute of Technology.
-// ALL RIGHTS RESERVED.  United States Government Sponsorship
-// acknowledged.
-//
+// \title  SignalGen.cpp
+// \author mstarch
+// \brief  cpp file for SignalGen component implementation class
 // ======================================================================
 
-#include <Fw/Types/Assert.hpp>
-#include <Ref/SignalGen/SignalGen.hpp>
-#include <cmath>
-#include <cstdlib>
 
-// TKC - don't know why it's undefined in VxWorks
-#ifdef TGT_OS_TYPE_VXWORKS
-#define M_PI (22.0/7.0)
-#endif
+#include <Ref/SignalGen/SignalGen.hpp>
+#include "Fw/Types/BasicTypes.hpp"
 
 namespace Ref {
 
@@ -27,19 +15,12 @@ namespace Ref {
   // ----------------------------------------------------------------------
 
   SignalGen ::
-    SignalGen(const char* name) :
-        SignalGenComponentBase(name),
-        sampleFrequency(25),
-        signalFrequency(1),
-        signalAmplitude(0.0f),
-        signalPhase(0.0f),
-        ticks(0),
-        sigType(SignalType::SINE),
-        sigHistory(),
-        sigPairHistory(),
-        running(false),
-        skipOne(false)
-  {}
+    SignalGen(
+        const char *const compName
+    ) : SignalGenComponentBase(compName)
+  {
+
+  }
 
   void SignalGen ::
     init(
@@ -47,135 +28,64 @@ namespace Ref {
         const NATIVE_INT_TYPE instance
     )
   {
-      SignalGenComponentBase::init(queueDepth, instance);
+    SignalGenComponentBase::init(queueDepth, instance);
   }
 
-  SignalGen :: ~SignalGen() { }
-
-  // ----------------------------------------------------------------------
-  // Handler implementations
-  // ----------------------------------------------------------------------
-
-  F32 SignalGen :: generateSample(U32 ticks) {
-      F32 val = 0.0f;
-      if (this->skipOne) {
-          return val;
-      }
-      // Samples per period
-      F32 samplesPerPeriod = static_cast<F32>(this->sampleFrequency)/static_cast<F32>(this->signalFrequency);
-      U32 halfSamplesPerPeriod = samplesPerPeriod/2;
-      /* Signals courtesy of the open source Aquila DSP Library */
-      switch(this->sigType.e) {
-          case SignalType::TRIANGLE:
-          {
-              F32 m = this->signalAmplitude / static_cast<F32>(halfSamplesPerPeriod);
-              val = m * static_cast<F32>(ticks % halfSamplesPerPeriod);
-              break;
-          }
-          case SignalType::SINE:
-          {
-              F32 normalizedFrequency = 1.0f / samplesPerPeriod;
-              val = this->signalAmplitude * std::sin((2.0 * M_PI * normalizedFrequency *
-                      static_cast<F32>(ticks)) + (this->signalPhase * 2.0 * M_PI));
-              break;
-          }
-          case SignalType::SQUARE:
-          {
-              val = this->signalAmplitude * ((ticks % static_cast<U32>(samplesPerPeriod) < halfSamplesPerPeriod) ? 1.0f : -1.0f);
-              break;
-          }
-          case SignalType::NOISE:
-          {
-              val = this->signalAmplitude * (std::rand() / static_cast<double>(RAND_MAX));
-              break;
-          }
-          default:
-              FW_ASSERT(0); // Should never happen
-      }
-      return val;
-  }
-
-  void SignalGen :: schedIn_handler(
-      NATIVE_INT_TYPE portNum, /*!< The port number*/
-      NATIVE_UINT_TYPE context /*!< The call order*/
-  )
+  SignalGen ::
+    ~SignalGen()
   {
-      F32 value = 0.0f;
-      // This is a queued component, so it must intentionally run the dispatch of commands and queue processing on this
-      // synchronous scheduled call
-      this->doDispatch();
 
-      // This short-circuits when the signal generator is not running
-      if (not this->running) {
-          return;
-      }
-      // Allows for skipping a single reading of the signal
-      if (not this->skipOne) {
-          value = this->generateSample(this->ticks);
-      }
-      this->skipOne = false;
-
-      // Build our new types
-      SignalPair pair = SignalPair(this->ticks, value);
-
-      // Shift and assign our array types
-      for (U32 i = 1; i < this->sigHistory.SIZE; i++) {
-          this->sigHistory[i - 1] = this->sigHistory[i];
-          this->sigPairHistory[i - 1] = this->sigPairHistory[i];
-      }
-      this->sigHistory[this->sigHistory.SIZE - 1] = value;
-      this->sigPairHistory[this->sigPairHistory.SIZE - 1] = pair;
-
-      // Composite structure
-      SignalInfo sigInfo(this->sigType, this->sigHistory, this->sigPairHistory);
-
-      // Write all signals
-      this->tlmWrite_Type(this->sigType);
-      this->tlmWrite_Output(value);
-      this->tlmWrite_PairOutput(pair);
-      this->tlmWrite_History(this->sigHistory);
-      this->tlmWrite_PairHistory(this->sigPairHistory);
-      this->tlmWrite_Info(sigInfo);
-      this->ticks += 1;
   }
 
-  void SignalGen :: SignalGen_Settings_cmdHandler(
-        FwOpcodeType opCode, /*!< The opcode*/
-        U32 cmdSeq, /*!< The command sequence number*/
+  // ----------------------------------------------------------------------
+  // Handler implementations for user-defined typed input ports
+  // ----------------------------------------------------------------------
+
+  void SignalGen ::
+    schedIn_handler(
+        const NATIVE_INT_TYPE portNum,
+        NATIVE_UINT_TYPE context
+    )
+  {
+    // TODO
+  }
+
+  // ----------------------------------------------------------------------
+  // Command handler implementations
+  // ----------------------------------------------------------------------
+
+  void SignalGen ::
+    SignalGen_Settings_cmdHandler(
+        const FwOpcodeType opCode,
+        const U32 cmdSeq,
         U32 Frequency,
         F32 Amplitude,
         F32 Phase,
         Ref::SignalType SigType
     )
   {
-      this->signalFrequency = Frequency;
-      this->signalAmplitude = Amplitude;
-      this->signalPhase     = Phase;
-      this->sigType = SigType;
-
-      this->log_ACTIVITY_LO_SignalGen_SettingsChanged(this->signalFrequency, this->signalAmplitude, this->signalPhase, this->sigType);
-      this->tlmWrite_Type(SigType);
-      this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+    // TODO
+    this->cmdResponse_out(opCode,cmdSeq,Fw::CmdResponse::OK);
   }
 
-  void SignalGen :: SignalGen_Toggle_cmdHandler(
-            FwOpcodeType opCode, /*!< The opcode*/
-            U32 cmdSeq /*!< The command sequence number*/
-        )
-  {
-      this->running = !this->running;
-      this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
-  }
-
-
-
-  void SignalGen :: SignalGen_Skip_cmdHandler(
-      FwOpcodeType opCode, /*!< The opcode*/
-      U32 cmdSeq /*!< The command sequence number*/
+  void SignalGen ::
+    SignalGen_Toggle_cmdHandler(
+        const FwOpcodeType opCode,
+        const U32 cmdSeq
     )
   {
-      this->skipOne = true;
-      this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+    // TODO
+    this->cmdResponse_out(opCode,cmdSeq,Fw::CmdResponse::OK);
   }
 
-};
+  void SignalGen ::
+    SignalGen_Skip_cmdHandler(
+        const FwOpcodeType opCode,
+        const U32 cmdSeq
+    )
+  {
+    // TODO
+    this->cmdResponse_out(opCode,cmdSeq,Fw::CmdResponse::OK);
+  }
+
+} // end namespace Ref
--- Ref/SignalGen/SignalGen.hpp
+++ Ref/SignalGen/SignalGen.hpp
@@ -1,24 +1,13 @@
 // ======================================================================
 // \title  SignalGen.hpp
-// \author bocchino
-// \brief  hpp file for SequenceFileLoader component implementation class
-//
-// \copyright
-// Copyright (C) 2009-2016 California Institute of Technology.
-// ALL RIGHTS RESERVED.  United States Government Sponsorship
-// acknowledged.
-//
+// \author mstarch
+// \brief  hpp file for SignalGen component implementation class
 // ======================================================================
 
-#ifndef Svc_SignalGen_HPP
-#define Svc_SignalGen_HPP
+#ifndef SignalGen_HPP
+#define SignalGen_HPP
 
-#include <Fw/Types/ByteArray.hpp>
-#include <Fw/Types/ConstByteArray.hpp>
-#include <Os/File.hpp>
-#include <Os/ValidateFile.hpp>
-#include <Ref/SignalGen/SignalGenComponentAc.hpp>
-#include <cmath>
+#include "Ref/SignalGen/SignalGenComponentAc.hpp"
 
 namespace Ref {
 
@@ -26,68 +15,78 @@ namespace Ref {
     public SignalGenComponentBase
   {
 
-    private:
-
-        void schedIn_handler(
-            NATIVE_INT_TYPE portNum, /*!< The port number*/
-            NATIVE_UINT_TYPE context /*!< The call order*/
-        );
-
-        void SignalGen_Settings_cmdHandler(
-        FwOpcodeType opCode, /*!< The opcode*/
-        U32 cmdSeq, /*!< The command sequence number*/
-        U32 Frequency,
-        F32 Amplitude,
-        F32 Phase,
-        Ref::SignalType SigType
-        );
-
-        void SignalGen_Toggle_cmdHandler(
-            FwOpcodeType opCode, /*!< The opcode*/
-            U32 cmdSeq /*!< The command sequence number*/
-        );
-        void SignalGen_Skip_cmdHandler(
-        FwOpcodeType opCode, /*!< The opcode*/
-        U32 cmdSeq /*!< The command sequence number*/
-        );
-        void SignalGen_GenerateArray_cmdHandler(
-        FwOpcodeType opCode, /*!< The opcode*/
-        U32 cmdSeq /*!< The command sequence number*/
-        );
-
     public:
-        //! Construct a SignalGen
-        SignalGen(
-            const char* compName //!< The component name
-        );
+
+      // ----------------------------------------------------------------------
+      // Construction, initialization, and destruction
+      // ----------------------------------------------------------------------
+
+      //! Construct object SignalGen
+      //!
+      SignalGen(
+          const char *const compName /*!< The component name*/
+      );
+
+      //! Initialize object SignalGen
+      //!
+      void init(
+          const NATIVE_INT_TYPE queueDepth, /*!< The queue depth*/
+          const NATIVE_INT_TYPE instance = 0 /*!< The instance number*/
+      );
+
+      //! Destroy object SignalGen
+      //!
+      ~SignalGen();
+
+    PRIVATE:
+
+      // ----------------------------------------------------------------------
+      // Handler implementations for user-defined typed input ports
+      // ----------------------------------------------------------------------
+
+      //! Handler implementation for schedIn
+      //!
+      void schedIn_handler(
+          const NATIVE_INT_TYPE portNum, /*!< The port number*/
+          NATIVE_UINT_TYPE context /*!< 
+      The call order
+      */
+      );
+
+    PRIVATE:
+
+      // ----------------------------------------------------------------------
+      // Command handler implementations
+      // ----------------------------------------------------------------------
+
+      //! Implementation for SignalGen_Settings command handler
+      //! Signal Generator Settings
+      void SignalGen_Settings_cmdHandler(
+          const FwOpcodeType opCode, /*!< The opcode*/
+          const U32 cmdSeq, /*!< The command sequence number*/
+          U32 Frequency, 
+          F32 Amplitude, 
+          F32 Phase, 
+          Ref::SignalType SigType 
+      );
+
+      //! Implementation for SignalGen_Toggle command handler
+      //! Toggle Signal Generator On/Off.
+      void SignalGen_Toggle_cmdHandler(
+          const FwOpcodeType opCode, /*!< The opcode*/
+          const U32 cmdSeq /*!< The command sequence number*/
+      );
+
+      //! Implementation for SignalGen_Skip command handler
+      //! Skip next sample
+      void SignalGen_Skip_cmdHandler(
+          const FwOpcodeType opCode, /*!< The opcode*/
+          const U32 cmdSeq /*!< The command sequence number*/
+      );
 
 
+    };
 
-        //! Initialize a SignalGen
-        void init(
-            const NATIVE_INT_TYPE queueDepth, //!< The queue depth
-            const NATIVE_INT_TYPE instance //!< The instance number
-        );
+} // end namespace Ref
 
-        //! Destroy a SignalGen
-        ~SignalGen();
-
-    private:
-        // Generate the next sample internal helper
-        F32 generateSample(U32 ticks);
-
-        // Member variables
-        U32 sampleFrequency;
-        U32 signalFrequency;
-        F32 signalAmplitude;
-        F32 signalPhase;
-        U32 ticks;
-        SignalType sigType;
-        SignalSet sigHistory;
-        SignalPairSet sigPairHistory;
-        bool running;
-        bool skipOne;
-
-  };
-};
 #endif
