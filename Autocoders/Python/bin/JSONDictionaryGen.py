9f4476f950fa9d98b842211c7e4990ab05db65a6
Thibault <70470823+ThibFrgsGmz@users.noreply.github.com>
2022-06-24 23:51:33 +0200

    Refactoring `format_type_item` fom JSONDict py

--- Autocoders/Python/bin/JSONDictionaryGen.py
+++ Autocoders/Python/bin/JSONDictionaryGen.py
@@ -29,22 +29,17 @@ VERSION = Version()
 
 def format_type_item(typeItem):
     typeIsTuple = str(type(typeItem)) == "<type 'tuple'>"
-    if typeIsTuple:
-        description = typeItem[0]
-        values = typeItem[1]
-        valuesArr = []
-        for value in values:
-            valuesArr.append({"name": value[0], "value": value[1], "comment": value[2]})
+    if not typeIsTuple:
+        return typeItem
 
-        typeObj = {
-            "type": description[0],
-            "name": description[1],
-            "values": valuesArr,
-        }
-    else:
-        typeObj = typeItem
+    description = typeItem[0]
+    values = typeItem[1]
+    valuesArr = [
+        {"name": value[0], "value": value[1], "comment": value[2]}
+        for value in values
+    ]
 
-    return typeObj
+    return {"type": description[0], "name": description[1], "values": valuesArr}
 
 
 def pinit():
