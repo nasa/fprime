8f80fbce3b5ff134b8dcf8fbc0ba397a611a5745
bocchino <bocchino@jpl.nasa.gov>
2022-07-05 14:11:06 -0700

    Reformat Framer implementation code

--- Svc/Framer/Framer.cpp
+++ Svc/Framer/Framer.cpp
@@ -23,8 +23,13 @@ namespace Svc {
 // Construction, initialization, and destruction
 // ----------------------------------------------------------------------
 
-Framer ::Framer(const char* const compName) : FramerComponentBase(compName), FramingProtocolInterface(),
-m_protocol(nullptr) {}
+Framer ::Framer(const char* const compName) :
+  FramerComponentBase(compName),
+  FramingProtocolInterface(),
+  m_protocol(nullptr)
+{
+
+}
 
 void Framer ::init(const NATIVE_INT_TYPE instance) {
     FramerComponentBase::init(instance);
@@ -42,23 +47,42 @@ void Framer ::setup(FramingProtocol& protocol) {
 // Handler implementations for user-defined typed input ports
 // ----------------------------------------------------------------------
 
-void Framer ::comIn_handler(const NATIVE_INT_TYPE portNum, Fw::ComBuffer& data, U32 context) {
+void Framer ::comIn_handler(
+    const NATIVE_INT_TYPE portNum,
+    Fw::ComBuffer& data,
+    U32 context
+) {
     FW_ASSERT(m_protocol != nullptr);
-    m_protocol->frame(data.getBuffAddr(), data.getBuffLength(), Fw::ComPacket::FW_PACKET_UNKNOWN);
+    m_protocol->frame(
+        data.getBuffAddr(),
+        data.getBuffLength(),
+        Fw::ComPacket::FW_PACKET_UNKNOWN
+    );
 }
 
-void Framer ::bufferIn_handler(const NATIVE_INT_TYPE portNum, Fw::Buffer& fwBuffer) {
+void Framer ::bufferIn_handler(
+    const NATIVE_INT_TYPE portNum,
+    Fw::Buffer& fwBuffer
+) {
     FW_ASSERT(m_protocol != nullptr);
-    m_protocol->frame(fwBuffer.getData(), fwBuffer.getSize(), Fw::ComPacket::FW_PACKET_FILE);
+    m_protocol->frame(
+        fwBuffer.getData(),
+        fwBuffer.getSize(),
+        Fw::ComPacket::FW_PACKET_FILE
+    );
     bufferDeallocate_out(0, fwBuffer);
 }
 
 void Framer ::send(Fw::Buffer& outgoing) {
     Drv::SendStatus sendStatus = framedOut_out(0, outgoing);
     if (sendStatus.e != Drv::SendStatus::SEND_OK) {
-        // Note: if there is a data sending problem, an EVR likely wouldn't make it down. Log the issue in hopes that
+        // Note: if there is a data sending problem, an EVR likely wouldn't 
+        // make it down. Log the issue in hopes that
         // someone will see it.
-        Fw::Logger::logMsg("[ERROR] Failed to send framed data: %d\n", sendStatus.e);
+        Fw::Logger::logMsg(
+            "[ERROR] Failed to send framed data: %d\n",
+            sendStatus.e
+        );
     }
 }
 
