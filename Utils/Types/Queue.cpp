e9dcd0b837ece8add957b069ca8001900e4bda3f
M Starch <LeStarch@googlemail.com>
2022-07-06 20:39:30 -0700

    lestarch: improving types definitions

--- Utils/Types/Queue.cpp
+++ Utils/Types/Queue.cpp
@@ -8,21 +8,41 @@
  *
  */
 #include "Queue.hpp"
+#include <Fw/Types/Assert.hpp>
 
 namespace Types {
 
-Queue::Queue(U8* const storage, const FwSizeType depth, const FwSizeType message_size)
-    : m_internal(storage, depth * message_size), m_message_size(message_size) {}
+Queue::Queue() : m_internal(), m_message_size(0) {}
 
-Fw::SerializeStatus Queue::enqueue(const U8* const message) {
+void Queue::setup(U8* const storage, const FwSizeType storage_size, const FwSizeType depth, const FwSizeType message_size) {
+    // Ensure that enough storage was supplied
+    const FwSizeType total_needed_size = depth * message_size;
+    FW_ASSERT(storage_size >= total_needed_size, storage_size, depth, message_size);
+    m_internal.setup(storage, total_needed_size);
+}
+
+Fw::SerializeStatus Queue::enqueue(const U8* const message, const FwSizeType size) {
+    FW_ASSERT(m_message_size > 0); // Ensure initialization
+    FW_ASSERT(m_message_size == size, size, m_message_size); // Message size is as expected
     return m_internal.serialize(message, static_cast<NATIVE_UINT_TYPE>(m_message_size));
 }
 
-Fw::SerializeStatus Queue::dequeue(U8* const message) {
+Fw::SerializeStatus Queue::dequeue(U8* const message, const FwSizeType size) {
+    FW_ASSERT(m_message_size > 0); // Ensure initialization
+    FW_ASSERT(m_message_size <= size, size, m_message_size); // Sufficient storage space for read message
     Fw::SerializeStatus result = m_internal.peek(message, static_cast<NATIVE_UINT_TYPE>(m_message_size), 0);
     if (result != Fw::FW_SERIALIZE_OK) {
         return result;
     }
     return m_internal.rotate(m_message_size);
 }
+
+NATIVE_UINT_TYPE Queue::get_high_water_mark() const {
+    return m_internal.get_high_water_mark() / m_message_size;
+}
+
+void Queue::clear_high_water_mark() {
+    m_internal.clear_high_water_mark();
+}
+
 };  // namespace Types
