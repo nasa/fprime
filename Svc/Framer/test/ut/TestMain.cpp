91bc864cb3d13ebcbaacc68a75747c941e4a9c19
bocchino <bocchino@jpl.nasa.gov>
2022-07-28 16:46:43 -0700

    Revise Framer unit tests
    Add comments and requirements to test output

--- Svc/Framer/test/ut/TestMain.cpp
+++ Svc/Framer/test/ut/TestMain.cpp
@@ -2,6 +2,7 @@
 // TestMain.cpp
 // ----------------------------------------------------------------------
 
+#include "Fw/Test/UnitTest.hpp"
 #include "Os/Log.hpp"
 #include "Tester.hpp"
 
@@ -9,13 +10,17 @@
 Os::Log logger;
 
 TEST(Nominal, Com) {
+    COMMENT("Send one Fw::Com buffer to the framer (nominal behavior)");
+    REQUIREMENT("SVC-FRAMER-001");
+    REQUIREMENT("SVC-FRAMER-003");
     Svc::Tester tester;
-    tester.test_com();
-    tester.setSendStatus(Drv::SendStatus::SEND_ERROR);
     tester.test_com();
 }
 
 TEST(Nominal, Buffer) {
+    COMMENT("Send one Fw::Buffer to the framer (nominal behavior)");
+    REQUIREMENT("SVC-FRAMER-002");
+    REQUIREMENT("SVC-FRAMER-003");
     Svc::Tester tester;
     tester.test_buffer();
     tester.setSendStatus(Drv::SendStatus::SEND_ERROR);
@@ -23,11 +28,33 @@ TEST(Nominal, Buffer) {
 }
 
 TEST(Nominal, ManySends) {
+    COMMENT("Send several buffers");
+    REQUIREMENT("SVC-FRAMER-001");
+    REQUIREMENT("SVC-FRAMER-002");
+    REQUIREMENT("SVC-FRAMER-003");
     Svc::Tester tester;
     tester.test_com(27);
     tester.test_buffer(27);
     tester.test_com(31);
     tester.test_buffer(31);
+}
+
+TEST(SendError, Buffer) {
+    COMMENT("Send one Fw::Buffer to the framer (send error)");
+    REQUIREMENT("SVC-FRAMER-002");
+    REQUIREMENT("SVC-FRAMER-003");
+    Svc::Tester tester;
+    tester.setSendStatus(Drv::SendStatus::SEND_ERROR);
+    tester.test_buffer();
+}
+
+TEST(SendError, Com) {
+    COMMENT("Send one Fw::Com buffer to the framer (send error)");
+    REQUIREMENT("SVC-FRAMER-001");
+    REQUIREMENT("SVC-FRAMER-003");
+    Svc::Tester tester;
+    tester.setSendStatus(Drv::SendStatus::SEND_ERROR);
+    tester.test_com();
 }
 
 int main(int argc, char **argv) {
