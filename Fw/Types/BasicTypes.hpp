5c4295e148f1b8fe8d80e16cb85ddf5f1f07653e
M Starch <LeStarch@googlemail.com>
2022-08-02 12:18:23 -0700

    lestarch: refactoring types and configuration header w.r.t type design

--- Fw/Types/BasicTypes.hpp
+++ Fw/Types/BasicTypes.hpp
@@ -13,77 +13,66 @@
 #ifndef FW_BASIC_TYPES_HPP
 #define FW_BASIC_TYPES_HPP
 
-#include <FpConfig.hpp>
+#include <StandardTypes.hpp>
 #ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
 
 #if defined __GNUC__ || __llvm__
-
-// This is used to cast pointers to integers
-// when a pointer needs to be stored generically.
-// In order to avoid chopping off bits,
-// the integer bit size needs to match
-// the pointer bit size.
-
-#ifdef __SIZEOF_POINTER__
- #if __SIZEOF_POINTER__ == 8
-  #define POINTER_CAST U64
- #elif __SIZEOF_POINTER__ == 4
-  #define POINTER_CAST U32
- #elif __SIZEOF_POINTER__ == 2
-  #define POINTER_CAST U16
- #else
-  #define POINTER_CAST U8
- #endif
-#elif defined (__i386) && __i386 == 1 // GCC 4.1.2
-  #define POINTER_CAST U32
-#elif defined (__x86_64) && __x86_64 == 1 // GCC 4.1.2
-  #define POINTER_CAST U64
-#elif defined (CPU) && defined (PPC604) && CPU == PPC604 // VxWorks 6.7 RAD750
-  #define POINTER_CAST U32
-#elif defined (CPU) && defined(SPARC) && CPU == SPARC
-  #define POINTER_CAST U32
 #else
- #error Cannot get size of pointer cast!
+  #error Unsupported compiler!
 #endif
-
-#else
- #error Unsupported compiler!
-#endif
-
-// compile-time assert
-#define COMPILE_TIME_ASSERT( condition, name )\
-  do { \
-   enum { assert_failed_ ## name = 1/(condition) }; \
-  } while(0)
 
 /*----------------------------------------------------------------------------*/
 typedef int8_t          I8; //!< 8-bit signed integer
 typedef uint8_t         U8; //!< 8-bit unsigned integer
 typedef U8              BYTE; //!< byte type
+typedef char CHAR;
+
+const int8_t I8_MIN = INT8_MIN;
+const int8_t I8_MAX = INT8_MAX;
+
+const uint8_t U8_MIN = 0;
+const uint8_t U8_MAX = UINT8_MAX;
 
 #if FW_HAS_16_BIT
- typedef int16_t        I16; //!< 16-bit signed integer
- typedef uint16_t       U16; //!< 16-bit unsigned integer
+    typedef int16_t        I16; //!< 16-bit signed integer
+    typedef uint16_t       U16; //!< 16-bit unsigned integer
+    const I16 I16_MIN = INT16_MIN;
+    const I16 I16_MAX = INT16_MAX;
+    const U16 U16_MIN = 0;
+    const U16 U16_MAX = UINT16_MAX;
 #endif
 
 #if FW_HAS_32_BIT
- typedef uint32_t       U32; //!< 32-bit signed integer
- typedef int32_t        I32; //!< 32-bit unsigned integer
+  typedef uint32_t       U32; //!< 32-bit signed integer
+  typedef int32_t        I32; //!< 32-bit unsigned integer
+  const I32 I32_MIN = INT32_MIN;
+  const I32 I32_MAX = INT32_MAX;
+  const U32 U32_MIN = 0;
+  const U32 U32_MAX = UINT32_MAX;
 #endif
 
 #if FW_HAS_64_BIT
- typedef int64_t        I64; //!< 64-bit signed integer
- typedef uint64_t       U64; //!< 64-bit unsigned integer
+  typedef int64_t        I64; //!< 64-bit signed integer
+  typedef uint64_t       U64; //!< 64-bit unsigned integer
+  const I64 I64_MIN = INT64_MIN;
+  const I64 I64_MAX = INT64_MAX;
+  const U64 U64_MIN = 0;
+  const U64 U64_MAX = UINT64_MAX;
 #endif
 
 typedef float   F32; //!< 32-bit floating point
 #if FW_HAS_F64
- typedef double  F64; //!< 64-bit floating point
+  typedef double  F64; //!< 64-bit floating point
 #endif
 
-typedef char CHAR;
+  // compile-time assert
+#define COMPILE_TIME_ASSERT( condition, name )\
+  do { \
+   enum { assert_failed_ ## name = 1/(condition) }; \
+  } while(0)
+
 
 #define FW_NUM_ARRAY_ELEMENTS(a)  (sizeof(a)/sizeof((a)[0])) //!< number of elements in an array
 
