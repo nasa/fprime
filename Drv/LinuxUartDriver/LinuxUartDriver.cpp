1b0c4f427654aec08eae509aee978c334c9ce50a
M Starch <LeStarch@googlemail.com>
2022-08-07 20:10:52 -0600

    lestarch: moving configuration into open call

--- Drv/LinuxUartDriver/LinuxUartDriver.cpp
+++ Drv/LinuxUartDriver/LinuxUartDriver.cpp
@@ -32,7 +32,7 @@ namespace Drv {
 // ----------------------------------------------------------------------
 
 LinuxUartDriver ::LinuxUartDriver(const char* const compName)
-    : LinuxUartDriverComponentBase(compName), m_fd(-1), m_device("NOT_EXIST"), m_quitReadThread(false) {
+    : LinuxUartDriverComponentBase(compName), m_fd(-1), m_allocationSize(-1),  m_device("NOT_EXIST"), m_quitReadThread(false) {
 }
 
 void LinuxUartDriver ::init(const NATIVE_INT_TYPE instance) {
@@ -42,10 +42,12 @@ void LinuxUartDriver ::init(const NATIVE_INT_TYPE instance) {
 bool LinuxUartDriver::open(const char* const device,
                            UartBaudRate baud,
                            UartFlowControl fc,
-                           UartParity parity) {
+                           UartParity parity,
+                           NATIVE_INT_TYPE allocationSize) {
     FW_ASSERT(device != nullptr);
     NATIVE_INT_TYPE fd = -1;
     NATIVE_INT_TYPE stat = -1;
+    this->m_allocationSize = allocationSize;
 
     this->m_device = device;
 
@@ -310,7 +312,7 @@ void LinuxUartDriver ::serialReadTaskEntry(void* ptr) {
     Drv::RecvStatus status = RecvStatus::RECV_ERROR;  // added by m.chase 03.06.2017
     LinuxUartDriver* comp = reinterpret_cast<LinuxUartDriver*>(ptr);
     while (!comp->m_quitReadThread) {
-        Fw::Buffer buff = comp->allocate_out(0, Drv::UART_READ_ALLOCATION_REQUEST_SIZE);
+        Fw::Buffer buff = comp->allocate_out(0, comp->m_allocationSize);
 
         // On failed allocation, error and deallocate
         if (buff.getData() == nullptr) {
--- Drv/LinuxUartDriver/LinuxUartDriver.hpp
+++ Drv/LinuxUartDriver/LinuxUartDriver.hpp
@@ -14,7 +14,6 @@
 #define LinuxUartDriver_HPP
 
 #include <Drv/LinuxUartDriver/LinuxUartDriverComponentAc.hpp>
-#include <LinuxUartDriverCfg.hpp>
 #include <Os/Mutex.hpp>
 #include <Os/Task.hpp>
 
@@ -44,7 +43,7 @@ class LinuxUartDriver : public LinuxUartDriverComponentBase {
     enum UartParity { PARITY_NONE, PARITY_ODD, PARITY_EVEN };
 
     // Open device with specified baud and flow control.
-    bool open(const char* const device, UartBaudRate baud, UartFlowControl fc, UartParity parity);
+    bool open(const char* const device, UartBaudRate baud, UartFlowControl fc, UartParity parity, NATIVE_INT_TYPE allocationSize);
 
     //! start the serial poll thread.
     //! buffSize is the max receive buffer size
@@ -72,6 +71,7 @@ class LinuxUartDriver : public LinuxUartDriverComponentBase {
 
 
     NATIVE_INT_TYPE m_fd;  //!< file descriptor returned for I/O device
+    NATIVE_INT_TYPE m_allocationSize; //!< size of allocation request to memory manager
     const char* m_device;  //!< original device path
 
     //! This method will be called by the new thread to wait for input on the serial port.
