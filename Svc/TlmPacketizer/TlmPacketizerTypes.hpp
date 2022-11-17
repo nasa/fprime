67c12136e937f2d17155e1cc26d587e827ed4cb1
M Starch <LeStarch@googlemail.com>
2022-11-17 13:05:02 -0800

    formatting telemetry packetizer cpde

--- Svc/TlmPacketizer/TlmPacketizerTypes.hpp
+++ Svc/TlmPacketizer/TlmPacketizerTypes.hpp
@@ -10,7 +10,6 @@
 // ALL RIGHTS RESERVED.  United States Government Sponsorship
 // acknowledged.
 
-
 #ifndef SVC_TLMPACKETIZER_TLMPACKETIZERTYPES_HPP_
 #define SVC_TLMPACKETIZER_TLMPACKETIZERTYPES_HPP_
 
@@ -19,22 +18,22 @@
 
 namespace Svc {
 
-    struct TlmPacketizerChannelEntry {
-        FwChanIdType id;  //!< Id of channel
-        NATIVE_UINT_TYPE size; //!< serialized size of channel in bytes
-    };
+struct TlmPacketizerChannelEntry {
+    FwChanIdType id;        //!< Id of channel
+    NATIVE_UINT_TYPE size;  //!< serialized size of channel in bytes
+};
 
-    struct TlmPacketizerPacket {
-        const TlmPacketizerChannelEntry* list; //!< pointer to a channel entry
-        FwTlmPacketizeIdType id; //!< packet ID
-        NATIVE_UINT_TYPE level; //!< packet level - used to select set of packets to send
-        NATIVE_UINT_TYPE numEntries; //!< number of channels in packet
-    };
+struct TlmPacketizerPacket {
+    const TlmPacketizerChannelEntry* list;  //!< pointer to a channel entry
+    FwTlmPacketizeIdType id;                //!< packet ID
+    NATIVE_UINT_TYPE level;                 //!< packet level - used to select set of packets to send
+    NATIVE_UINT_TYPE numEntries;            //!< number of channels in packet
+};
 
-    struct TlmPacketizerPacketList {
-        const TlmPacketizerPacket* list[MAX_PACKETIZER_PACKETS]; //!< 
-        NATIVE_UINT_TYPE numEntries;
-    };
-}
+struct TlmPacketizerPacketList {
+    const TlmPacketizerPacket* list[MAX_PACKETIZER_PACKETS];  //!<
+    NATIVE_UINT_TYPE numEntries;
+};
+}  // namespace Svc
 
 #endif /* SVC_TLMPACKETIZER_TLMPACKETIZERTYPES_HPP_ */
--- Svc/TlmPacketizer/test/ut/main.cpp
+++ Svc/TlmPacketizer/test/ut/main.cpp
@@ -3,64 +3,55 @@
 // ALL RIGHTS RESERVED.  United States Government Sponsorship
 // acknowledged.
 
-
-#include "Tester.hpp"
 #include <gtest/gtest.h>
 #include <Fw/Test/UnitTest.hpp>
+#include "Tester.hpp"
 
-TEST(TestNominal,Initialization) {
-
-    TEST_CASE(100.1.1,"Initialization");
+TEST(TestNominal, Initialization) {
+    TEST_CASE(100.1.1, "Initialization");
     Svc::Tester tester;
     tester.initTest();
 }
 
-
-TEST(TestNominal,PushTlm) {
-
-    TEST_CASE(100.1.2,"Push Telemetry");
+TEST(TestNominal, PushTlm) {
+    TEST_CASE(100.1.2, "Push Telemetry");
     Svc::Tester tester;
     tester.pushTlmTest();
 }
 
-TEST(TestNominal,SendPackets) {
-
-    TEST_CASE(100.1.2,"Send Packets");
+TEST(TestNominal, SendPackets) {
+    TEST_CASE(100.1.2, "Send Packets");
     Svc::Tester tester;
     tester.sendPacketsTest();
 }
 
-//TEST(TestNominal,SendPacketLevels) {
+// TEST(TestNominal,SendPacketLevels) {
 //
-//    TEST_CASE(100.1.3,"Send Packets with levels");
-//    Svc::Tester tester;
-//    tester.sendPacketLevelsTest();
-//}
+//     TEST_CASE(100.1.3,"Send Packets with levels");
+//     Svc::Tester tester;
+//     tester.sendPacketLevelsTest();
+// }
 
-TEST(TestNominal,UpdatePacketsTest) {
-
-    TEST_CASE(100.1.4,"Update Packets");
+TEST(TestNominal, UpdatePacketsTest) {
+    TEST_CASE(100.1.4, "Update Packets");
     Svc::Tester tester;
     tester.updatePacketsTest();
 }
 
-TEST(TestNominal,PingTest) {
-
-    TEST_CASE(100.1.5,"Ping");
+TEST(TestNominal, PingTest) {
+    TEST_CASE(100.1.5, "Ping");
     Svc::Tester tester;
     tester.pingTest();
 }
 
-TEST(TestNominal,IgnoredChannelTest) {
-
-    TEST_CASE(100.1.6,"Ignored Channels");
+TEST(TestNominal, IgnoredChannelTest) {
+    TEST_CASE(100.1.6, "Ignored Channels");
     Svc::Tester tester;
     tester.ignoreTest();
 }
 
-TEST(TestNominal,SendPacketTest) {
-
-    TEST_CASE(100.1.7,"Manually sent packets");
+TEST(TestNominal, SendPacketTest) {
+    TEST_CASE(100.1.7, "Manually sent packets");
     Svc::Tester tester;
     tester.sendManualPacketTest();
 }
@@ -72,9 +63,8 @@ TEST(TestNominal,SetPacketLevelTest) {
     tester.setPacketLevelTest();
 }
 #endif
-TEST(TestOffNominal,NonPacketizedChannelTest) {
-
-    TEST_CASE(100.2.1,"Non-packetized Channels");
+TEST(TestOffNominal, NonPacketizedChannelTest) {
+    TEST_CASE(100.2.1, "Non-packetized Channels");
     Svc::Tester tester;
     tester.nonPacketizedChannelTest();
 }
