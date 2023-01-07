468713abbd8eba861ca38dce858c9e8730fe330a
ThibFrgsGmz <farges.thibault@gmail.com>
2023-01-07 01:06:50 +0100

    Clang-formatting

--- Os/Linux/SystemResources.cpp
+++ Os/Linux/SystemResources.cpp
@@ -9,10 +9,10 @@
 // acknowledged.
 //
 // ======================================================================
-#include <cstdio>
 #include <array>
-#include <sys/sysinfo.h>
+#include <cstdio>
 #include <cstring>
+#include <sys/sysinfo.h>
 #include <Os/SystemResources.hpp>
 #include <Fw/Types/Assert.hpp>
 
@@ -23,16 +23,16 @@ std::array<char, LINE_SIZE> proc_stat_line;
 
 namespace Os {
 
-    SystemResources::SystemResourcesStatus SystemResources::getCpuCount(U32 &cpuCount) {
+    SystemResources::SystemResourcesStatus SystemResources::getCpuCount(U32& cpuCount) {
         cpuCount = get_nprocs();
         return SYSTEM_RESOURCES_OK;
     }
 
     SystemResources::SystemResourcesStatus SystemResources::getCpuTicks(CpuTicks &cpu_ticks, U32 cpu_index) {
-        FILE *fp = nullptr;
-        std::array<U32, 4> cpu_data = {0};
-        U32 cpuCount = 0;
         SystemResources::SystemResourcesStatus status  = SYSTEM_RESOURCES_ERROR;
+        std::array<U32, 4> cpu_data = {0};
+        FILE* fp = nullptr;
+        U32 cpuCount = 0;
         U64 cpuUsed = 0;
         U64 cpuTotal = 0;
 
@@ -45,27 +45,27 @@ namespace Os {
         if ((fp = fopen(PROC_STAT_PATH, READ_ONLY)) == nullptr) {
             return SYSTEM_RESOURCES_ERROR;
         }
-        if (fgets(proc_stat_line.data(), proc_stat_line.size(), fp) == nullptr) { //1st line.  Aggregate cpu line.
+        if (fgets(proc_stat_line.data(), proc_stat_line.size(), fp) == nullptr) {  // 1st line.  Aggregate cpu line.
             fclose(fp);
             return SYSTEM_RESOURCES_ERROR;
         }
 
         for (U32 i = 0; i < cpu_index + 1; i++) {
-            if (fgets(proc_stat_line.data(), proc_stat_line.size(), fp) == nullptr) { //cpu# line
+            if (fgets(proc_stat_line.data(), proc_stat_line.size(), fp) == nullptr) {  // cpu# line
                 fclose(fp);
                 return SYSTEM_RESOURCES_ERROR;
             }
-            if (i != cpu_index) { continue; }
+            if (i != cpu_index) {
+                continue;
+            }
 
             if (strncmp(proc_stat_line.data(), "cpu", 3) != 0) {
                 fclose(fp);
                 return SYSTEM_RESOURCES_ERROR;
             }
             // No string concerns, as string is discarded
-            sscanf(proc_stat_line.data(), "%*s %d %d %d %d", &cpu_data[0],
-                   &cpu_data[1],
-                   &cpu_data[2],
-                   &cpu_data[3]); //cpu#: 4 numbers: usr, nice, sys, idle
+            sscanf(proc_stat_line.data(), "%*s %d %d %d %d", &cpu_data[0], &cpu_data[1], &cpu_data[2],
+                   &cpu_data[3]);  // cpu#: 4 numbers: usr, nice, sys, idle
 
             cpuUsed = cpu_data[0] + cpu_data[1] + cpu_data[2];
             cpuTotal = cpu_data[0] + cpu_data[1] + cpu_data[2] + cpu_data[3];
@@ -80,12 +80,11 @@ namespace Os {
     }
 
     SystemResources::SystemResourcesStatus SystemResources::getMemUtil(MemUtil &memory_util) {
-
         struct sysinfo memory_info;
 
         if (sysinfo(&memory_info) != 0) {
             return SYSTEM_RESOURCES_ERROR;
-        }       
+        }
 
         const FwSizeType total_ram = static_cast<FwSizeType>(memory_info.totalram);
         const FwSizeType free_ram = static_cast<FwSizeType>(memory_info.freeram);
@@ -94,8 +93,7 @@ namespace Os {
         // Check for casting and type errors
         if (((total_ram <= 0) || (static_cast<unsigned long>(total_ram) != memory_info.totalram)) ||
             ((free_ram <= 0) || (static_cast<unsigned long>(free_ram) != memory_info.freeram)) ||
-            ((memory_unit <= 0) || (static_cast<unsigned int>(memory_unit) != memory_info.mem_unit))
-        ) {
+            ((memory_unit <= 0) || (static_cast<unsigned int>(memory_unit) != memory_info.mem_unit))) {
             return SYSTEM_RESOURCES_ERROR;
         }
 
@@ -105,8 +103,7 @@ namespace Os {
         }
 
         // Check for overflow in multiplication
-        if (total_ram > (FpLimits::FwSizeType_MAX / memory_unit))
-        {
+        if (total_ram > (FpLimits::FwSizeType_MAX / memory_unit)) {
             return SYSTEM_RESOURCES_ERROR;
         }
 
