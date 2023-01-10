b61ab25b9136cb7e1854945d32074882de5b2256
Timothy Canham <timothy.canham@jpl.nasa.gov>
2023-01-09 17:28:29 -0800

    Fixed merge conflict check-ins

--- Svc/TlmChan/test/ut/Main.cpp
+++ Svc/TlmChan/test/ut/Main.cpp
@@ -1,30 +1,11 @@
 /*
-<<<<<<< HEAD
- * PrmDbTester.cpp
-=======
  * Main.cpp
->>>>>>> d60a606905dd70cc9b8024b95ca6eeab605976e8
  *
  *  Created on: Mar 18, 2015
  *  Updated: 6/22/2022
  *      Author: tcanham
  */
 
-<<<<<<< HEAD
-#include <Svc/TlmChan/test/ut/Tester.hpp>
-#include <Svc/TlmChan/TlmChan.hpp>
-#include <gtest/gtest.h>
-#include <Fw/Test/UnitTest.hpp>
-
-
-TEST(TlmChanTest,InitTest) {
-    Svc::Tester tester;
-}
-
-TEST(TlmChanTest,NominalChannelTest) {
-
-    TEST_CASE(107.1.1,"Nominal channelized telemetry");
-=======
 #include <gtest/gtest.h>
 #include <Fw/Test/UnitTest.hpp>
 #include <Svc/TlmChan/TlmChan.hpp>
@@ -36,55 +17,31 @@ TEST(TlmChanTest, InitTest) {
 
 TEST(TlmChanTest, NominalChannelTest) {
     TEST_CASE(107.1.1, "Nominal channelized telemetry");
->>>>>>> d60a606905dd70cc9b8024b95ca6eeab605976e8
     COMMENT("Write a single channel and verify it is read back and pushed correctly.");
 
     Svc::Tester tester;
     // run test
     tester.runNominalChannel();
-<<<<<<< HEAD
-
-}
-
-TEST(TlmChanTest,MultiChannelTest) {
-
-    TEST_CASE(107.1.2,"Nominal Multi-channel channelized telemetry");
-=======
 }
 
 TEST(TlmChanTest, MultiChannelTest) {
     TEST_CASE(107.1.2, "Nominal Multi-channel channelized telemetry");
->>>>>>> d60a606905dd70cc9b8024b95ca6eeab605976e8
     COMMENT("Write multiple channels and verify they are read back and pushed correctly.");
 
     Svc::Tester tester;
 
     // run test
     tester.runMultiChannel();
-<<<<<<< HEAD
-
-}
-
-
-TEST(TlmChanTest,OffNominal) {
-
-    TEST_CASE(107.2.1,"Off-nominal channelized telemetry");
-=======
 }
 
 TEST(TlmChanTest, OffNominal) {
     TEST_CASE(107.2.1, "Off-nominal channelized telemetry");
->>>>>>> d60a606905dd70cc9b8024b95ca6eeab605976e8
     COMMENT("Attempt to read a channel that hasn't been written.");
 
     Svc::Tester tester;
 
     // run test
     tester.runOffNominal();
-<<<<<<< HEAD
-
-=======
->>>>>>> d60a606905dd70cc9b8024b95ca6eeab605976e8
 }
 
 // TEST(TlmChanTest,TooManyChannels) {
@@ -106,20 +63,3 @@ TEST(TlmChanTest, OffNominal) {
 //     tester.runTooManyChannels();
 
 // }
-
-<<<<<<< HEAD
-
-
-=======
->>>>>>> d60a606905dd70cc9b8024b95ca6eeab605976e8
-#ifndef TGT_OS_TYPE_VXWORKS
-int main(int argc, char* argv[]) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
-
-#endif
-<<<<<<< HEAD
-
-=======
->>>>>>> d60a606905dd70cc9b8024b95ca6eeab605976e8
--- Svc/TlmChan/test/ut/Tester.hpp
+++ Svc/TlmChan/test/ut/Tester.hpp
@@ -10,103 +10,6 @@
 #include "GTestBase.hpp"
 #include "Svc/TlmChan/TlmChan.hpp"
 
-<<<<<<< HEAD
-namespace Svc
-{
-
-    class Tester : public TlmChanGTestBase
-    {
-
-        // ----------------------------------------------------------------------
-        // Construction and destruction
-        // ----------------------------------------------------------------------
-
-    public:
-        //! Construct object Tester
-        //!
-        Tester();
-
-        //! Destroy object Tester
-        //!
-        ~Tester();
-
-    public:
-        // ----------------------------------------------------------------------
-        // Tests
-        // ----------------------------------------------------------------------
-
-        void runNominalChannel();
-        void runMultiChannel();
-        void runOffNominal();
-
-    private:
-        // ----------------------------------------------------------------------
-        // Handlers for typed from ports
-        // ----------------------------------------------------------------------
-
-        //! Handler for from_PktSend
-        //!
-        void from_PktSend_handler(
-            const NATIVE_INT_TYPE portNum, /*!< The port number*/
-            Fw::ComBuffer &data,           /*!<
-                  Buffer containing packet data
-                  */
-            U32 context                    /*!<
-                           Call context value; meaning chosen by user
-                           */
-        );
-
-        //! Handler for from_pingOut
-        //!
-        void from_pingOut_handler(
-            const NATIVE_INT_TYPE portNum, /*!< The port number*/
-            U32 key                        /*!<
-                               Value to return to pinger
-                               */
-        );
-
-    private:
-        // ----------------------------------------------------------------------
-        // Helper methods
-        // ----------------------------------------------------------------------
-
-        //! Connect ports
-        //!
-        void connectPorts();
-
-        //! Initialize components
-        //!
-        void initComponents();
-
-        void sendBuff(FwChanIdType id, U32 val);
-        bool doRun(bool check);
-        void checkBuff(NATIVE_UINT_TYPE chanNum, NATIVE_UINT_TYPE totalChan, FwChanIdType id, U32 val);
-
-        void clearBuffs();
-
-        // dump functions
-        void dumpHash();
-        static void dumpTlmEntry(TlmChan::TlmEntry* entry);
-
-
-    private:
-        // ----------------------------------------------------------------------
-        // Variables
-        // ----------------------------------------------------------------------
-
-        //! The component under test
-        //!
-        TlmChan component;
-        // Keep a history
-        NATIVE_UINT_TYPE m_numBuffs;
-        Fw::ComBuffer m_rcvdBuffer[TLMCHAN_HASH_BUCKETS];
-        bool m_bufferRecv;
-
-
-    };
-
-} // end namespace Svc
-=======
 namespace Svc {
 
 class Tester : public TlmChanGTestBase {
@@ -194,6 +97,5 @@ class Tester : public TlmChanGTestBase {
 };
 
 }  // end namespace Svc
->>>>>>> d60a606905dd70cc9b8024b95ca6eeab605976e8
 
 #endif
