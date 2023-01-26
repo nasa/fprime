af9d5d68ae19354f437ae2ae647466b71edf36cd
Rob Bocchino <bocchino@icloud.com>
2023-01-25 17:41:46 -0800

    Update FatalHandlerComponentLinuxImpl.cpp
    Remove reference to seg fault from diagnostic output.

--- Svc/FatalHandler/FatalHandlerComponentLinuxImpl.cpp
+++ Svc/FatalHandler/FatalHandlerComponentLinuxImpl.cpp
@@ -28,7 +28,7 @@ namespace Svc {
         // for **nix, delay then exit with error code
         Fw::Logger::logMsg("FATAL %d handled.\n",Id,0,0,0,0,0);
         (void)Os::Task::delay(1000);
-        Fw::Logger::logMsg("Exiting with segfault and core dump file.\n",0,0,0,0,0,0);
+        Fw::Logger::logMsg("Exiting with abort signal and core dump file.\n",0,0,0,0,0,0);
         (void)raise( SIGABRT );
         exit(1);
     }
