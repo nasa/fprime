ce6cac829d8ac2784720dc3eeea5ee00900befbc
EbenezerA99 <60404240+EbenezerA99@users.noreply.github.com>
2023-02-22 13:03:14 -0500

    Add standardization header for PolyDb (#1898)

---
+++ Svc/PolyDb/PolyDb.hpp
@@ -0,0 +1,17 @@
+// ======================================================================
+// PolyDb.hpp
+// Standardization header for PolyDb
+// ======================================================================
+
+#ifndef Svc_PolyDb_HPP
+#define Svc_PolyDb_HPP
+
+#include "Svc/PolyDb/PolyDbImpl.hpp"
+
+namespace Svc {
+
+  typedef PolyDbImpl PolyDb;
+
+}
+
+#endif
