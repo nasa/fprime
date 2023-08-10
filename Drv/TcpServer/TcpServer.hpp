beb946320f70d43c00d4c8be8f8c508fb04da5bc
thomas-bc <thomas.boyerchammard@gmail.com>
2023-08-10 10:00:22 -0700

    Add TcpServer standardization header

---
+++ Drv/TcpServer/TcpServer.hpp
@@ -0,0 +1,17 @@
+// ======================================================================
+// TcpServer.hpp
+// Standardization header for TcpServer
+// ======================================================================
+
+#ifndef Drv_TcpServer_HPP
+#define Drv_TcpServer_HPP
+
+#include "Drv/TcpServer/TcpServerComponentImpl.hpp"
+
+namespace Drv {
+
+  typedef TcpServerComponentImpl TcpServer;
+
+}
+
+#endif
