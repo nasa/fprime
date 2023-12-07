c968928f2f6215bbccfbd18ff894fe20e5b212bf
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2023-12-07 11:58:23 -0800

    Fixing improper user of CMAKE_SYSTEM_NAME

--- cmake/API.cmake
+++ cmake/API.cmake
@@ -31,9 +31,9 @@ set(FPRIME_AUTOCODER_TARGET_LIST "" CACHE INTERNAL "FPRIME_AUTOCODER_TARGET_LIST
 #####
 macro(restrict_platforms)
     set(__CHECKER ${ARGN})
-    if (NOT FPRIME_TOOLCHAIN_NAME IN_LIST __CHECKER AND NOT CMAKE_SYSTEM_NAME IN_LIST __CHECKER)
+    if (NOT FPRIME_TOOLCHAIN_NAME IN_LIST __CHECKER AND NOT FPRIME_PLATFORM IN_LIST __CHECKER)
         get_module_name("${CMAKE_CURRENT_LIST_DIR}")
-        message(STATUS "Neither toolchain ${FPRIME_TOOLCHAIN_NAME} nor platform ${CMAKE_SYSTEM_NAME} supported for module ${MODULE_NAME}")
+        message(STATUS "Neither toolchain ${FPRIME_TOOLCHAIN_NAME} nor platform ${FPRIME_PLATFORM} supported for module ${MODULE_NAME}")
         return()
     endif()
 endmacro()
