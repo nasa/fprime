4738c07089a56424e0e32ed8208b549d71724081
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2024-02-08 13:14:23 -0800

    Fixing implementation to use source files

--- cmake/API.cmake
+++ cmake/API.cmake
@@ -617,6 +617,7 @@ function(register_fprime_implementation IMPLEMENTATION IMPLEMENTOR)
     resolve_dependencies(IMPLEMENTOR "${IMPLEMENTOR}")
     create_implementation_interface("${IMPLEMENTATION}")
     append_list_property("${IMPLEMENTOR}" TARGET "${IMPLEMENTATION}" PROPERTY "IMPLEMENTORS")
+    append_list_property("${ARGN}" TARGET "${IMPLEMENTOR}" PROPERTY "REQUIRED_SOURCE_FILES")
     add_dependencies("${IMPLEMENTATION}" "${IMPLEMENTOR}")
 endfunction()
 
