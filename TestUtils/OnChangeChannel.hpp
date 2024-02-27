846cbc1e5d210dae78e156f79136cca4e798c507
Robert L. Bocchino Jr <bocchino@jpl.nasa.gov>
2024-02-26 16:20:26 -0800

    Add explicit keyword

--- TestUtils/OnChangeChannel.hpp
+++ TestUtils/OnChangeChannel.hpp
@@ -28,7 +28,7 @@ template <typename T>
 class OnChangeChannel {
   public:
     //! Constructor
-    OnChangeChannel(T value) : value(value) {}
+    explicit OnChangeChannel(T value) : value(value) {}
     //! Update the previous value
     OnChangeStatus updatePrev() {
         const auto status = ((!this->prev.hasValue()) || (this->value != this->prev.get()))
