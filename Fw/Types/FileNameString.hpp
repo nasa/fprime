327063b0684d892581eff9bc29f150bab1ae45da
Robert L. Bocchino Jr <bocchino@jpl.nasa.gov>
2024-03-12 18:26:28 -0700

    Revise FileNameString

--- Fw/Types/FileNameString.hpp
+++ Fw/Types/FileNameString.hpp
@@ -8,31 +8,29 @@
 
 namespace Fw {
 
-    class FileNameString : public Fw::StringBase {
-        public:
-
-            enum {
-                SERIALIZED_TYPE_ID = FW_TYPEID_FILE_NAME_STRING, //!< typeid for string type
-                STRING_SIZE = FileNameStringSize, //!< Storage for string
-                SERIALIZED_SIZE = STRING_SIZE + sizeof(FwBuffSizeType) //!< Serialized size is size of buffer + size field
-            };
-
-            FileNameString(const char* src); //!< char* source constructor
-            FileNameString(const StringBase& src); //!< other string constructor
-            FileNameString(const FileNameString& src); //!< String string constructor
-            FileNameString(); //!< default constructor
-            FileNameString& operator=(const FileNameString& other); //!< assignment operator
-            FileNameString& operator=(const StringBase& other); //!< other string assignment operator
-            FileNameString& operator=(const char* other); //!< char* assignment operator
-            ~FileNameString(); //!< destructor
-
-            const char* toChar() const; //!< gets char buffer
-            NATIVE_UINT_TYPE getCapacity() const ; //!< return buffer size
-
-        private:
-
-            char m_buf[STRING_SIZE]; //!< storage for string data
+class FileNameString : public Fw::StringBase {
+  public:
+    enum {
+        SERIALIZED_TYPE_ID = FW_TYPEID_FILE_NAME_STRING,        //!< typeid for string type
+        STRING_SIZE = FileNameStringSize,                       //!< Storage for string
+        SERIALIZED_SIZE = STRING_SIZE + sizeof(FwBuffSizeType)  //!< Serialized size is size of buffer + size field
     };
-}
+
+    explicit FileNameString(const char* src);                //!< char* source constructor
+    explicit FileNameString(const StringBase& src);          //!< other string constructor
+    explicit FileNameString(const FileNameString& src);      //!< String string constructor
+    FileNameString();                                        //!< default constructor
+    FileNameString& operator=(const FileNameString& other);  //!< assignment operator
+    FileNameString& operator=(const StringBase& other);      //!< other string assignment operator
+    FileNameString& operator=(const char* other);            //!< char* assignment operator
+    ~FileNameString();                                       //!< destructor
+
+    const char* toChar() const;            //!< gets char buffer
+    NATIVE_UINT_TYPE getCapacity() const;  //!< return buffer size
+
+  private:
+    char m_buf[STRING_SIZE];  //!< storage for string data
+};
+}  // namespace Fw
 
 #endif
