91e78f963b9d42aef273f7ecd93abff07a4d2d62
Robert L. Bocchino Jr. <bocchino@jpl.nasa.gov>
2024-04-12 16:16:36 -0700

    Revise LogString

--- Fw/Log/LogString.hpp
+++ Fw/Log/LogString.hpp
@@ -1,41 +1,44 @@
+// ======================================================================
+// @file   LogString.hpp
+// @author F Prime
+// @brief  A string sized for an event log entry
+// ======================================================================
+
 #ifndef FW_LOG_STRING_TYPE_HPP
 #define FW_LOG_STRING_TYPE_HPP
 
 #include <FpConfig.hpp>
-#include <Fw/Types/StringType.hpp>
-#include <Fw/Cfg/SerIds.hpp>
+
+#include "Fw/Cfg/SerIds.hpp"
+#include "Fw/Types/ExternalString.hpp"
 
 namespace Fw {
 
-    class LogStringArg : public Fw::StringBase {
-        public:
-
-            enum {
-                SERIALIZED_TYPE_ID = FW_TYPEID_LOG_STR,
-                SERIALIZED_SIZE = FW_LOG_STRING_MAX_SIZE + sizeof(FwBuffSizeType) // size of buffer + storage of two size words
-            };
-
-            LogStringArg();
-            LogStringArg(const LogStringArg& src); //!< LogStringArg string constructor
-            LogStringArg(const StringBase& src); //!< other string constructor
-            LogStringArg(const char* src); //!< char* source constructor
-            LogStringArg& operator=(const LogStringArg& other); //!< assignment operator
-            LogStringArg& operator=(const StringBase& other); //!< other string assignment operator
-            LogStringArg& operator=(const char* other); //!< char* assignment operator
-            ~LogStringArg();
-
-            const char* toChar() const override;
-            NATIVE_UINT_TYPE getCapacity() const override;
-
-            SerializeStatus serialize(SerializeBufferBase& buffer) const override; //!< serialization function
-            SerializeStatus serialize(SerializeBufferBase& buffer, NATIVE_UINT_TYPE maxLen) const override; //!< serialization function
-            SerializeStatus deserialize(SerializeBufferBase& buffer) override; //!< deserialization function
-
-        private:
-
-            char m_buf[FW_LOG_STRING_MAX_SIZE];
+class LogStringArg : public ExternalString {
+  public:
+    enum {
+        SERIALIZED_TYPE_ID = FW_TYPEID_LOG_STR,
+        SERIALIZED_SIZE = FW_LOG_STRING_MAX_SIZE + sizeof(FwSizeStoreType)
     };
 
-}
+    //!< zero-argument constructor
+    LogStringArg() : ExternalString(this->m_buf, sizeof this->m_buf) {}
+
+    //! const LogStringArg& constructor
+    LogStringArg(const LogStringArg& src) : ExternalString(this->m_buf, sizeof this->m_buf, src) {}
+
+    //! const StringBase& constructor
+    LogStringArg(const StringBase& src) : ExternalString(this->m_buf, sizeof this->m_buf, src) {}
+
+    //!< const char* source constructor
+    LogStringArg(const char* src) : ExternalString(this->m_buf, sizeof this->m_buf, src) {}
+
+    //! destructor
+    ~LogStringArg() {}
+
+  private:
+    char m_buf[FW_LOG_STRING_MAX_SIZE];  //!< storage for string data
+};
+}  // namespace Fw
 
 #endif
