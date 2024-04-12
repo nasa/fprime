d3c59dbbc983638a2e35cf0b21b4e9aba7696168
Robert L. Bocchino Jr. <bocchino@jpl.nasa.gov>
2024-04-12 16:34:54 -0700

    Revise TlmString

--- Fw/Tlm/TlmString.hpp
+++ Fw/Tlm/TlmString.hpp
@@ -1,39 +1,44 @@
+// ======================================================================
+// @file   TlmString.hpp
+// @author F Prime
+// @brief  A string sized for a telemetry channel
+// ======================================================================
+
 #ifndef FW_TLM_STRING_TYPE_HPP
 #define FW_TLM_STRING_TYPE_HPP
 
 #include <FpConfig.hpp>
-#include <Fw/Types/StringType.hpp>
-#include <Fw/Cfg/SerIds.hpp>
+
+#include "Fw/Cfg/SerIds.hpp"
+#include "Fw/Types/ExternalString.hpp"
 
 namespace Fw {
 
-    class TlmString : public Fw::StringBase {
-        public:
-
-            enum {
-                SERIALIZED_TYPE_ID = FW_TYPEID_TLM_STR,
-                SERIALIZED_SIZE = FW_TLM_STRING_MAX_SIZE + sizeof(FwBuffSizeType) // size of buffer + storage of size word
-            };
-
-            TlmString();
-            TlmString(const TlmString& src); //!< TlmString string constructor
-            TlmString(const StringBase& src); //!< other string constructor
-            TlmString(const char* src); //!< char* source constructor
-            TlmString& operator=(const TlmString& other); //!< assignment operator
-            TlmString& operator=(const StringBase& other); //!< other string assignment operator
-            TlmString& operator=(const char* other); //!< char* assignment operator
-            ~TlmString();
-
-            const char* toChar() const override;
-            NATIVE_UINT_TYPE getCapacity() const override;
-
-            SerializeStatus serialize(SerializeBufferBase& buffer) const override; //!< serialization function
-            SerializeStatus serialize(SerializeBufferBase& buffer, NATIVE_UINT_TYPE maxLen) const override; //!< serialization function
-            SerializeStatus deserialize(SerializeBufferBase& buffer) override; //!< deserialization function
-
-        private:
-            char m_buf[FW_TLM_STRING_MAX_SIZE];
+class TlmString : public ExternalString {
+  public:
+    enum {
+        SERIALIZED_TYPE_ID = FW_TYPEID_TLM_STR,
+        SERIALIZED_SIZE = FW_TLM_STRING_MAX_SIZE + sizeof(FwSizeStoreType)
     };
-}
+
+    //!< zero-argument constructor
+    TlmString() : ExternalString(this->m_buf, sizeof this->m_buf) {}
+
+    //! const TlmString& constructor
+    TlmString(const TlmString& src) : ExternalString(this->m_buf, sizeof this->m_buf, src) {}
+
+    //! const StringBase& constructor
+    TlmString(const StringBase& src) : ExternalString(this->m_buf, sizeof this->m_buf, src) {}
+
+    //!< const char* source constructor
+    TlmString(const char* src) : ExternalString(this->m_buf, sizeof this->m_buf, src) {}
+
+    //! destructor
+    ~TlmString() {}
+
+  private:
+    char m_buf[FW_TLM_STRING_MAX_SIZE];  //!< storage for string data
+};
+}  // namespace Fw
 
 #endif
