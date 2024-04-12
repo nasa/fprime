24a92429ef618345c62cee1c4364f2c115334f83
Robert L. Bocchino Jr. <bocchino@jpl.nasa.gov>
2024-04-12 16:19:42 -0700

    Revise PrmString

--- Fw/Prm/PrmString.hpp
+++ Fw/Prm/PrmString.hpp
@@ -1,37 +1,44 @@
-#ifndef FW_PRM_STRING_TYPE_HPP
-#define FW_PRM_STRING_TYPE_HPP
+// ======================================================================
+// @file   PrmString.hpp
+// @author F Prime
+// @brief  A string sized for a parameter
+// ======================================================================
+
+#ifndef FW_PARAM_STRING_HPP
+#define FW_PARAM_STRING_HPP
 
 #include <FpConfig.hpp>
-#include <Fw/Types/StringType.hpp>
-#include <Fw/Cfg/SerIds.hpp>
+
+#include "Fw/Cfg/SerIds.hpp"
+#include "Fw/Types/ExternalString.hpp"
 
 namespace Fw {
 
-    class ParamString : public Fw::StringBase {
-        public:
-
-            enum {
-                SERIALIZED_TYPE_ID = FW_TYPEID_PRM_STR,
-                SERIALIZED_SIZE = FW_PARAM_STRING_MAX_SIZE + sizeof(FwBuffSizeType) // size of buffer + storage of two size words
-            };
-
-            ParamString(const char* src);
-            ParamString(const StringBase& src);
-            ParamString(const ParamString& src);
-            ParamString();
-            ParamString& operator=(const ParamString& other);
-            ParamString& operator=(const StringBase& other);
-            ParamString& operator=(const char* other);
-            ~ParamString();
-
-            const char* toChar() const;
-            NATIVE_UINT_TYPE getCapacity() const;
-
-        private:
-
-            char m_buf[FW_PARAM_STRING_MAX_SIZE];
+class ParamString : public ExternalString {
+  public:
+    enum {
+        SERIALIZED_TYPE_ID = FW_TYPEID_PRM_STR,
+        SERIALIZED_SIZE = FW_PARAM_STRING_MAX_SIZE + sizeof(FwSizeStoreType)
     };
 
-}
+    //!< zero-argument constructor
+    ParamString() : ExternalString(this->m_buf, sizeof this->m_buf) {}
+
+    //! const ParamString& constructor
+    ParamString(const ParamString& src) : ExternalString(this->m_buf, sizeof this->m_buf, src) {}
+
+    //! const StringBase& constructor
+    ParamString(const StringBase& src) : ExternalString(this->m_buf, sizeof this->m_buf, src) {}
+
+    //!< const char* source constructor
+    ParamString(const char* src) : ExternalString(this->m_buf, sizeof this->m_buf, src) {}
+
+    //! destructor
+    ~ParamString() {}
+
+  private:
+    char m_buf[FW_PARAM_STRING_MAX_SIZE];  //!< storage for string data
+};
+}  // namespace Fw
 
 #endif
