5b9a4de573c039f2ccf28a882ca8fc1030ba6e1f
Robert L. Bocchino Jr. <bocchino@jpl.nasa.gov>
2024-04-12 16:46:40 -0700

    Revise Os/TaskString

--- Os/TaskString.hpp
+++ Os/TaskString.hpp
@@ -1,32 +1,38 @@
-#ifndef OS_TASK_STRING_TYPE_HPP
-#define OS_TASK_STRING_TYPE_HPP
+// ======================================================================
+// @file   TaskString.hpp
+// @author F Prime
+// @brief  A string sized for an OS task name
+// ======================================================================
+
+#ifndef OS_TASK_STRING_HPP
+#define OS_TASK_STRING_HPP
 
 #include <FpConfig.hpp>
-#include <Fw/Types/StringType.hpp>
+
+#include "Fw/Types/ExternalString.hpp"
 
 namespace Os {
 
-    class TaskString : public Fw::StringBase {
-        public:
+class TaskString : public Fw::ExternalString {
+  public:
+    //!< zero-argument constructor
+    TaskString() : Fw::ExternalString(this->m_buf, sizeof this->m_buf) {}
 
-            TaskString(const char* src); //!< char buffer constructor
-            TaskString(const StringBase& src); //!< Copy constructor
-            TaskString(const TaskString& src); //!< Copy constructor
-            TaskString(); //!< default constructor
-            TaskString& operator=(const TaskString& other); //!< assignment operator
-            TaskString& operator=(const StringBase& other); //!< other string assignment operator
-            TaskString& operator=(const char* other); //!< char* assignment operator
-            ~TaskString(); //!< destructor
+    //! const TaskString& constructor
+    TaskString(const TaskString& src) : Fw::ExternalString(this->m_buf, sizeof this->m_buf, src) {}
 
-            const char* toChar() const; //!< get pointer to internal char buffer
-            NATIVE_UINT_TYPE getCapacity() const; //!< return buffer size
+    //! const StringBase& constructor
+    TaskString(const Fw::StringBase& src) : Fw::ExternalString(this->m_buf, sizeof this->m_buf, src) {}
 
-        private:
+    //!< const char* source constructor
+    TaskString(const char* src) : Fw::ExternalString(this->m_buf, sizeof this->m_buf, src) {}
 
-            char m_buf[FW_TASK_NAME_MAX_SIZE]; //!< buffer for string
+    //! destructor
+    ~TaskString() {}
 
-    };
-
-}
+  private:
+    char m_buf[FW_TASK_NAME_MAX_SIZE];  //!< storage for string data
+};
+}  // namespace Os
 
 #endif
