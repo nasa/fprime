e078a5909203925251cb7c4574a9c0fd74bc5695
Robert L. Bocchino Jr. <bocchino@jpl.nasa.gov>
2024-04-12 16:22:56 -0700

    Revise TextLogString

--- Fw/Log/TextLogString.hpp
+++ Fw/Log/TextLogString.hpp
@@ -1,37 +1,44 @@
+// ======================================================================
+// @file   TextLogString.hpp
+// @author F Prime
+// @brief  A string sized for an event log entry
+// ======================================================================
+
 #ifndef FW_TEXT_LOG_STRING_TYPE_HPP
 #define FW_TEXT_LOG_STRING_TYPE_HPP
 
 #include <FpConfig.hpp>
-#include <Fw/Types/StringType.hpp>
-#include <Fw/Cfg/SerIds.hpp>
+
+#include "Fw/Cfg/SerIds.hpp"
+#include "Fw/Types/ExternalString.hpp"
 
 namespace Fw {
 
-    class TextLogString : public Fw::StringBase {
-        public:
-
-            enum {
-                SERIALIZED_TYPE_ID = FW_TYPEID_LOG_STR,
-                SERIALIZED_SIZE = FW_LOG_TEXT_BUFFER_SIZE + sizeof(FwBuffSizeType) // size of buffer + storage of two size words
-            };
-
-            TextLogString(const char* src);
-            TextLogString(const StringBase& src);
-            TextLogString(const TextLogString& src);
-            TextLogString();
-            TextLogString& operator=(const TextLogString& other);
-            TextLogString& operator=(const StringBase& other);
-            TextLogString& operator=(const char* other);
-            ~TextLogString();
-
-            const char* toChar() const;
-            NATIVE_UINT_TYPE getCapacity() const ;
-
-        private:
-
-            char m_buf[FW_LOG_TEXT_BUFFER_SIZE];
+class TextLogString : public ExternalString {
+  public:
+    enum {
+        SERIALIZED_TYPE_ID = FW_TYPEID_LOG_STR,
+        SERIALIZED_SIZE = FW_LOG_TEXT_BUFFER_SIZE + sizeof(FwSizeStoreType)
     };
 
-}
+    //!< zero-argument constructor
+    TextLogString() : ExternalString(this->m_buf, sizeof this->m_buf) {}
+
+    //! const TextLogString& constructor
+    TextLogString(const TextLogString& src) : ExternalString(this->m_buf, sizeof this->m_buf, src) {}
+
+    //! const StringBase& constructor
+    TextLogString(const StringBase& src) : ExternalString(this->m_buf, sizeof this->m_buf, src) {}
+
+    //!< const char* source constructor
+    TextLogString(const char* src) : ExternalString(this->m_buf, sizeof this->m_buf, src) {}
+
+    //! destructor
+    ~TextLogString() {}
+
+  private:
+    char m_buf[FW_LOG_TEXT_BUFFER_SIZE];  //!< storage for string data
+};
+}  // namespace Fw
 
 #endif
