4e7ab414b028fa7d4f0bb3f88b79363c4acf7a08
Robert L. Bocchino Jr. <bocchino@jpl.nasa.gov>
2024-04-12 23:13:09 -0700

    Revise String type

--- Fw/Types/String.hpp
+++ Fw/Types/String.hpp
@@ -10,52 +10,47 @@
 #include <FpConfig.hpp>
 
 #include "Fw/Cfg/SerIds.hpp"
-#include "Fw/Types/ExternalString.hpp"
-#include "Fw/Types/StringUtils.hpp"
+#include "Fw/Types/StringBase.hpp"
 
 namespace Fw {
 
-class String : public ExternalString {
+class String : public StringBase {
   public:
     enum {
-        SERIALIZED_TYPE_ID = FW_TYPEID_FIXED_LENGTH_STRING,  //!< type id for Fw::String
-        STRING_SIZE = FW_FIXED_LENGTH_STRING_SIZE,           //!< storage for string
-        SERIALIZED_SIZE =
-            STRING_SIZE + sizeof(FwSizeStoreType)  //!< static serialized size is size of buffer + size of size field
+        SERIALIZED_TYPE_ID = FW_TYPEID_FIXED_LENGTH_STRING,
+        STRING_SIZE = FW_FIXED_LENGTH_STRING_SIZE,
+        SERIALIZED_SIZE = STRING_SIZE + sizeof(FwSizeStoreType)
     };
 
-    //! zero-argument constructor
-    String() : ExternalString(this->m_buf, sizeof this->m_buf) {}
+    String() : StringBase() { *this = ""; }
 
-    //! const String& constructor
-    String(const String& src) : ExternalString(this->m_buf, sizeof this->m_buf, src) {}
+    String(const String& src) : StringBase() { *this = src; }
 
-    //! const StringBase& constructor
-    String(const StringBase& src) : ExternalString(this->m_buf, sizeof this->m_buf, src) {}
+    String(const StringBase& src) : StringBase() { *this = src; }
 
-    //! const char* source constructor
-    String(const char* src) : ExternalString(this->m_buf, sizeof this->m_buf, src) {}
+    String(const char* src) : StringBase() { *this = src; }
 
-    //! Operator= (const String&)
     String& operator=(const String& src) {
         (void)StringBase::operator=(src);
         return *this;
     }
 
-    //! Operator= (const StringBase& src)
-    StringBase& operator=(const StringBase& src) {
+    String& operator=(const StringBase& src) {
         (void)StringBase::operator=(src);
         return *this;
     }
 
-    //! Operator= (const char*)
-    StringBase& operator=(const char* src) {
+    String& operator=(const char* src) {
         (void)StringBase::operator=(src);
         return *this;
     }
+
+    const char* toChar() const { return this->m_buf; }
+
+    StringBase::SizeType getCapacity() const { return sizeof this->m_buf; }
 
   private:
-    char m_buf[String::STRING_SIZE];  //!< storage for string data
+    char m_buf[STRING_SIZE];
 };
 }  // namespace Fw
 
