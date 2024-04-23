d19355d41e900506f2d544e550c9f0c37576a87f
thomas-bc <thomas.boyerchammard@gmail.com>
2024-04-22 18:18:00 -0700

    Add CMake test for version target

--- cmake/test/src/test_ref_shared.py
+++ cmake/test/src/test_ref_shared.py
@@ -11,6 +11,7 @@ import settings
 
 import cmake
 
+import json
 from pathlib import Path
 
 _ = cmake.get_build(
@@ -70,7 +71,7 @@ def test_ref_dictionary(REF_BUILD):
 
 
 def test_ref_dictionary_json(REF_BUILD):
-    """Run reference and assert reference targets exit"""
+    """Build Ref and assert JSON dictionary exists"""
     cmake.assert_process_success(REF_BUILD)
     output_path = (
         REF_BUILD["install"]
@@ -81,6 +82,16 @@ def test_ref_dictionary_json(REF_BUILD):
     )
     assert output_path.exists(), "Failed to locate Ref JSON Dictionary in build output"
 
+def test_ref_version_info(REF_BUILD):
+    """Build Ref and assert version files exist"""
+    cmake.assert_process_success(REF_BUILD)
+    version_hpp  = REF_BUILD["build"] / "versions" / "version.hpp"
+    version_json = REF_BUILD["build"] / "versions" / "version.json"
+    assert version_hpp.exists(), "Failed to locate version.hpp in build output"
+    assert version_json.exists(), "Failed to locate version.json in build output"
+    versions_dict = json.loads(version_json.read_text())
+    for key in ["framework_version", "project_version", "library_versions"]:
+        assert key in versions_dict, f"Failed to locate key: {key} in version.json"
 
 def test_ref_module_info(REF_BUILD):
     """Run reference and assert module-info.txt was created"""
