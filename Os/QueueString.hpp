88d65d71e253915569eef2555bf43e65c4a9049c
Rob Bocchino <bocchino@jpl.nasa.gov>
2024-04-25 13:58:48 -0700

    Add Fw::ExternalString and revise string implementations (#2679)
    * Reformat cpp and hpp files in Fw/Types
    
    * Revise string types
    
    * Revise string types
    
    * Revise string types
    
    * Remove EightyCharString
    
    * Revise CmdString
    
    * Revise LogString
    
    * Revise PrmString
    
    * Revise TextLogString
    
    * Revise Test/String
    
    * Revise TlmString
    
    * Revise InternalInterfaceString
    
    * Revise Os/QueueString
    
    * Revise Os/TaskString
    
    * Add missing type qualifier
    
    * Fix warning in String.hpp
    
    * Revise string types
    
    * Remove stray character
    
    * Revise StringBase
    
    * Revise string types
    
    * Revise String type
    
    * Revise FileNameString
    
    * Revise string types
    
    * Add missing file
    
    * Revise InternalInterfaceString
    
    * Revise comment
    
    * Revise TlmString
    
    * Revise string types
    
    * Revise log strings
    
    * Revise PrmString
    
    * Revise Os string classes
    
    * Revise string types
    
    * Revise string types
    
    * Revise os strings
    
    * Add test for ExternalString to TypesTest
    
    * Revise ExternalString
    
    * Revise ExternalString

--- Os/QueueString.hpp
+++ Os/QueueString.hpp
@@ -1,29 +1,54 @@
-#ifndef OS_QUEUE_STRING_TYPE_HPP
-#define OS_QUEUE_STRING_TYPE_HPP
+// ======================================================================
+// @file   QueueString.hpp
+// @author F Prime
+// @brief  A string sized for an OS queue name
+// ======================================================================
+
+#ifndef OS_QUEUE_STRING_HPP
+#define OS_QUEUE_STRING_HPP
 
 #include <FpConfig.hpp>
-#include <Fw/Types/StringType.hpp>
+
+#include "Fw/Types/StringBase.hpp"
 
 namespace Os {
 
-    class QueueString : public Fw::StringBase {
-        public:
+class QueueString final : public Fw::StringBase {
+  public:
+    enum { STRING_SIZE = FW_QUEUE_NAME_MAX_SIZE, SERIALIZED_SIZE = STRING_SIZE + sizeof(FwSizeStoreType) };
 
-            QueueString(const char* src); //!< char buffer constructor
-            QueueString(const StringBase& src); //!< copy constructor
-            QueueString(const QueueString& src); //!< copy constructor
-            QueueString(); //!< default constructor
-            QueueString& operator=(const QueueString& other); //!< assignment operator
-            QueueString& operator=(const StringBase& other); //!< other string assignment operator
-            QueueString& operator=(const char* other); //!< char* assignment operator
-            ~QueueString(); //!< destructor
+    QueueString() : StringBase() { *this = ""; }
 
-            const char* toChar() const; //!< get pointer to char buffer
-            NATIVE_UINT_TYPE getCapacity() const ;
+    QueueString(const QueueString& src) : StringBase() { *this = src; }
 
-        private:
-            char m_buf[FW_QUEUE_NAME_MAX_SIZE]; //!< buffer for string
-    };
-}
+    QueueString(const StringBase& src) : StringBase() { *this = src; }
+
+    QueueString(const char* src) : StringBase() { *this = src; }
+
+    ~QueueString() {}
+
+    QueueString& operator=(const QueueString& src) {
+        (void)StringBase::operator=(src);
+        return *this;
+    }
+
+    QueueString& operator=(const StringBase& src) {
+        (void)StringBase::operator=(src);
+        return *this;
+    }
+
+    QueueString& operator=(const char* src) {
+        (void)StringBase::operator=(src);
+        return *this;
+    }
+
+    const char* toChar() const { return this->m_buf; }
+
+    StringBase::SizeType getCapacity() const { return sizeof this->m_buf; }
+
+  private:
+    char m_buf[QueueString::STRING_SIZE];
+};
+}  // namespace Os
 
 #endif
