7ff002c719d7b4e546a6ba48f2e999ca050838e8
menaman123 <53198002+menaman123@users.noreply.github.com>
2024-06-07 16:29:26 -0400

    modified:   Drv/TcpClient/TcpClientComponentImpl.cpp (#2759)
    * modified:   Drv/TcpClient/TcpClientComponentImpl.cpp
    
    * Changed header and implementation for buffer size
    
    * Changed size_t to FwSizeType
    
    * Missed member variable
    
    * Convert types and added a check
    
    * Adding cast to assert arg type
    
    ---------
    
    Co-authored-by: M Starch <LeStarch@googlemail.com>

--- Drv/TcpClient/TcpClientComponentImpl.hpp
+++ Drv/TcpClient/TcpClientComponentImpl.hpp
@@ -54,12 +54,14 @@ class TcpClientComponentImpl : public TcpClientComponentBase, public SocketReadT
      * \param send_timeout_seconds: send timeout seconds component. Defaults to: SOCKET_TIMEOUT_SECONDS
      * \param send_timeout_microseconds: send timeout microseconds component. Must be less than 1000000. Defaults to:
      * SOCKET_TIMEOUT_MICROSECONDS
+     * \param buffer_size: size of the buffer to be allocated. Defaults to 1024.
      * \return status of the configure
      */
     SocketIpStatus configure(const char* hostname,
                              const U16 port,
                              const U32 send_timeout_seconds = SOCKET_SEND_TIMEOUT_SECONDS,
-                             const U32 send_timeout_microseconds = SOCKET_SEND_TIMEOUT_MICROSECONDS);
+                             const U32 send_timeout_microseconds = SOCKET_SEND_TIMEOUT_MICROSECONDS,
+                             FwSizeType buffer_size = 1024);
 
   PROTECTED:
     // ----------------------------------------------------------------------
@@ -126,6 +128,9 @@ class TcpClientComponentImpl : public TcpClientComponentBase, public SocketReadT
     Drv::SendStatus send_handler(const NATIVE_INT_TYPE portNum, Fw::Buffer& fwBuffer);
 
     Drv::TcpClientSocket m_socket; //!< Socket implementation
+
+    // Member variable to store the buffer size
+    FwSizeType m_allocation_size;
 };
 
 }  // end namespace Drv
