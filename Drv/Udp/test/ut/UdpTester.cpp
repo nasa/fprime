ea5b7afb4ed57462d636d950057d1bf2ad98eea3
Johan Bertrand <6369937+JohanBertrand@users.noreply.github.com>
2024-09-01 15:32:58 +0200

    Fix indentation

--- Drv/Udp/test/ut/UdpTester.cpp
+++ Drv/Udp/test/ut/UdpTester.cpp
@@ -107,8 +107,8 @@ void UdpTester::test_with_loop(U32 iterations, bool recv_thread) {
         }
         // Properly stop the client on the last iteration
         if (((1 + i) == iterations) && recv_thread) {
-                this->component.stop();
-                this->component.join();
+            this->component.stop();
+            this->component.join();
         } else {
             this->component.close();
         }
