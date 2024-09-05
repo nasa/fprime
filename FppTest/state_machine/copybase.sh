de9296fc7d78f1c27abb927cd743dd8c2528cbf0
watney <garth.j.watney@jpl.nasa.gov>
2024-09-05 13:06:54 -0700

    Fix spelling issues with CI

--- FppTest/state_machine/copybase.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DESTINATION_DIR="../build-fprime-automatic-native-ut/FppTest/state_machine"
-
-# Copy the file to the destination with a new name
-cp "SmTestComponent.hpp" "$DESTINATION_DIR/SmTestComponentAc.hpp"
-cp "SmTestComponent.cpp" "$DESTINATION_DIR/SmTestComponentAc.cpp"
-
