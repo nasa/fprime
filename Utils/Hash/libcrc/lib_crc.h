cc606e4f92685cf389f0c7d5310ae5116f05c2e8
bdshenker <boris.d.shenker@jpl.nasa.gov>
2024-10-09 13:55:38 -0700

    GIT #2899: Fix F' C header files (.h) which are not C++ compliant.

--- Utils/Hash/libcrc/lib_crc.h
+++ Utils/Hash/libcrc/lib_crc.h
@@ -50,6 +50,9 @@
 #ifndef UTILS_HASH_LIB_CRC_HPP
 #define UTILS_HASH_LIB_CRC_HPP
 
+#ifdef  __cplusplus
+extern "C" {
+#endif
 
 #define CRC_VERSION     "1.16"
 
@@ -66,5 +69,9 @@ unsigned short          update_crc_ccitt(  unsigned short crc, char c           
 unsigned short          update_crc_dnp(    unsigned short crc, char c                 );
 unsigned short          update_crc_kermit( unsigned short crc, char c                 );
 unsigned short          update_crc_sick(   unsigned short crc, char c, char prev_byte );
+
+#ifdef  __cplusplus
+}
+#endif
 
 #endif // UTILS_HASH_LIB_CRC_HPP
