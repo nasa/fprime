a684e3fd4cb79519def2f99325496f65d091950e
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2024-10-08 17:48:03 -0700

    Fixing names

--- cmake/platform/Linux.cmake
+++ cmake/platform/Linux.cmake
@@ -17,8 +17,8 @@ choose_fprime_implementation(Os/Task Os/Task/Posix)
 choose_fprime_implementation(Os/Mutex Os/Mutex/Posix)
 choose_fprime_implementation(Os/Queue Os/Generic/PriorityQueue)
 
-choose_fprime_implementation(Os/Cpu Os/Linux/Cpu)
-choose_fprime_implementation(Os/Memory Os/Linux/Memory)
+choose_fprime_implementation(Os/Cpu Os/Cpu/Linux)
+choose_fprime_implementation(Os/Memory Os/Memory/Linux)
 
 # Use common linux setup
 add_definitions(-DTGT_OS_TYPE_LINUX)
