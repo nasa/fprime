1f1886e5810c10a736ce326570a4b7287ceadb31
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2024-10-21 09:56:17 -0700

    Reworking prevent_prescan in subbuild context

--- Svc/FatalHandler/FatalHandlerComponentBaremetalImpl.cpp
+++ Svc/FatalHandler/FatalHandlerComponentBaremetalImpl.cpp
@@ -5,7 +5,7 @@
 // ======================================================================
 
 #include <cstdlib>
-#include <Os/Console.hpp>
+#include <Fw/Logger/Logger.hpp>
 #include <Svc/FatalHandler/FatalHandlerComponentImpl.hpp>
 #include <FpConfig.hpp>
 
@@ -19,7 +19,7 @@ namespace Svc {
             const NATIVE_INT_TYPE portNum,
             FwEventIdType Id) {
         // for **nix, delay then exit with error code
-        Os::Log::log("FATAL %d handled.\n",Id);
+        Fw::Logger::log("FATAL %" PRI_FwEventIdType "handled.\n",Id);
         while (true) {} // Returning might be bad
     }
 
