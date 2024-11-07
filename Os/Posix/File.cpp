da5de71e0b7b24e2657b830e7dd61ed13c8ea945
Kevin F. Ortega <kevin.f.ortega@jpl.nasa.gov>
2024-11-06 16:23:46 -0800

    Adding pound-define code for VxWorks otherwise this does not build when building VxWorks using Posix. Better to do a pound-if here than copying the entire file and deleting a line in the vxworks repo

--- Os/Posix/File.cpp
+++ Os/Posix/File.cpp
@@ -45,28 +45,21 @@ static_assert(std::numeric_limits<FwSignedSizeType>::min() <= std::numeric_limit
               "Minimum value of FwSizeType larger than the minimum value of ssize_t. Configure a larger type.");
 
 //!\brief default copy constructor
+#ifndef TGT_OS_TYPE_VXWORKS
 PosixFile::PosixFile(const PosixFile& other) {
-#ifdef TGT_OS_TYPE_VXWORKS
-    // dup is not supported in VxWorks
-    FW_ASSERT(0);
-#else
     // Must properly duplicate the file handle
     this->m_handle.m_file_descriptor = ::dup(other.m_handle.m_file_descriptor);
-#endif
 }
+#endif
 
+#ifndef TGT_OS_TYPE_VXWORKS
 PosixFile& PosixFile::operator=(const PosixFile& other) {
-#ifdef TGT_OS_TYPE_VXWORKS
-    // dup is not supported in VxWorks
-    FW_ASSERT(0);
-    return *this;
-#else
     if (this != &other) {
         this->m_handle.m_file_descriptor = ::dup(other.m_handle.m_file_descriptor);
     }
     return *this;
-#endif
 }
+#endif
 
 PosixFile::Status PosixFile::open(const char* filepath,
                                   PosixFile::Mode requested_mode,
