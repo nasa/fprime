dde2ef44d55a1db7a75a25fad9c086460f665be1
thomas-bc <thomas.boyer.chammard@jpl.nasa.gov>
2025-01-14 18:30:08 -0800

    fix glob pattern looparound

--- docs/doxygen/sdd_processing.py
+++ docs/doxygen/sdd_processing.py
@@ -9,7 +9,7 @@ os.makedirs(SDD_DIR, exist_ok=True)
 with open(Path(SDD_DIR) / 'index.md', 'a') as index_file:
 
     # Find all sdd.md files and process them
-    for file in Path('.').rglob('*/docs/sdd.md'):
+    for file in list(Path('.').rglob('*/docs/sdd.md')):
         # Get module name and parent directory directory name
         second_parent = file.parents[1].name
         third_parent = file.parents[2].name
@@ -18,7 +18,7 @@ with open(Path(SDD_DIR) / 'index.md', 'a') as index_file:
             continue
 
         source_dir = file.parents[1] / 'docs'
-        dest_dir = Path(SDD_DIR) / third_parent / second_parent
+        dest_dir = Path(SDD_DIR) / third_parent / second_parent / 'docs'
 
         os.makedirs(dest_dir, exist_ok=True)
         shutil.copytree(source_dir, dest_dir, dirs_exist_ok=True)
