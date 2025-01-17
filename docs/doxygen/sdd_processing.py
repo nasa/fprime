93ce372db9df06f8d7803fe00e9b88ac9b9adfcd
thomas-bc <thomas.boyer.chammard@jpl.nasa.gov>
2025-01-17 11:58:14 -0800

    Fix SDD processing so it is always ordered

--- docs/doxygen/sdd_processing.py
+++ docs/doxygen/sdd_processing.py
@@ -9,7 +9,9 @@ os.makedirs(SDD_DIR, exist_ok=True)
 with open(Path(SDD_DIR) / 'index.md', 'a') as index_file:
 
     # Find all sdd.md files and process them
-    for file in list(Path('.').rglob('*/docs/sdd.md')):
+    # Sort them for convenience, but also can't use the rglob generator since
+    # it would start matching generated files, so using sorted to create a list
+    for file in sorted(Path('.').rglob('*/docs/sdd.md')):
         # Get module name and parent directory name
         second_parent = file.parents[1].name
         third_parent = file.parents[2].name
