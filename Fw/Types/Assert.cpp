a5e6115cfd20256806a11a3c5354271c4c2ac700
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2025-01-30 14:50:23 -0800

    Reverting test change to assert

--- Fw/Types/Assert.cpp
+++ Fw/Types/Assert.cpp
@@ -13,8 +13,7 @@
 namespace Fw {
 
 void defaultPrintAssert(const CHAR* msg) {
-    //Fw::Logger::log("%s", msg);
-    fprintf(stderr, "%s", msg);
+    Fw::Logger::log("%s", msg);
 }
 
 void defaultReportAssert(FILE_NAME_ARG file,
