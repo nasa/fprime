c02006a60fb011ee24d2a8cc5ef914e8f198464a
thomas-bc <thomas.boyerchammard@gmail.com>
2025-02-19 18:22:01 -0800

    finish renaming footer to trailer and add implementation note

--- Svc/FrameAccumulator/FrameDetector/FprimeFrameDetector.cpp
+++ Svc/FrameAccumulator/FrameDetector/FprimeFrameDetector.cpp
@@ -12,13 +12,22 @@ namespace FrameDetectors {
 FrameDetector::Status FprimeFrameDetector::detect(const Types::CircularBuffer& data, FwSizeType& size_out) const {
 
     FprimeProtocol::FrameHeader header;
-    FprimeProtocol::FrameTrailer footer;
+    FprimeProtocol::FrameTrailer trailer;
 
-    // If not enough data for header + footer, report MORE_DATA_NEEDED
+    // If not enough data for header + trailer, report MORE_DATA_NEEDED
     if (data.get_allocated_size() < FprimeProtocol::FrameHeader::SERIALIZED_SIZE + FprimeProtocol::FrameTrailer::SERIALIZED_SIZE) {
         size_out = FprimeProtocol::FrameHeader::SERIALIZED_SIZE + FprimeProtocol::FrameTrailer::SERIALIZED_SIZE;
         return Status::MORE_DATA_NEEDED;
     }
+
+    // NOTE: it is understood and accepted that the following code is not as efficient as it could technically be
+    // We are leveraging the FPP autocoded types to do the deserialization for us.
+    // In its current implementation, CircularBuffer is not a SerializeBufferBase, which prevents us from deserializing
+    // directly from the CircularBuffer into FrameHeader/FrameTrailer. Instead, we have to copy the data into
+    // a temporary SerializeBuffer, and then deserialize from that buffer into the FrameHeader/FrameTrailer objects.
+    // A better implementation would be to have CircularBuffer implement a shared interface with SerializeBufferBase, and
+    // then we could pass the CircularBuffer directly into the FrameHeader/FrameTrailer deserializers.
+    // This is left as a TODO for future improvement as it is a significant refactor
 
     // ---------------- Frame Header ----------------
     // Copy CircularBuffer data into linear buffer, for serialization into FrameHeader object
@@ -45,16 +54,16 @@ FrameDetector::Status FprimeFrameDetector::detect(const Types::CircularBuffer& d
         return Status::MORE_DATA_NEEDED;
     }
 
-    // ---------------- Frame Footer ----------------
-    U8 footer_data[FprimeProtocol::FrameTrailer::SERIALIZED_SIZE];
-    Fw::ExternalSerializeBuffer footer_ser_buffer(footer_data, FprimeProtocol::FrameTrailer::SERIALIZED_SIZE);
-    status = data.peek(footer_data, FprimeProtocol::FrameTrailer::SERIALIZED_SIZE, FprimeProtocol::FrameHeader::SERIALIZED_SIZE + header.getlength());
+    // ---------------- Frame Trailer ----------------
+    U8 trailer_data[FprimeProtocol::FrameTrailer::SERIALIZED_SIZE];
+    Fw::ExternalSerializeBuffer trailer_ser_buffer(trailer_data, FprimeProtocol::FrameTrailer::SERIALIZED_SIZE);
+    status = data.peek(trailer_data, FprimeProtocol::FrameTrailer::SERIALIZED_SIZE, FprimeProtocol::FrameHeader::SERIALIZED_SIZE + header.getlength());
     if (status != Fw::FW_SERIALIZE_OK) {
         return Status::NO_FRAME_DETECTED;
     }
-    footer_ser_buffer.setBuffLen(FprimeProtocol::FrameTrailer::SERIALIZED_SIZE);
-    // Desiralize header from circular buffer (peeked data) into footer object
-    footer.deserialize(footer_ser_buffer);
+    trailer_ser_buffer.setBuffLen(FprimeProtocol::FrameTrailer::SERIALIZED_SIZE);
+    // Desiralize header from circular buffer (peeked data) into trailer object
+    trailer.deserialize(trailer_ser_buffer);
     if (status != Fw::FW_SERIALIZE_OK) {
         return Status::NO_FRAME_DETECTED;
     }
@@ -71,13 +80,13 @@ FrameDetector::Status FprimeFrameDetector::detect(const Types::CircularBuffer& d
     }
     hash.final(hashBuffer);
 
-    // printf("transmitted_crc: %08X\n", footer.getcrc());
+    // printf("transmitted_crc: %08X\n", trailer.getcrc());
     // printf("computed_crc: %08X\n", hashBuffer.asBigEndianU32());
     // Compare the transmitted CRC with the computed one
-    if (footer.getcrc() != hashBuffer.asBigEndianU32()) {
+    if (trailer.getcrc() != hashBuffer.asBigEndianU32()) {
         // Note: CRC mismatch - there likely was data corruption
         // Should there be an event / telemetry for frames dropped ??
-        FW_ASSERT(footer.getcrc() == hashBuffer.asBigEndianU32());
+        FW_ASSERT(trailer.getcrc() == hashBuffer.asBigEndianU32());
         return Status::NO_FRAME_DETECTED;
     }
     size_out = header.getlength() + FprimeProtocol::FrameHeader::SERIALIZED_SIZE + FprimeProtocol::FrameTrailer::SERIALIZED_SIZE;
