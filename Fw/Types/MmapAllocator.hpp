d932413fa4d0ed0f7f7ee2e7657c176c09aa0a6f
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2025-02-26 13:23:36 -0800

    NATIVE_INT_TYPE use in identifier

--- Fw/Types/MmapAllocator.hpp
+++ Fw/Types/MmapAllocator.hpp
@@ -31,12 +31,12 @@ class MmapAllocator : public MemAllocator {
     //! \param identifier: identifier to use with allocation
     //! \param size: size of memory to be allocated
     //! \param recoverable: (output) is this memory recoverable after a reset. Always false for mmap.
-    void* allocate(const NATIVE_UINT_TYPE identifier, FwSizeType& size, bool& recoverable);
+    void* allocate(const FwEnumStoreType identifier, FwSizeType& size, bool& recoverable);
 
     //! Deallocation of memory using the mmap allocator
     //! \param identifier: identifier used at allocation
     //! \param ptr: pointer to memory being deallocated
-    void deallocate(const NATIVE_UINT_TYPE identifier, void* ptr);
+    void deallocate(const FwEnumStoreType identifier, void* ptr);
 
   private:
     FwSizeType m_length;
