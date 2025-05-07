e746482027316929e50d71f599f58ebbe50610b6
Cel Andromeda Skeggs <cel.a.skeggs@jpl.nasa.gov>
2025-05-07 22:45:34 +0000

    Clarify allocation failure error
    It's not useful to report an UNKNOWN_ERROR when the cause is known.

--- Os/Queue.hpp
+++ Os/Queue.hpp
@@ -38,6 +38,7 @@ class QueueInterface {
         INVALID_PRIORITY,  //!<  invalid priority requested
         FULL,              //!<  Queue was full when attempting to send a message
         NOT_SUPPORTED,     //!<  Queue feature is not supported
+        ALLOCATION_FAILED, //!<  required memory could not be allocated
         UNKNOWN_ERROR      //!<  Unexpected error; can't match with returns
     };
 
