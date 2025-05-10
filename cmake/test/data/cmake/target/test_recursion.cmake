6cb5690c4b169904b0df2431b59a929ed18dc694
M Starch <LeStarch@googlemail.com>
2025-05-10 11:47:40 -0700

    Linux CMake UT fixs

--- cmake/test/data/cmake/target/test_recursion.cmake
+++ cmake/test/data/cmake/target/test_recursion.cmake
@@ -6,8 +6,8 @@
 include(utilities)
 # Current full dependency list for TestDeployment (mostly via Svc_CmdDispatcher)
 set(EXPECTED_FULL_DEPENDENCIES Fw Fw_Buffer Fw_Cmd Fw_Com Fw_Comp Fw_CompQueued Fw_Fpy Fw_Log Fw_Logger Fw_Obj Fw_Port
-    Fw_Prm Fw_Time Fw_Tlm Fw_Types Os Os_Console_Common Os_Console_Posix Os_Cpu_Common Os_Cpu_Darwin Os_File_Common
-    Os_File_Posix Os_Generic_PriorityQueue Os_Generic_Types Os_Memory_Common Os_Memory_Darwin Os_Mutex_Common
+    Fw_Prm Fw_Time Fw_Tlm Fw_Types Os Os_Console_Common Os_Console_Posix Os_Cpu_Common Os_Cpu_${FPRIME_PLATFORM} Os_File_Common
+    Os_File_Posix Os_Generic_PriorityQueue Os_Generic_Types Os_Memory_Common Os_Memory_${FPRIME_PLATFORM} Os_Mutex_Common
     Os_Mutex_Posix Os_Posix_Shared Os_RawTime_Common Os_RawTime_Posix Os_Task_Common Os_Task_Posix Svc_CmdDispatcher
     Svc_Ping Svc_Sched TestDeployment TestLibrary2_TestComponent TestLibrary_TestComponent Utils_Hash __fprime_config
     cmake_platform_unix_Platform default_config snprintf-format)
