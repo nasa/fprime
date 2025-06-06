ad7531d99aa0eb5ff7869a1acd2715db4b90c671
Kevin F Ortega <kevin.f.ortega@jpl.nasa.gov>
2025-06-06 15:34:53 -0700

    fixed copy paste error

--- cmake/platform/Linux.cmake
+++ cmake/platform/Linux.cmake
@@ -12,7 +12,7 @@ if (NOT DEFINED FPRIME_USE_STUBBED_DRIVERS)
 endif()
 # Add unix include path which is compatible with Linux for PlatformTypes.hpp
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/unix/Platform/")
-# Override unix implementations with DARWIN specific ones
+# Override unix implementations with LINUX specific ones
 register_fprime_config(
       PlatformLinux
    INTERFACE # No buildable files generated
