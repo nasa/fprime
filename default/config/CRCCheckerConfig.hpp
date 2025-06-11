7c87f0ce94cc5df2fe4123d244ea23c86d914bed
0xb-s <145866191+0xb-s@users.noreply.github.com>
2025-06-11 10:12:30 -0700

    use a constant instead of  #define

--- default/config/CRCCheckerConfig.hpp
+++ default/config/CRCCheckerConfig.hpp
@@ -6,7 +6,10 @@
 #ifndef CONFIG_CRC_CHECKER_CONFIG_HPP
 #define CONFIG_CRC_CHECKER_CONFIG_HPP
 
+#include <Fw/FPrimeBasicTypes.hpp>  
+
 // Maximum size allowed for filename strings in CRC checking
-#define CONFIG_CRC_MAX_FILENAME_SIZE 128
+
+constexpr FwSizeType CONFIG_CRC_MAX_FILENAME_SIZE = 128;
 
 #endif 
