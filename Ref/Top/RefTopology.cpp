29d4ff8242574f1afc1bb6aacdf5cfef4d79987d
Moises Mata <mm6155@columbia.edu>
2025-06-20 14:26:10 -0700

    Removal of redundant teardowncomponents()

--- Ref/Top/RefTopology.cpp
+++ Ref/Top/RefTopology.cpp
@@ -91,6 +91,5 @@ void teardownTopology(const TopologyState& state) {
     // Autocoded (active component) task clean-up. Functions provided by topology autocoder.
     stopTasks(state);
     freeThreads(state);
-    tearDownComponents(state);
 }
 }  // namespace Ref
