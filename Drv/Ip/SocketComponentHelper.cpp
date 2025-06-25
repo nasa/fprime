0d471ae1b16434c06e2284b2e9e0c3f4af0a9d06
thomas-bc <thomas.boyerchammard@gmail.com>
2025-06-25 11:39:34 -0700

    Fix bufferRepeater size usage

--- Drv/Ip/SocketComponentHelper.cpp
+++ Drv/Ip/SocketComponentHelper.cpp
@@ -195,7 +195,7 @@ void SocketComponentHelper::readLoop() {
             Fw::Buffer buffer = this->getBuffer();
             U8* data = buffer.getData();
             FW_ASSERT(data);
-            FW_ASSERT(buffer.getSize() < std::numeric_limits<U32>::max(),
+            FW_ASSERT(buffer.getSize() <= std::numeric_limits<U32>::max(),
                       static_cast<FwAssertArgType>(buffer.getSize()));
             U32 size = static_cast<U32>(buffer.getSize());
             // recv blocks, so it may have been a while since its done an isOpened check
