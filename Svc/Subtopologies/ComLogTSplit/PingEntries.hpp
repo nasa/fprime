25f11ba1d3aeb3b139effd0473987acb32318eb6
Moises Mata <mm6155@columbia.edu>
2025-07-09 16:46:06 -0700

    Fix ComLogTSplit PingEntries Namespace

--- Svc/Subtopologies/ComLogTSplit/PingEntries.hpp
+++ Svc/Subtopologies/ComLogTSplit/PingEntries.hpp
@@ -1,8 +1,8 @@
-#ifndef COMLOG_PINGENTRIES_HPP
-#define COMLOG_PINGENTRIES_HPP
+#ifndef COMLOGTSPLIT_PINGENTRIES_HPP
+#define COMLOGTSPLIT_PINGENTRIES_HPP
 
   namespace PingEntries {
-    namespace ComLog_comLog       {enum { WARN = 3, FATAL = 5 };}
+    namespace ComLogTSplit_comLog       {enum { WARN = 3, FATAL = 5 };}
   }
 
 #endif
