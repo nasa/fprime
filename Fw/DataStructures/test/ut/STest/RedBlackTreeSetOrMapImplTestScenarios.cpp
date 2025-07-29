b5dd4d1e5bbeedc6a4bf481c87940857bdd8e25f
Rob Bocchino <bocchino@jpl.nasa.gov>
2025-07-29 09:41:07 -0700

    Revise RedBlackTree tests

--- Fw/DataStructures/test/ut/STest/RedBlackTreeSetOrMapImplTestScenarios.cpp
+++ Fw/DataStructures/test/ut/STest/RedBlackTreeSetOrMapImplTestScenarios.cpp
@@ -20,7 +20,7 @@ void random(const Fw::StringBase& name, State& state, U32 maxNumSteps) {
       //&Rules::clear,
       //&Rules::find,
       //&Rules::findExisting,
-      //&Rules::insertExisting,
+      &Rules::insertExisting,
       //&Rules::insertFull,
       &Rules::insertNotFull
       //&Rules::remove,
