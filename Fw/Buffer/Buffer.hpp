1dd1430003bc878f16e4563ac863e62fa7d17d78
Andrei Tumbar <andrei.tumbar@jpl.nasa.gov>
2025-07-31 12:52:45 -0700

    Return UNIMPLEMENTED status on default F Prime serializables

--- Fw/Buffer/Buffer.hpp
+++ Fw/Buffer/Buffer.hpp
@@ -141,8 +141,10 @@ class Buffer : public Fw::Serializable {
     // Deprecated methods
     // ----------------------------------------------------------------------
 
+    __attribute__((deprecated))
     Fw::SerializeStatus serialize(Fw::SerializeBufferBase& serialBuffer) const;
 
+    __attribute__((deprecated))
     Fw::SerializeStatus deserialize(Fw::SerializeBufferBase& buffer);
 
     // ----------------------------------------------------------------------
