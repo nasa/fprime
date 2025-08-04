ec14d6ad484abbe1aebb562c4e54572cc01dfad2
M Starch <LeStarch@googlemail.com>
2025-08-04 16:24:26 -0700

    Make telemetry std::atomic

--- Drv/LinuxUartDriver/LinuxUartDriver.hpp
+++ Drv/LinuxUartDriver/LinuxUartDriver.hpp
@@ -18,6 +18,7 @@
 #include <Os/Task.hpp>
 
 #include <termios.h>
+#include <atomic>
 
 namespace Drv {
 
@@ -123,9 +124,9 @@ class LinuxUartDriver final : public LinuxUartDriverComponentBase {
 
     Os::Task m_readTask;  //!< task instance for thread to read serial port
 
-    FwSizeType m_bytesSent;      //!< number of bytes sent
-    FwSizeType m_bytesReceived;  //!< number of bytes received
-    bool m_quitReadThread;       //!< flag to quit thread
+    std::atomic<FwSizeType> m_bytesSent;      //!< number of bytes sent
+    std::atomic<FwSizeType> m_bytesReceived;  //!< number of bytes received
+    bool m_quitReadThread;                    //!< flag to quit thread
 };
 
 }  // end namespace Drv
