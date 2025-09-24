824f314a0817edec1edf57c7a2eac8b7d2b259ad
ekswang <eric.k.wang@jpl.nasa.gov>
2025-09-24 15:31:01 -0700

    Update FpySequencer.hpp

--- Svc/FpySequencer/FpySequencer.hpp
+++ Svc/FpySequencer/FpySequencer.hpp
@@ -26,9 +26,9 @@ static_assert(Svc::Fpy::MAX_SEQUENCE_ARG_COUNT <= std::numeric_limits<U8>::max()
 static_assert(Svc::Fpy::MAX_SEQUENCE_STATEMENT_COUNT <= std::numeric_limits<U16>::max(),
               "Sequence statement count must be below U16 max");
 static_assert(Svc::Fpy::MAX_STACK_SIZE <= std::numeric_limits<U32>::max(), "Max stack size must be below U32 max");
-static_assert(Svc::Fpy::MAX_STACK_SIZE >= static_cast<int>(FW_TLM_BUFFER_MAX_SIZE),
+static_assert(Svc::Fpy::MAX_STACK_SIZE >= static_cast<size_t>(FW_TLM_BUFFER_MAX_SIZE),
               "Max stack size must be greater than max tlm buffer size");
-static_assert(Svc::Fpy::MAX_STACK_SIZE >= static_cast<int>(FW_PARAM_BUFFER_MAX_SIZE),
+static_assert(Svc::Fpy::MAX_STACK_SIZE >= static_cast<size_t>(FW_PARAM_BUFFER_MAX_SIZE),
               "Max stack size must be greater than max prm buffer size");
 
 namespace Svc {
