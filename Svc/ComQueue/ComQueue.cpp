98bc9be69aed2f7ca63cb613b93e6372a3f2044e
M Starch <LeStarch@googlemail.com>
2025-09-25 13:10:00 -0700

    Fix static_assert -> static_cast

--- Svc/ComQueue/ComQueue.cpp
+++ Svc/ComQueue/ComQueue.cpp
@@ -310,12 +310,12 @@ void ComQueue::processQueue() {
             // thus it takes an address to the object to fill and the size of the actual object.
             FW_ASSERT(this->m_buffer_state == OWNED);
             auto dequeue_status = queue.dequeue(reinterpret_cast<U8*>(&this->m_dequeued_com_buffer), sizeof(this->m_dequeued_com_buffer));
-            FW_ASSERT(dequeue_status == Fw::SerializeStatus::FW_SERIALIZE_OK, static_assert<FwAssertArgType>(dequeue_status));
+            FW_ASSERT(dequeue_status == Fw::SerializeStatus::FW_SERIALIZE_OK, static_cast<FwAssertArgType>(dequeue_status));
             this->sendComBuffer(this->m_dequeued_com_buffer, entry.index);
         } else {
             Fw::Buffer buffer;
             auto dequeue_status = queue.dequeue(reinterpret_cast<U8*>(&buffer), sizeof(buffer));
-            FW_ASSERT(dequeue_status == Fw::SerializeStatus::FW_SERIALIZE_OK, static_assert<FwAssertArgType>(dequeue_status));
+            FW_ASSERT(dequeue_status == Fw::SerializeStatus::FW_SERIALIZE_OK, static_cast<FwAssertArgType>(dequeue_status));
             this->sendBuffer(buffer, entry.index);
         }
 
