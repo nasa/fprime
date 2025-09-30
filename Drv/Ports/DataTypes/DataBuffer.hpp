bce02f5e0cd2d68b8dd9c7532c684aa0e6f9b698
WeijiaYan <wj_yann@outlook.com>
2025-09-30 15:55:52 +0800

    Docs: Add comments to DataBuffer class
    - Added class description
    - Documented enum constants
    - Added brief comments for all methods

--- Drv/Ports/DataTypes/DataBuffer.hpp
+++ Drv/Ports/DataTypes/DataBuffer.hpp
@@ -6,26 +6,35 @@
 
 namespace Drv {
 
+//! Fixed-size buffer for driver data transfer
 class DataBuffer : public Fw::SerializeBufferBase {
   public:
     enum {
-        DATA_BUFFER_SIZE = 256,
-        SERIALIZED_TYPE_ID = 1010,
-        SERIALIZED_SIZE = DATA_BUFFER_SIZE + sizeof(FwBuffSizeType)
+        DATA_BUFFER_SIZE = 256,          //!< Buffer capacity in bytes
+        SERIALIZED_TYPE_ID = 1010,       //!< Type ID for serialization
+        SERIALIZED_SIZE = DATA_BUFFER_SIZE + sizeof(FwBuffSizeType)  //!< Total serialized size
     };
 
+    //! Construct from existing data
     DataBuffer(const U8* args, FwSizeType size);
+    //! Default constructor
     DataBuffer();
+    //! Copy constructor
     DataBuffer(const DataBuffer& other);
+    //! Destructor
     virtual ~DataBuffer();
+    //! Assignment operator
     DataBuffer& operator=(const DataBuffer& other);
 
-    FwSizeType getBuffCapacity() const;  // !< returns capacity, not current size, of buffer
+    //! Get buffer capacity
+    FwSizeType getBuffCapacity() const;
+    //! Get buffer address
     U8* getBuffAddr();
+    //! Get const buffer address
     const U8* getBuffAddr() const;
 
   private:
-    U8 m_data[DATA_BUFFER_SIZE];  // packet data buffer
+    U8 m_data[DATA_BUFFER_SIZE];  //!< Internal data storage
 };
 }  // namespace Drv
 
