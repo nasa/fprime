d730105736d612be1e2b532799f910e884fb21a5
M Starch <LeStarch@googlemail.com>
2025-10-12 10:35:43 -0700

    Update Fw/Buffer/Buffer.hpp
    Co-authored-by: Thomas Boyer-Chammard <49786685+thomas-bc@users.noreply.github.com>

--- Fw/Buffer/Buffer.hpp
+++ Fw/Buffer/Buffer.hpp
@@ -52,8 +52,8 @@ class Buffer : public Fw::Serializable {
     //!
     //! A convenience enumeration to help users implement ownership tracking of buffers.
     enum class OwnershipState {
-        NOT_OWNED,  //!< The buffer is currently not owned by the TmFramer
-        OWNED,      //!< The buffer is currently owned by the TmFramer
+        NOT_OWNED,  //!< The buffer is currently not owned
+        OWNED,      //!< The buffer is currently owned
     };
 
   public:
