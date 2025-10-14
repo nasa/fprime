3920c685afbe90d071c00ecf4f380f2e6b6aeeaf
Rob Bocchino <bocchino@jpl.nasa.gov>
2025-10-14 16:09:37 -0700

    Revise FppTest
    Add A component to topology

---
+++ FppTestProject/FppTest/topology/components/A/test/ut/ATester.hpp
@@ -0,0 +1,68 @@
+// ======================================================================
+// \title  ATester.hpp
+// \author bocchino
+// \brief  hpp file for A component test harness implementation class
+// ======================================================================
+
+#ifndef FppTest_ATester_HPP
+#define FppTest_ATester_HPP
+
+#include "FppTest/topology/components/A/A.hpp"
+#include "FppTest/topology/components/A/AGTestBase.hpp"
+
+namespace FppTest {
+
+class ATester final : public AGTestBase {
+  public:
+    // ----------------------------------------------------------------------
+    // Constants
+    // ----------------------------------------------------------------------
+
+    // Maximum size of histories storing events, telemetry, and port outputs
+    static const FwSizeType MAX_HISTORY_SIZE = 10;
+
+    // Instance ID supplied to the component instance under test
+    static const FwEnumStoreType TEST_INSTANCE_ID = 0;
+
+  public:
+    // ----------------------------------------------------------------------
+    // Construction and destruction
+    // ----------------------------------------------------------------------
+
+    //! Construct object ATester
+    ATester();
+
+    //! Destroy object ATester
+    ~ATester();
+
+  public:
+    // ----------------------------------------------------------------------
+    // Tests
+    // ----------------------------------------------------------------------
+
+    //! Send data
+    void sendData();
+
+  private:
+    // ----------------------------------------------------------------------
+    // Helper functions
+    // ----------------------------------------------------------------------
+
+    //! Connect ports
+    void connectPorts();
+
+    //! Initialize components
+    void initComponents();
+
+  private:
+    // ----------------------------------------------------------------------
+    // Member variables
+    // ----------------------------------------------------------------------
+
+    //! The component under test
+    A component;
+};
+
+}  // namespace FppTest
+
+#endif
