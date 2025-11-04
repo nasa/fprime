955cd1830f41bce37191fd1545bf286887331fe0
thomas-bc <thomas.boyer.chammard@jpl.nasa.gov>
2025-11-04 13:38:47 -0800

    Other UT cast fix

--- Svc/AssertFatalAdapter/test/ut/AssertFatalAdapterTester.cpp
+++ Svc/AssertFatalAdapter/test/ut/AssertFatalAdapterTester.cpp
@@ -42,7 +42,7 @@ void AssertFatalAdapterTester::testAsserts() {
     // Apply all truncations to file buffer size
     const FwSizeType fileMaxSize = FW_MIN(FW_MIN(static_cast<FwSizeType>(AssertFatalAdapterEventFileSize),
                                                  static_cast<FwSizeType>(FW_LOG_STRING_MAX_SIZE)),
-                                          FW_ASSERT_TEXT_SIZE);
+                                          static_cast<FwSizeType>(FW_ASSERT_TEXT_SIZE));
 
     char file[Fw::StringBase::BUFFER_SIZE(fileMaxSize)];
     Fw::String fileString;
