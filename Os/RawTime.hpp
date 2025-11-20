0e40c95e973425edc52b6d4392c8f6de00a2042f
kevin-f-ortega <kevin.f.ortega@gmail.com>
2025-11-20 08:20:17 -0800

    missing more comments (#4463)

--- Os/RawTime.hpp
+++ Os/RawTime.hpp
@@ -146,6 +146,7 @@ class RawTime final : public RawTimeInterface {
     //! that value in its config/ folder.
     //!
     //! \param buffer The buffer to serialize the contents into.
+    //! \param mode Endianness to use when serializing to buffer.
     //! \return Fw::SerializeStatus indicating the result of the serialization.
     Fw::SerializeStatus serializeTo(Fw::SerialBufferBase& buffer,
                                     Fw::Endianness mode = Fw::Endianness::BIG) const override;
@@ -161,6 +162,7 @@ class RawTime final : public RawTimeInterface {
     //! that value in its config/ folder.
     //!
     //! \param buffer The buffer to deserialize the contents from.
+    //! \param mode Endianness to use when deserializing from the buffer.
     //! \return Fw::SerializeStatus indicating the result of the deserialization.
     Fw::SerializeStatus deserializeFrom(Fw::SerialBufferBase& buffer,
                                         Fw::Endianness mode = Fw::Endianness::BIG) override;
