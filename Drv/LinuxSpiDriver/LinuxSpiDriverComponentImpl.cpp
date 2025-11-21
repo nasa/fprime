76c34120d170b00f7b87708fdaeffd87634c7995
djbyrne <djbyrne@jpl.nasa.gov>
2025-11-20 18:07:22 -0800

    Add assertions to LinuxSpiDriver

--- Drv/LinuxSpiDriver/LinuxSpiDriverComponentImpl.cpp
+++ Drv/LinuxSpiDriver/LinuxSpiDriverComponentImpl.cpp
@@ -38,12 +38,16 @@ namespace Drv {
 void LinuxSpiDriverComponentImpl::SpiReadWrite_handler(const FwIndexType portNum,
                                                        Fw::Buffer& writeBuffer,
                                                        Fw::Buffer& readBuffer) {
+    FW_ASSERT(portNum >= 0, static_cast<FwAssertArgType>(portNum));
+    FW_ASSERT(writeBuffer.isValid());
+    FW_ASSERT(readBuffer.isValid());
     (void)SpiWriteRead_handler(portNum, writeBuffer, readBuffer);
 }
 
 SpiStatus LinuxSpiDriverComponentImpl::SpiWriteRead_handler(const FwIndexType portNum,
                                                             Fw::Buffer& writeBuffer,
                                                             Fw::Buffer& readBuffer) {
+    FW_ASSERT(portNum >= 0, static_cast<FwAssertArgType>(portNum));
     FW_ASSERT(writeBuffer.isValid());
     FW_ASSERT(readBuffer.isValid());
     FW_ASSERT(writeBuffer.getSize() == readBuffer.getSize());
