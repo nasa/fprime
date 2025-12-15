3d1bb572cde5bde0b3de71575b6ba63010412451
Timothy Canham <timothy.canham@jpl.nasa.gov>
2025-12-14 17:56:06 -0800

    Initial commit

---
+++ Svc/FileDispatcher/FileDispatcher.cpp
@@ -0,0 +1,84 @@
+// ======================================================================
+// \title  FileDispatcher.cpp
+// \author tcanham
+// \brief  cpp file for FileDispatcher component implementation class
+// ======================================================================
+
+#include "Svc/FileDispatcher/FileDispatcher.hpp"
+#include "Fw/Types/StringUtils.hpp"
+
+namespace Svc {
+
+// ----------------------------------------------------------------------
+// Component construction and destruction
+// ----------------------------------------------------------------------
+
+FileDispatcher ::FileDispatcher(const char* const compName) : FileDispatcherComponentBase(compName) {
+    // disable entries
+    for (FwSizeType i = 0; i < Svc::FileDispatcherCfg::FileDispatchPort::MAX_FILE_DISPATCH_PORTS; i++) {
+        this->m_dispatchTable[i].enabled = false;
+    }
+}
+
+FileDispatcher ::~FileDispatcher() {
+    this->deinit();
+}
+
+void FileDispatcher ::configure(FileDispatcherEntry* entries, FwSizeType numEntries) {
+    FW_ASSERT(entries != nullptr);
+    FW_ASSERT(numEntries > 0);
+    FW_ASSERT(numEntries <= Svc::FileDispatcherCfg::FileDispatchPort::MAX_FILE_DISPATCH_PORTS);
+
+    // copy entries into table
+    for (FwSizeType i = 0; i < numEntries && i < Svc::FileDispatcherCfg::FileDispatchPort::MAX_FILE_DISPATCH_PORTS;
+         i++) {
+        this->m_dispatchTable[i] = entries[i];
+    }
+    // disable remaining entries
+    for (FwSizeType i = numEntries; i < Svc::FileDispatcherCfg::FileDispatchPort::MAX_FILE_DISPATCH_PORTS; i++) {
+        this->m_dispatchTable[i].enabled = false;
+    }
+}
+
+// ----------------------------------------------------------------------
+// Handler implementations for typed input ports
+// ----------------------------------------------------------------------
+
+void FileDispatcher ::fileAnnounceRecv_handler(FwIndexType portNum, Fw::StringBase& file_name) {
+    // determine file extension and dispatch accordingly
+
+    // walk table to find match
+    for (FwIndexType i = 0; i < Svc::FileDispatcherCfg::FileDispatchPort::MAX_FILE_DISPATCH_PORTS; i++) {
+        if (this->m_dispatchTable[i].enabled) {
+            if (Fw::StringUtils::substring_find(file_name.toChar(), file_name.length(),
+                                                this->m_dispatchTable[i].fileExt.toChar(),
+                                                this->m_dispatchTable[i].fileExt.length()) != -1) {
+                // dispatch on this port
+                this->fileDispatch_out(this->m_dispatchTable[i].port, file_name);
+                this->log_ACTIVITY_HI_FileDispatched(file_name,
+                                                     static_cast<Svc::FileDispatcherCfg::FileDispatchPort::T>(i));
+                return;  // dispatched, return
+            }
+        }
+    }
+}
+
+// ----------------------------------------------------------------------
+// Handler implementations for commands
+// ----------------------------------------------------------------------
+
+void FileDispatcher ::ENABLE_DISPATCH_cmdHandler(FwOpcodeType opCode,
+                                                 U32 cmdSeq,
+                                                 Svc::FileDispatcherCfg::FileDispatchPort file_type,
+                                                 Fw::Enabled enable) {
+    this->m_dispatchTable[file_type].enabled = enable;
+    this->log_ACTIVITY_HI_FileDispatchState(file_type, enable);
+    this->cmdResponse_out(opCode, cmdSeq, Fw::CmdResponse::OK);
+}
+
+void FileDispatcher ::pingIn_handler(FwIndexType portNum, U32 key) {
+    // return key
+    this->pingOut_out(0, key);
+}
+
+}  // namespace Svc
---
+++ Svc/FileDispatcher/docs/sdd.md
@@ -0,0 +1,72 @@
+# Svc::FileDispatcher
+
+Component to dispatch delivered files to various services. The user passes in a table that maps file extensions to numbered output ports represented by their own version of the enumeration in the configuration [FileDispatcherCfg.fpp file](../../../default/config/FileDispatcherCfg.fpp). The user then connects the components that process the files to the output ports.
+
+## Requirements
+
+Add requirements in the chart below
+
+| Name | Description | Rationale | Validation |
+|---|---|---|---|
+|FPRIME-FDISP-001|File dispatcher will provide a way to dispatch files to other components|Need to have a way to supply a new file name|Unit Test/System Test|
+|FPRIME-FDISP-002|File dispatcher will provide user configuration to map file types to output ports|Projects will want to customize the dispatching types|Unit Test/System Test|
+|FPRIME-FDISP-003|File dispatcher will dispatch to specified output ports based on the supplied table|Projects will want to connect the dispatch types to components for utilizing files|Unit Test/System Test|
+
+## Usage Examples
+
+### Typical Usage
+
+The `Ref` example connects `Svc::FIleUplink` to `FileDispatcher` to process uplinked files. The `Ref` example configuration table has compiled sequences with a certain extension that `FileDispatcher` dispatches to `Svc::CmdSequencer` for automatic running when those files are uplinked.
+
+## Diagrams
+
+### Class Diagram
+
+![`FileDispatcher` Diagram](img/FileDispatcher.drawio.png "FileUplink")
+
+## Port Descriptions
+
+| Name | Description |
+|---|---|
+|`fileAnnounceRecv`|Input port that receives file name of new file|
+|`fileDispatch`|Array of output ports that dispatch a new files based on a user supplied table|
+|`pingIn`|Input port for Health pinging
+|`pingOut`|Output port for returning Health pings
+
+## Component States
+No state machines
+
+## Parameters
+No parameters
+
+## Commands
+| Name | Description |
+|---|---|
+|`ENABLE_DISPATCH`|Enables or disables a particular dispatch type|
+
+## Events
+| Name | Description |
+|---|---|
+|`FileDispatchState`|Sent when a dispatch type is enabled or disabled|
+|`FileDispatched`|Sent when a dispatch type is matched and dispatched|
+
+## Telemetry
+
+No telemetry
+
+## Unit Tests
+
+| Name | Description | Output | Coverage |
+|---|---|---|---|
+|`dispatchTest`|Tests dispatches of files|---|---|
+|`dispatchAllDisabledTest`|Tests dispatches of files with all file types initially disabled|---|---|
+|`dispatchAllCmdDisabledTest`|Tests dispatches of files with all file types initially enabled, but then disabled by command|---|---|
+|`dispatchAllCmdEnabledTest`|Tests dispatches of files with all file types initially disabled, but then enabled by command|---|---|
+|`dispatchNotFullConfigTest`|Tests dispatches of files with a table with less entries than output ports|---|---|
+|`dispatchPingTest`|Tests ping returns|---|---|
+
+
+## Change Log
+| Date | Description |
+|---|---|
+|12/10/2025| Initial Draft |
