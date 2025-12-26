4f835df36c8adbf37c68b10d3968c88e11f7d6da
Heath Dutton🕴️ <heathdutton@gmail.com>
2025-12-26 01:55:19 -0500

    Use FPP constant for FileDownlink filename length (#4587)

--- Svc/FileDownlink/FileDownlink.hpp
+++ Svc/FileDownlink/FileDownlink.hpp
@@ -13,6 +13,7 @@
 #define Svc_FileDownlink_HPP
 
 #include <Fw/FilePacket/FilePacket.hpp>
+#include <Fw/Types/FileNameString.hpp>
 #include <Os/File.hpp>
 #include <Os/Mutex.hpp>
 #include <Os/Queue.hpp>
@@ -193,12 +194,10 @@ class FileDownlink final : public FileDownlinkComponentBase {
     //! Sources of send file requests
     enum CallerSource { COMMAND, PORT };
 
-#define FILE_ENTRY_FILENAME_LEN 101
-
     //! Used to track a single file downlink request
     struct FileEntry {
-        char srcFilename[FILE_ENTRY_FILENAME_LEN];   // Name of requested file
-        char destFilename[FILE_ENTRY_FILENAME_LEN];  // Name of requested file
+        char srcFilename[Fw::FileNameString::STRING_SIZE];   // Name of requested file
+        char destFilename[Fw::FileNameString::STRING_SIZE];  // Name of requested file
         U32 offset;
         U32 length;
         CallerSource source;  // Source of the downlink request
