dfb31a5165441103583f6783dabcd42874ae9bdb
M Starch <LeStarch@googlemail.com>
2026-03-31 11:04:32 -0700

    Add in channel config

--- Svc/TlmPacketizer/TlmPacketizer.hpp
+++ Svc/TlmPacketizer/TlmPacketizer.hpp
@@ -241,8 +241,8 @@ class TlmPacketizer final : public TlmPacketizerComponentBase, public Fw::ParamE
 
   private:
     FwSizeType m_numChannels;  //!< number of channels being packetized
-    Fw::RedBlackTreeMap<FwChanIdType, FwSizeType, MAX_PACKETIZER_PACKETS> m_channelIndices;
-    Fw::Array<TlmEntry, MAX_PACKETIZER_PACKETS>
+    Fw::RedBlackTreeMap<FwChanIdType, FwSizeType, MAX_PACKETIZER_CHANNELS> m_channelIndices;
+    Fw::Array<TlmEntry, MAX_PACKETIZER_CHANNELS>
         m_channels;  //!< flat storage for channel entries indexed by m_channelIndices
 };
 
--- Svc/TlmPacketizer/config/TlmPacketizerConfig/TlmPacketizerCfg.hpp
+++ Svc/TlmPacketizer/config/TlmPacketizerConfig/TlmPacketizerCfg.hpp
@@ -16,8 +16,11 @@
 #include <Fw/FPrimeBasicTypes.hpp>
 
 namespace Svc {
-//! Maximum number of packets that the packetizer can handle. Must be >= number of non-omitted packets
-static const FwChanIdType MAX_PACKETIZER_PACKETS = 200;
+//! Maximum number of packets that the packetizer can handle.
+static const FwChanIdType MAX_PACKETIZER_PACKETS = 50;
+
+//! Maximum number of channels that the packetizer can handle. Must be >= number of non-omitted channels
+static const FwChanIdType MAX_PACKETIZER_CHANNELS = 200;
 
 //! Maximum number of missing channels to track and report
 static const FwChanIdType TLMPACKETIZER_MAX_MISSING_TLM_CHECK = 25;
