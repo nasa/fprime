d1375a3a051c4e4292a81f5796cffb8064c1cd73
M Starch <LeStarch@googlemail.com>
2026-03-31 14:22:34 -0700

    Fix unread variable

--- Svc/GenericHub/GenericHub.cpp
+++ Svc/GenericHub/GenericHub.cpp
@@ -124,12 +124,8 @@ void GenericHub::fromBufferDriver_handler(const FwIndexType portNum, Fw::Buffer&
     if (status == Fw::FW_SERIALIZE_OK) {
         status = incoming.deserializeTo(size);
     }
-    // Check that the declared size matches the size of the data available
-    if ((status == Fw::FW_SERIALIZE_OK) && (size != (fwBuffer.getSize() - sizeof(U32) - sizeof(U32) - sizeof(FwBuffSizeType)))) {
-        status = Fw::FW_DESERIALIZE_INVALID_DATA;
-    }
-    // All deserialization looks good
-    else if (status == Fw::FW_SERIALIZE_OK) {
+    // All deserialization looks good, check that the size matches the buffer size before calling the appropriate ports
+    if (status == Fw::FW_SERIALIZE_OK && (size == (fwBuffer.getSize() - sizeof(U32) - sizeof(U32) - sizeof(FwBuffSizeType)))) {
         // invokeSerial deserializes arguments before calling a normal invoke, this will return ownership immediately
         U8* rawData = fwBuffer.getData() + sizeof(U32) + sizeof(U32) + sizeof(FwBuffSizeType);
         FwSizeType rawSize = fwBuffer.getSize() - sizeof(U32) - sizeof(U32) - sizeof(FwBuffSizeType);
