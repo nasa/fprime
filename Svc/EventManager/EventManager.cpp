07237988268c5b6834d45e88b661d91913736a2a
vsoulgard <vsoulgard@gmail.com>
2026-04-01 09:34:35 +0300

    Remove unused includes

--- Svc/EventManager/EventManager.cpp
+++ Svc/EventManager/EventManager.cpp
@@ -5,12 +5,9 @@
  *      Author: tcanham
  */
 
-#include <cstdio>
-
 #include <Fw/Types/Assert.hpp>
 #include <Os/File.hpp>
 #include <Svc/EventManager/EventManager.hpp>
-#include <cstring>
 
 namespace Svc {
 static_assert(std::numeric_limits<FwSizeType>::max() >= TELEM_ID_FILTER_SIZE,
