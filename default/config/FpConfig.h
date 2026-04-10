8be09f3308318424ba518de6b3bc8d82ec7652a0
Rob Bocchino <bocchino@jpl.nasa.gov>
2026-04-10 08:19:11 -0700

    Revise FP config
    By default, enable direct port calls when BUILD_UT is not defined

--- default/config/FpConfig.h
+++ default/config/FpConfig.h
@@ -28,7 +28,11 @@ extern "C" {
 
 // Enable direct port calls
 #ifndef FW_DIRECT_PORT_CALLS
-#define FW_DIRECT_PORT_CALLS (0)  //!< Indicates whether direct port calls are used (saves space and time)
+#ifdef BUILD_UT
+#define FW_DIRECT_PORT_CALLS (0)  //!< Indirect port calls are required for unit tests
+#else
+#define FW_DIRECT_PORT_CALLS (1)  //!< Indicates whether direct port calls are used (saves space and time)
+#endif
 #endif
 
 // Allow objects to have names. Allocates storage for each instance
