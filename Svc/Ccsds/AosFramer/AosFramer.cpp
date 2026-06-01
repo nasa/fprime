fce6555aada1dc13871213fa99d4a076cf9ade19
thomas-bc <thomas.boyerchammard@gmail.com>
2026-06-01 15:30:53 -0700

    fix RHEL8 warnings

--- Svc/Ccsds/AosFramer/AosFramer.cpp
+++ Svc/Ccsds/AosFramer/AosFramer.cpp
@@ -162,9 +162,9 @@ void AosFramer ::setup_header(const ComCfg::FrameContext& context) {
     AOSHeader header;
 
     // GVCID (Global Virtual Channel ID) (Standard 4.1.2.2 and 4.1.2.3)
-    U16 globalVcId = static_cast<U16>(context.get_vcId() << AOSHeaderSubfields::virtualChannelIdOffset);
-    globalVcId |= static_cast<U16>((m_spacecraftId & 0x00FF) << AOSHeaderSubfields::spacecraftIdLsbOffset);
-    globalVcId |= static_cast<U16>((Tfvn::AOS & 0x3) << AOSHeaderSubfields::frameVersionOffset);
+    U16 globalVcId = static_cast<U16>((context.get_vcId() << AOSHeaderSubfields::virtualChannelIdOffset) |
+                                      ((m_spacecraftId & 0x00FF) << AOSHeaderSubfields::spacecraftIdLsbOffset) |
+                                      ((Tfvn::AOS & 0x3) << AOSHeaderSubfields::frameVersionOffset));
 
     // Virtual Channel Frame Count (4.1.2.4)
     U32 frameCountAndSignaling = static_cast<U32>((currentVc.virtualFrameCount & 0x00FFFFFFU)
