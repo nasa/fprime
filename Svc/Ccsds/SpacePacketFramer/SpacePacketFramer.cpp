4e153b1576382bc4b1493e6f6a5569ac246d2bb8
Shan Zhang <shan.zhang@fireflyspace.com>
2026-06-04 14:25:24 -0500

    SpacePacketFramer: add secondary header flag

--- Svc/Ccsds/SpacePacketFramer/SpacePacketFramer.cpp
+++ Svc/Ccsds/SpacePacketFramer/SpacePacketFramer.cpp
@@ -41,13 +41,15 @@ void SpacePacketFramer ::dataIn_handler(FwIndexType portNum, Fw::Buffer& data, c
     // -----------------------------------------------
     // Header
     // -----------------------------------------------
-    // PVN is always 0 per Standard - Packet Type is 0 for Telemetry (downlink) - SecHdr flag is 0 for no secondary
-    // header
+    // PVN is always 0 per Standard - Packet Type is 0 for Telemetry (downlink)
     U16 packetIdentification = 0;
     ComCfg::Apid::T apid = context.get_apid();
     FW_ASSERT((apid >> SpacePacketSubfields::ApidWidth) == 0,
               static_cast<FwAssertArgType>(apid));  // apid must fit in 11 bits
+    bool hasSecHdr = context.get_hasSecHdr();
     packetIdentification |= static_cast<U16>(apid) & static_cast<U16>(SpacePacketSubfields::ApidMask);  // 11 bit APID
+    // 1 bit SecHdr flag
+    packetIdentification |= static_cast<U16>(hasSecHdr) & static_cast<U16>(SpacePacketSubfields::SecHdrMask);
 
     U16 sequenceCount = this->getApidSeqCount_out(0, apid, 0);  // retrieve the sequence count for this APID
     U16 packetSequenceControl = 0;
