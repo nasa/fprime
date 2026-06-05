7a2cc5bffe47ba16c614d506f12ff19aa5da490a
thomas-bc <thomas.boyerchammard@gmail.com>
2026-06-04 18:26:23 -0700

    Remove FPRIME_STUB_FALLBACK_ALLOWED

--- cmake/implementation.cmake
+++ cmake/implementation.cmake
@@ -112,19 +112,7 @@ function(fprime__internal_choose_implementations CURRENT_MODULE OUTPUT_VARIABLE)
         elseif(UNKNOWNS AND NOT FPRIME_IS_SUB_BUILD)
             fprime_cmake_warning("[implementation] ${CURRENT_MODULE} requires implementation of ${REQUIRED} assuming one of ${UNKNOWNS} is correct")
         elseif(NOT FPRIME_IS_SUB_BUILD)
-            # Check if this implementation explicitly allows a Stub fallback.
-            # Only implementations listed in FPRIME_STUB_FALLBACK_ALLOWED may
-            # degrade gracefully; all others remain a fatal error so that
-            # platform mis-configurations for critical services (Os_File,
-            # Os_Task, Os_Mutex, …) are caught at configure time.
-            get_property(FALLBACK_ALLOWED GLOBAL PROPERTY "FPRIME_STUB_FALLBACK_ALLOWED")
-            get_property(STUB_IMPL GLOBAL PROPERTY "FPRIME_${REQUIRED}_Stub_IMPLEMENTS")
-            if (STUB_IMPL STREQUAL "${REQUIRED}" AND "${REQUIRED}" IN_LIST FALLBACK_ALLOWED)
-                fprime_cmake_warning("[implementation] ${CURRENT_MODULE}: no implementation chosen for ${REQUIRED}; falling back to ${REQUIRED}_Stub. Platform should add a ${REQUIRED}_* entry to CHOOSES_IMPLEMENTATIONS.")
-                list(APPEND IMPLEMENTATION_DEPENDENCIES "${REQUIRED}_Stub")
-            else()
-                fprime_cmake_fatal_error("[implementation] ${CURRENT_MODULE} requires implementation of ${REQUIRED} but none was chosen")
-            endif()
+            fprime_cmake_fatal_error("[implementation] ${CURRENT_MODULE} requires implementation of ${REQUIRED} but none was chosen")
         endif()
     endforeach()
 
