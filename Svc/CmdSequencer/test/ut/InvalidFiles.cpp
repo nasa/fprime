5d4e370b8d6ee657fc72dcd5865efa6a62bc67f9
M Starch <LeStarch@googlemail.com>
2026-06-09 15:37:39 -0700

    Update Svc/CmdSequencer/test/ut/InvalidFiles.cpp
    Co-authored-by: lestarch-autobot <lestarch+autobot@gmail.com>

--- Svc/CmdSequencer/test/ut/InvalidFiles.cpp
+++ Svc/CmdSequencer/test/ut/InvalidFiles.cpp
@@ -46,7 +46,7 @@ void CmdSequencerTester ::BadCRC() {
     // Assert events
     const CmdSequencerComponentImpl::FPrimeSequence::CRC& crc = file.getCRC();
     ASSERT_EVENTS_SIZE(1);
-    U32 crcFinal;
+    U32 crcFinal = 0;
     crc.m_computed.finalize(crcFinal);
     ASSERT_EVENTS_CS_FileCrcFailure(0, file.getName().toChar(), crc.m_stored, crcFinal);
     // Assert telemetry
