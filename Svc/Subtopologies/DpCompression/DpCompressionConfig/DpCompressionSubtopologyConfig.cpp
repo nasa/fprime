30c80fddda5b4f200a0e311655a9971ca47d5042
Gerik Kubiak <gerik.kubiak@jpl.nasa.gov>
2026-06-16 10:44:42 -0700

    Adding DpCompression subtopology

---
+++ Svc/Subtopologies/DpCompression/DpCompressionConfig/DpCompressionSubtopologyConfig.cpp
@@ -0,0 +1,10 @@
+#include "DpCompressionSubtopologyConfig.hpp"
+
+namespace DpCompression {
+namespace Allocation {
+// This instance can be changed to use a different allocator in the DataProducts Subtopology
+Fw::MallocAllocator mallocatorInstance;
+Fw::MemAllocator& memAllocator = mallocatorInstance;
+}  // namespace Allocation
+}  // namespace DataProducts
+
