83ca1cb1a0111a35ffeb1ea1b37727218a6566b2
Yukti Vijay <yvijay@umich.edu>
2026-07-21 15:03:25 -0700

    chnaging test to auto discover test dir

--- Svc/FileUplink/test/int/test_cmd_uplink.py
+++ Svc/FileUplink/test/int/test_cmd_uplink.py
@@ -1,23 +1,25 @@
 """test_cmd_uplink.py:
 
-Test command dispatcher with basic integration tests.
+Test FileUplink component with basic integration tests.
 """
+from pathlib import Path
 
 
 def test_send_uplink_command(fprime_test_api):
-    """Test that commands may be sent
+    """Test file uplink commands
 
-    Tests command send, dispatch, and receipt using send_and_assert command with a pair of CmdDispatcher commands.
+    Uploads test files from fprime test directory to FSW /tmp/.
+    These files are then used by FileManager and FileDownlink tests.
     """
-    # file_path = test_seq.seq  and destination = /tmp/test_seq.seq (for fileManager)
+    # Locate test files in fprime repository
+    test_dir = Path(__file__).parent.resolve()
+
     fprime_test_api.uplink_file_and_await_completion(
-        "test_seq.seq", "/tmp/test_seq.seq", timeout=100
+        str(test_dir / "test_seq.seq"), "/tmp/test_seq.seq", timeout=100
     )
-    # for fileDownlink
     fprime_test_api.uplink_file_and_await_completion(
-        "test_seq_wait.seq", "/tmp/test_seq_wait.seq", timeout=100
+        str(test_dir / "test_seq_wait.seq"), "/tmp/test_seq_wait.seq", timeout=100
     )
-    # for health, fileDownlink, fileManager
     fprime_test_api.uplink_file_and_await_completion(
-        "1MiB.txt", "/tmp/1MiB.txt", timeout=100
+        str(test_dir / "1MiB.txt"), "/tmp/1MiB.txt", timeout=100
     )
