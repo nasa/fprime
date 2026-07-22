75bc181637c85413d60baa36876c6324ba1c5904
yvijay99 <yvijay@umich.edu>
2026-07-22 15:22:14 -0700

    Remove clear_histories call in telemetry test
    Removed call to clear_histories before awaiting telemetry.

--- Svc/Subtopologies/CdhCore/test/int/test_cdh_core.py
+++ Svc/Subtopologies/CdhCore/test/int/test_cdh_core.py
@@ -62,7 +62,6 @@ def test_telemetry_update(fprime_test_api: IntegrationTestAPI):
     cmd_dispatched_channel = fprime_test_api.get_telemetry_pred("CommandsDispatched")
 
     fprime_test_api.set_tlm_packet_level(3)
-    fprime_test_api.clear_histories()
 
     begin_result = fprime_test_api.await_telemetry(cmd_dispatched_channel, timeout=3)
     begin_tlm_val = begin_result.val_obj.val
